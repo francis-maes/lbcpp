ca86f74c1ec1fa5c88f29105a294482cdd5695a7
francis <francis@885cb74b-98ed-4c74-c960-63734602d729>
2009-03-08 18:13:07 +0000

    Scripts de build

---
+++ build-win32-msvc7.bat
@@ -0,0 +1,7 @@
+cd build
+mkdir win32-msvc7
+cd win32-msvc7
+call "C:\Program Files\Microsoft Visual Studio .NET\Vc7\bin\vcvars32.bat"
+CMakeSetup.exe ..\..
+start /MAX devenv.exe CRAlgo.sln
+cd ../..
