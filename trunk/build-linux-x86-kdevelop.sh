63fe7d5d3bc36faab84662df5f82b190fb4b32af
francis <francis@885cb74b-98ed-4c74-c960-63734602d729>
2009-04-02 15:19:29 +0000

    Fin du renomage vers lcpp

--- trunk/build-linux-x86-kdevelop.sh
+++ trunk/build-linux-x86-kdevelop.sh
@@ -3,5 +3,5 @@ cd build
 mkdir linux-x86-kdevelop
 cd linux-x86-kdevelop
 ccmake -G"KDevelop3" ../..
-kdevelop CRAlgo.kdevelop &
+kdevelop Lcpp.kdevelop &
 cd ../..
--- trunk/build-win32-msvc9.bat
+++ trunk/build-win32-msvc9.bat
@@ -4,5 +4,5 @@ if not exist win32-msvc9 mkdir win32-msvc9
 cd win32-msvc9
 call "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcvarsall.bat"
 CMakeSetup.exe ..\..
-if exist CRAlgo.sln start /MAX Devenv CRAlgo.sln
+if exist CRAlgo.sln start /MAX Devenv Lcpp.sln
 cd ../..
