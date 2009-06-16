2853300e342a0d44c391b6f335a7b7ce687dc8e3
francis <francis@885cb74b-98ed-4c74-c960-63734602d729>
2009-06-16 17:25:32 +0000

    Nouvel example "UserTypeTraits" pour montrer l'utilisation des Traits

---
+++ trunk/examples/Misc/UserTypeTraits.cpp
@@ -0,0 +1,87 @@
+/*-----------------------------------------.---------------------------------.
+| Filename: UserTypeTraits.cpp             | An example that illustrates     |
+| Author  : Francis Maes                   |   definition of Type Traits     |
+| Started : 16/06/2009 19:20               |     for a User type             |
+`------------------------------------------/                                 |
+                               |                                             |
+                               `--------------------------------------------*/
+                               
+#include <lbcpp/lbcpp.h>
+#include <fstream>
+using namespace lbcpp;
+
+class MyClass
+{
+public:
+  MyClass(int value = 0)
+    : value(value) {}
+    
+  int getValue() const
+    {return value;}
+    
+  void setValue(int value)
+    {this->value = value;}
+  
+private:
+  int value;
+};
+
+namespace lbcpp
+{
+
+template<>
+struct Traits<MyClass>
+{
+  static inline std::string toString(const MyClass& c)
+    {return "value = " + lbcpp::toString(c.getValue());}
+
+  static inline void write(std::ostream& ostr, const MyClass& c)
+    {lbcpp::write(ostr, c.getValue());}
+    
+  static inline bool read(std::istream& istr, MyClass& result)
+  {
+    int val;
+    if (lbcpp::read(istr, val))
+    {
+      result.setValue(val);
+      return true;
+    }
+    return false;
+  }
+};
+
+};  // namespace lbcpp 
+
+int main(int argc, char* argv[])
+{
+  MyClass toto1(1);
+  MyClass toto2(2);
+  MyClass* totoptr = &toto1;
+  MyClass** totoptrptr = &totoptr;
+  std::vector<MyClass> totos(5);
+  for (size_t i = 0; i < totos.size(); ++i) totos[i].setValue(i);
+  std::vector<MyClass* > totoptrs(5);
+  for (size_t i = 0; i < totoptrs.size(); ++i) {totoptrs[i] = new MyClass(i);}
+  
+  std::cout << lbcpp::toString(toto1) << std::endl
+            << lbcpp::toString(toto2) << std::endl
+            << lbcpp::toString(&toto1) << std::endl
+            << lbcpp::toString(&toto2) << std::endl
+            << lbcpp::toString(totoptr) << std::endl
+            << lbcpp::toString(totoptrptr) << std::endl
+            << lbcpp::toString(totos) << std::endl
+            << lbcpp::toString(totoptrs) << std::endl
+            << lbcpp::toString(&totoptrs) << std::endl;
+  
+  {
+    std::ofstream ostr("tmpfile");
+    lbcpp::write(ostr, toto1);
+  }
+  
+  {
+    std::ifstream istr("tmpfile");
+    lbcpp::read(istr, toto2);
+    std::cout << lbcpp::toString(toto2) << std::endl;
+  }
+  return 0;
+}
