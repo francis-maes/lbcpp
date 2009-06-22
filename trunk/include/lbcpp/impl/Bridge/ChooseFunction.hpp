69ea24584b25d91f796443f237e832e8b88b240c
francis <francis@885cb74b-98ed-4c74-c960-63734602d729>
2009-06-22 17:45:19 +0000

    Restructuration..

--- trunk/include/lbcpp/impl/Bridge/ChooseFunction.hpp
+++ trunk/include/lbcpp/impl/Bridge/ChooseFunction.hpp
@@ -1,5 +1,5 @@
 /*-----------------------------------------.---------------------------------.
-| Filename: ChooseFunctionStatic.hpp       | Static Choose Functions         |
+| Filename: ChooseFunction.hpp             | Static Choose Functions         |
 | Author  : Francis Maes                   |            Interface            |
 | Started : 12/03/2009 16:39               |                                 |
 `------------------------------------------/                                 |
@@ -93,6 +93,65 @@ struct ActionDescriptionFunction : public ChooseFunction<ExactType>
   std::string compute(const ChoiceType& choice) const
     {assert(false); return "";}
 };
+
+
+/*
+** Choose Function
+*/
+STATIC_TO_DYNAMIC_ABSTRACT_CLASS(ChooseFunction_, Object)
+  virtual void setChoose(ChoosePtr choose)
+  {
+    assert(choose);
+    BaseClass::impl.setChoose(choose);
+  }
+};
+
+STATIC_TO_DYNAMIC_CLASS(ChooseFunction, ChooseFunction_)
+STATIC_TO_DYNAMIC_ENDCLASS(ChooseFunction);
+
+/*
+** Values
+*/
+STATIC_TO_DYNAMIC_CLASS(StateValueFunction, ChooseFunction_)
+  virtual double compute() const
+    {return BaseClass::impl.compute();}
+STATIC_TO_DYNAMIC_ENDCLASS(StateValueFunction);
+
+STATIC_TO_DYNAMIC_CLASS(ActionValueFunction, ChooseFunction_)
+  virtual double compute(VariablePtr choice) const
+    {return BaseClass::impl.computeDynamicType(choice);}
+STATIC_TO_DYNAMIC_ENDCLASS(ActionValueFunction);
+
+/*
+** Features
+*/
+STATIC_TO_DYNAMIC_CLASS(StateFeaturesFunction, ChooseFunction_)
+  virtual FeatureGeneratorPtr compute() const
+    {return BaseClass::impl.compute();}
+STATIC_TO_DYNAMIC_ENDCLASS(StateFeaturesFunction);
+
+
+STATIC_TO_DYNAMIC_CLASS(ActionFeaturesFunction, ChooseFunction_)
+  virtual FeatureDictionaryPtr getDictionary() const
+    {typedef typename ImplementationType::FeatureGenerator FG; return FG::getDictionary();}
+
+  virtual FeatureGeneratorPtr compute(VariablePtr choice) const
+    {assert(choice); return BaseClass::impl.compute(choice->getConstReference<typename ImplementationType::ChoiceType>());}
+STATIC_TO_DYNAMIC_ENDCLASS_1(ActionFeaturesFunction);
+
+
+/*
+** String Descriptions
+*/
+STATIC_TO_DYNAMIC_CLASS(StateDescriptionFunction, ChooseFunction_)
+  virtual std::string compute() const
+    {return BaseClass::impl.compute();}
+STATIC_TO_DYNAMIC_ENDCLASS(StateDescriptionFunction);
+
+STATIC_TO_DYNAMIC_CLASS(ActionDescriptionFunction, ChooseFunction_)
+  virtual std::string compute(VariablePtr choice) const
+    {return BaseClass::impl.compute(choice->getConstReference<typename ImplementationType::ChoiceType>());}
+STATIC_TO_DYNAMIC_ENDCLASS_1(ActionDescriptionFunction);
 
 }; /* namespace impl */
 }; /* namespace lbcpp */
