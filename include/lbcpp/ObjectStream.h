42036c6733698148e15270d4fc522b132ee0f74b
froz <froz@885cb74b-98ed-4c74-c960-63734602d729>
2009-07-07 22:30:33 +0000

    [DOC] ObjectStream corrections...

--- include/lbcpp/ObjectStream.h
+++ include/lbcpp/ObjectStream.h
@@ -29,19 +29,23 @@ namespace lbcpp
 
 /*!
 ** @class ObjectFunction
-** @brief Object function container for streaming.
+** @brief Represents a function which takes an Object as input and
+** returns an Object. ObjectFunction can be applied to Object streams
+** and Object containers.
 **
-** A function can be "converted" to an ObjectFunction and used throw a
-** stream.
+** A function object is a computer programming construct allowing an
+** object to be invoked or called as if it were an ordinary function,
+** usually with the same syntax.
 **
+** @see ObjectStream, ObjectContainer
 */
 class ObjectFunction : public Object
 {
 public:
   /*!
-  ** @a getOutputClassName returns Object class name.
+  ** Returns the class name of the function output.
   **
-  ** @return class name.
+  ** @return the class name of the function output.
   */
   virtual std::string getOutputClassName() const = 0;
 
@@ -62,10 +66,10 @@ public:
 **
 ** Some databases are too huge to fit into memory, so with
 ** ObjectStream, it is not necessary to load them entirely. A stream is
-** open and items are load from the database only when necessary.
+** open and items are loaded from the database only when necessary.
 **
-** In order to fit all usages, users can used ObjectStream as a stream
-** (each new item should be load from the database using next()
+** In order to fit all usages, users can use ObjectStream as a stream
+** (each new item should be loaded from the database using next()
 ** method) or as a data loader (all items are loaded into memory).
 */
 class ObjectStream : public Object
@@ -80,7 +84,7 @@ public:
     {return "Object";}
 
   /*!
-  ** Check if the stream is OK or not.
+  ** Checks if the stream is OK or not.
   **
   ** @return True, if the stream is valid. Returns False if any
   ** stream problem occurs.
@@ -89,14 +93,14 @@ public:
     {return true;}
 
   /*!
-  ** Load and return the next item from the database.
+  ** Loads and returns the next item from the stream.
   **
   ** @return an object pointer on the next database item.
   */
   virtual ObjectPtr next() = 0;
 
   /*!
-  ** Load, cast and return the next item from the database.
+  ** Loads, casts and returns the next item from the stream.
   **
   ** @return an object pointer (of type @a T) on the next database item.
   */
@@ -108,48 +112,108 @@ public:
   }
 
   /*!
-  ** Check object content by its name.
+  ** Checks if getContentClassName() == @a expectedClassName.
   **
+  ** @see ObjectStream::getContentClassName
+  ** @see Object::error
   ** @param expectedClassName : expected class name.
   **
-  ** @return true if ContentClassName == expectClassName, false otherwise.
+  ** @return True if (ContentClassName == expectClassName), False
+  ** otherwise and throw an error to ErrorManager.
   */
   bool checkContentClassName(const std::string& expectedClassName) const;
 
   /*!
-  ** Load object at the position currentPosition + @a maximumCount
-  ** into the stream.
+  ** Calls next() up to @a maximumCount times and ignores the loaded
+  ** objects. If @a maximumCount == 0, the function calls next() until
+  ** the end of the stream. Otherwise, if the end of the stream occurs
+  ** before @a maximumCount calls to next(), the function stops. Note
+  ** that this function is essentially provided for debugging purpose.
   **
-  ** If @a maximunCount == 0, @a iterate() loads the last item and @a
-  ** count == number of objects.
   **
   ** @param maximumCount : iteration steps.
-  ** @return false if any errors occurs.
+  ** @return False if any errors occurs.
   */
   bool iterate(size_t maximumCount = 0);
 
   /*!
-  ** Load \a maximumCount items from the database into the stream. If
+  ** Loads \a maximumCount items (maximum) from the stream and stores
+  ** them into memory. If
   ** @a maximumCount == 0, @a load() loads all items into memory.
   **
+  ** @see ObjectContainer
   ** @param maximumCount : number of item to load.
   **
-  ** @return an object container pointer.
+  ** @return an object container instance containing loaded items.
   */
   ObjectContainerPtr load(size_t maximumCount = 0);
 
   /*!
-  ** Apply the function @a function to stream items.
+  ** Applies the function @a function to a new stream derivated from the
+  ** current stream.
   **
   ** @param function : function to apply on items.
   **
-  ** @return a new object stream pointer.
+  ** @return a new object stream instance.
   */
   ObjectStreamPtr apply(ObjectFunctionPtr function);
 };
 
 /*!
-** Classification examples parser.
+** Creates a classification examples parser.
+**
+** Each line of the datafile should contain a classification example.
+** Each example begins with the class identifier followed by a list of
+** features. A feature is defined by an identifier and an optional
+** associated real value. When no value is specified, the default
+** value 1.0 is assumed.
+**
+**     Examples:
+**
+**     - Dense Vectors, 6 continuous features, 2 classes:
+**     \verbatim
+**     1 1:0.0526316 2:0.2 3:-0.456954 4:-0.428571 5:-0.821918 6:-1
+**     2 1:0.0526316 2:-0.286957 3:-0.271523 4:-0.298701 5:-0.876712
+**     6:-1
+**     2 1:0.105263 2:-0.46087 3:-0.615894 4:-0.714286 5:-0.664384
+**     6:-1
+**     \endverbatim
+**
+**     - Sparse Vectors, binary features, 3 classes:
+**     \verbatim
+**     yes 6:1 8:1 15:1 21:1 25:1 33:1 34:1 37:1 42:1 50:1 53:1 57:1
+**     67:1 76:1 78:1 81:1 84:1 86:1
+**     no 2:1 3:1 20:1 22:1 23:1 33:1 35:1 36:1 47:1 50:1 51:1 58:1
+**     67:1 76:1 79:1 81:1 82:1 86:1
+**     maybe 2:1 8:1 19:1 21:1 27:1 33:1 34:1 36:1 44:1 50:1 53:1 57:1
+**     67:1 76:1 78:1 83:1 87:1
+**     \endverbatim
+**
+**     - Equivalently, short syntax:
+**     \verbatim
+**     yes 6 8 15 21 25 33 34 37 42 50 53 57 67 76 78 81 84 86
+**     no 2 3 20 22 23 33 35 36 47 50 51 58 67 76 79 81 82 86
+**     maybe 2 8 19 21 27 33 34 36 44 50 53 57 67 76 78 83 87
+**     \endverbatim
+**
+**     - Features do not need to be sorted and can use any
+**     alphanumeric identifier:
+**     \verbatim
+**     class1 afeature anotherfeature
+**     class2 feat150 feat12 feat315
+**     class3 501636 23543 2353262
+**     class4 aaa AAA bbb BBB
+**     \endverbatim
+**
+**     - Both feature syntaxes can be mixed:
+**     \verbatim
+**     -1 1:0.7 2 4 5:0.9
+**     +1 1:0.3 2 3 5:-0.7
+**     \endverbatim
+**
+** Note that the format used by libSVM is a particular case of the one
+** presented below. A repository of example datafiles can be found
+** here: http://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets.
 **
 ** @param filename : classification examples file name.
 ** @param features : feature dictionary.
@@ -160,9 +224,14 @@ public:
 extern ObjectStreamPtr classificationExamplesParser(const std::string& filename,
                                 FeatureDictionaryPtr features, StringDictionaryPtr labels);
 
-// generateur d'exemples de classification linéairement séparable
 /*!
-** Classification examples generator (linear).
+** Creates a synthetic generator of linearly separable classification
+** data.
+**
+** Each time you call next() function (@see ObjectStream::next), a new
+** classification example is generated where each feature is drawed
+** from a normal Gaussian distribution and class labels are drawed from
+** range [0, @a numClasses[.
 **
 ** @param numFeatures : feature vector dimension.
 ** @param numClasses : number of classes.
@@ -173,7 +242,7 @@ extern ObjectStreamPtr classificationExamplesSyntheticGenerator(size_t numFeatur
 
 
 /*!
-**  Regression examples parser.
+**  Creates a regression examples parser.
 **
 ** @param filename : regression examples file name.
 ** @param features : feature dictionary.
@@ -187,7 +256,15 @@ extern ObjectStreamPtr regressionExamplesParser(const std::string& filename, Fea
 ** @class TextObjectParser
 ** @brief Text object parser.
 **
-** #FIXME
+** Parses a file line by line. Calls parseBegin() the first time you call
+** next() function, them it calls parseLine() function until setResult() function
+** is called. When next() function reaches the end of file, it calls
+** parseEnd() function.
+**
+** @see TextObjectParser::parseBegin
+** @see TextObjectParser::parseLine
+** @see TextObjectParser::parseEnd
+** @see TextObjectParser::setResult
 */
 class TextObjectParser : public ObjectStream
 {
@@ -216,7 +293,7 @@ public:
   virtual ~TextObjectParser();
 
   /*!
-  ** Check if the stream is OK or not.
+  ** Checks if the stream is OK or not.
   **
   ** @return True, if the stream is valid. Returns False if any
   ** stream problem occurs.
@@ -225,33 +302,34 @@ public:
     {return istr != NULL;}
 
   /*!
-  ** #FIXME: what did you expect ?
+  ** Function called at the begin of the parsing.
   **
   */
   virtual void parseBegin()   {}
 
   /*!
-  ** Parse a string line.
+  ** Parses a string line.
   **
   ** @param line : text line.
   **
-  ** @return
+  ** @return a boolean.
   */
   virtual bool parseLine(const std::string& line) = 0;
 
   /*!
   **
-  ** #FIXME: ?
+  ** Function called at the end of the parsing.
   **
-  ** @return
+  ** @return a boolean.
   */
   virtual bool parseEnd()     {return true;}
 
   /*!
-  ** Load the next object in the stream (ie. the current object
+  ** Loads the next object in the stream (ie. the current object
   ** pointer is updated).
   **
-  ** @return a pointer on the next object in the stream.
+  ** @return a pointer on the next object in the stream or ObjectPtr()
+  ** is there is no more object into the stream..
   */
   virtual ObjectPtr next();
 
@@ -266,8 +344,7 @@ protected:
     {currentObject = object;}
 
   /*!
-  ** Tokenize a text classification example and load it into
-  ** the @a columns vector.
+  ** Tokenizes a line.
   **
   ** @param line : example text line.
   ** @param columns : item container.
@@ -309,9 +386,63 @@ private:
 
 /*!
 ** @class LearningDataObjectParser
-** @brief Learning data object parser.
+** @brief Base classe for data file parsers.
 **
-** #FIXME
+** Parsers using libSVM data file format.
+**
+** Each line of the datafile should contain a classification example.
+** Each example begins with the class identifier followed by a list of
+** features. A feature is defined by an identifier and an optional
+** associated real value. When no value is specified, the default
+** value 1.0 is assumed.
+**
+**     Examples:
+**
+**     - Dense Vectors, 6 continuous features, 2 classes:
+**     \verbatim
+**     1 1:0.0526316 2:0.2 3:-0.456954 4:-0.428571 5:-0.821918 6:-1
+**     2 1:0.0526316 2:-0.286957 3:-0.271523 4:-0.298701 5:-0.876712
+**     6:-1
+**     2 1:0.105263 2:-0.46087 3:-0.615894 4:-0.714286 5:-0.664384
+**     6:-1
+**     \endverbatim
+**
+**     - Sparse Vectors, binary features, 3 classes:
+**     \verbatim
+**     yes 6:1 8:1 15:1 21:1 25:1 33:1 34:1 37:1 42:1 50:1 53:1 57:1
+**     67:1 76:1 78:1 81:1 84:1 86:1
+**     no 2:1 3:1 20:1 22:1 23:1 33:1 35:1 36:1 47:1 50:1 51:1 58:1
+**     67:1 76:1 79:1 81:1 82:1 86:1
+**     maybe 2:1 8:1 19:1 21:1 27:1 33:1 34:1 36:1 44:1 50:1 53:1 57:1
+**     67:1 76:1 78:1 83:1 87:1
+**     \endverbatim
+**
+**     - Equivalently, short syntax:
+**     \verbatim
+**     yes 6 8 15 21 25 33 34 37 42 50 53 57 67 76 78 81 84 86
+**     no 2 3 20 22 23 33 35 36 47 50 51 58 67 76 79 81 82 86
+**     maybe 2 8 19 21 27 33 34 36 44 50 53 57 67 76 78 83 87
+**     \endverbatim
+**
+**     - Features do not need to be sorted and can use any
+**     alphanumeric identifier:
+**     \verbatim
+**     class1 afeature anotherfeature
+**     class2 feat150 feat12 feat315
+**     class3 501636 23543 2353262
+**     class4 aaa AAA bbb BBB
+**     \endverbatim
+**
+**     - Both feature syntaxes can be mixed:
+**     \verbatim
+**     -1 1:0.7 2 4 5:0.9
+**     +1 1:0.3 2 3 5:-0.7
+**     \endverbatim
+**
+** Note that the format used by libSVM is a particular case of the one
+** presented below. A repository of example datafiles can be found
+** here: http://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets.
+**
 */
 class LearningDataObjectParser : public TextObjectParser
 {
@@ -340,48 +471,57 @@ public:
     : TextObjectParser(newInputStream), features(features) {}
 
   /*!
-  ** Parse an empty line.
+  ** Parses an empty line.
   **
-  ** @return Always True.
+  ** @return a boolean.
   */
   virtual bool parseEmptyLine()
     {return true;}
 
   /*!
-  ** Parse a data line.
+  ** Parses a tokenized data line.
   **
-  ** @param columns : data container.
+  ** @param columns : tokenized data line.
   **
-  ** @return False if any error occurs.
+  ** @return a boolean.
   */
   virtual bool parseDataLine(const std::vector<std::string>& columns)
     {return false;}
 
   /*!
-  ** Parse comment line.
+  ** Parses comment line (a line starting by '#').
   **
-  ** @param comment : text line.
+  ** @param comment : comment line.
   **
-  ** @return always True.
+  ** @return a boolean.
   */
   virtual bool parseCommentLine(const std::string& comment)
     {return true;}
 
   /*!
-  ** #FIXME ?
+  ** @a parseEnd is called when the parser reaches the end of file. It
+  ** calls parseEmptyLine().
   **
+  ** @see LearningDataObjectParser::parseEmptyLine
   **
-  ** @return parseEmptyLine().
+  ** @return parseEmptyLine() result.
   */
   virtual bool parseEnd()
     {return parseEmptyLine();}
 
   /*!
-  ** Parse a text line.
+  ** Parses a text line following these rules:
+  ** - if the line is empty, it calls @a parseEmptyLine()
+  ** - if the line starts by '#', it calls @a parseCommentLine()
+  ** - otherwise, it tokenizes the line before calling @a parseDataLine()
+  **
+  ** @see LearningDataObjectParser::parseEmptyLine
+  ** @see LearningDataObjectParser::parseCommentLine
+  ** @see LearningDataObjectParser::parseDataLine
   **
   ** @param line : text line.
   **
-  ** @return False if any error occurs.
+  ** @return a boolean.
   */
   virtual bool parseLine(const std::string& line);
 
@@ -396,7 +536,7 @@ protected:
   ** @param firstColumn : start column number.
   ** @param res : parse result container.
   **
-  ** @return False i any error occurs.
+  ** @return False if any error occurs.
   */
   bool parseFeatureList(const std::vector<std::string>& columns,
                         size_t firstColumn, SparseVectorPtr& res);
@@ -410,7 +550,7 @@ protected:
   ** @param featureId : feature ID container.
   ** @param featureValue : feature value container.
   **
-  ** @return False is any problem occurs.
+  ** @return False if any problem occurs.
   */
   static bool parseFeature(const std::string& str, std::string& featureId, double& featureValue);
 
