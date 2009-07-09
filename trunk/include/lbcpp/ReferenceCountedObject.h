e5ad7719746ec1d5101ccf7424be373667986193
francis <francis@885cb74b-98ed-4c74-c960-63734602d729>
2009-07-09 16:56:03 +0000

    [doc] ReferenceCountedObject et ReferenceCountedObjectPtr

--- trunk/include/lbcpp/ReferenceCountedObject.h
+++ trunk/include/lbcpp/ReferenceCountedObject.h
@@ -22,34 +22,45 @@
 namespace lbcpp
 {
 
-/*!
-** @class ReferenceCountedObject
-** @brief
+/**
+    Adds reference-counting to an object.
+
+    To add reference-counting to a class, derive it from this class, and
+    use the ReferenceCountedObjectPtr class to point to it.
+
+    e.g. @code
+    class MyClass : public ReferenceCountedObject
+    {
+    public:
+        void foo();
+    };
+    typedef ReferenceCountedObjectPtr<MyClass> MyClassPtr;
+
+    MyClassPtr p = new MyClass();
+    MyClassPtr p2 = p;
+    p = MyClassPtr();
+    p2->foo();
+    @endcode
+
+    Once a new ReferenceCountedObject has been assigned to a pointer, be
+    careful not to delete the object manually.
+
+    @see ReferenceCountedObjectPtr
 */
 class ReferenceCountedObject
 {
 public:
-  /*!
-  **
-  **
-  */
+  /** Creates the reference-counted object (with an initial ref count of zero). */
   ReferenceCountedObject() : refCount(0) {}
 
-  /*!
-  **
-  **
-  **
-  ** @return
-  */
+  /** Destructor. */
   virtual ~ReferenceCountedObject()
-    {assert(refCount == 0);}
+  {
+    // it's dangerous to delete an object that's still referenced by something else!
+    assert(refCount == 0);
+  }
 
-  /*!
-  **
-  **
-  **
-  ** @return
-  */
+  /** Returns the object's current reference count. */
   int getReferenceCount() const
     {return refCount;}
 
@@ -59,19 +70,13 @@ protected:
   template<class T>
   friend struct StaticallyAllocatedReferenceCountedObjectPtr; /*!< */
 
-  size_t refCount;              /*!< */
+  size_t refCount;              /*!< The object's reference count */
 
-  /*!
-  **
-  **
-  */
+  /** Increments the object's reference count.  */
   inline void incrementReferenceCounter()
     {++refCount;}
 
-  /*!
-  **
-  **
-  */
+  /** Decrements the object's reference count.  */
   inline void decrementReferenceCounter()
   {
     assert(refCount > 0);
@@ -81,181 +86,127 @@ protected:
   }
 };
 
-/*!
-** @struct ReferenceCountedObjectPtr
-** @brief
+/**
+    Used to point to an object of type ReferenceCountedObject.
+
+    It's wise to use a typedef instead of typing out the templated name
+    each time - e.g.
+
+    typedef ReferenceCountedObjectPtr<MyClass> MyClassPtr;
+
+    @see ReferenceCountedObject
 */
 template <class T>
 struct ReferenceCountedObjectPtr
 {
+  /** Copies another pointer.
 
-  /*!
-  **
-  **
-  ** @param other
-  **
-  ** @return
+      This will increment the object's reference-count (if it is non-null).
   */
   template<class O>
   ReferenceCountedObjectPtr(const ReferenceCountedObjectPtr<O>& other) : ptr(static_cast<T* >(other.get()))
     {if (ptr != 0) cast(ptr).incrementReferenceCounter();}
 
-  /*!
-  **
-  **
-  ** @param other
-  **
-  ** @return
+  /** Copies another pointer.
+
+      This will increment the object's reference-count (if it is non-null).
   */
   ReferenceCountedObjectPtr(const ReferenceCountedObjectPtr<T>& other) : ptr(other.get())
     {if (ptr != 0) cast(ptr).incrementReferenceCounter();}
 
-  /*!
-  **
-  **
-  ** @param ptr
-  **
-  ** @return
+  /** Creates a pointer to an object.
+
+      This will increment the object's reference-count if it is non-null.
   */
   ReferenceCountedObjectPtr(T* ptr) : ptr(ptr)
     {if (ptr != 0) cast(ptr).incrementReferenceCounter();}
 
-  /*!
-  **
-  **
-  **
-  ** @return
-  */
+  /** Creates a pointer to a null object. */
   ReferenceCountedObjectPtr() : ptr(NULL)
     {}
 
-  /*!
-  **
-  **
-  **
-  ** @return
+  /** Destructor.
+
+      This will decrement the object's reference-count, and may delete it if it
+      gets to zero.
   */
   ~ReferenceCountedObjectPtr()
-  {if (ptr) cast(ptr).decrementReferenceCounter();}
+    {if (ptr) cast(ptr).decrementReferenceCounter();}
 
-  /*!
-  **
-  **
-  ** @param other
-  **
-  ** @return
+  /** Changes this pointer to point at a different object.
+
+      The reference count of the old object is decremented, and it might be
+      deleted if it hits zero. The new object's count is incremented.
   */
   ReferenceCountedObjectPtr<T>& operator =(const ReferenceCountedObjectPtr<T>& other)
-  {changePtr(other.get()); return *this;}
+    {changePtr(other.get()); return *this;}
 
-  /*!
-  **
-  **
-  ** @param other
-  **
-  ** @return
+  /** Changes this pointer to point at a different object.
+
+      The reference count of the old object is decremented, and it might be
+      deleted if it hits zero. The new object's count is incremented.
   */
   template<class O>
   ReferenceCountedObjectPtr<T>& operator =(const ReferenceCountedObjectPtr<O>& other)
-  {changePtr(static_cast<T* >(other.get())); return *this;}
+    {changePtr(static_cast<T* >(other.get())); return *this;}
 
-  /*!
-  **
-  **
-  ** @param newT
-  **
-  ** @return
+  /** Changes this pointer to point at a different object.
+
+      The reference count of the old object is decremented, and it might be
+      deleted if it hits zero. The new object's count is incremented.
   */
   ReferenceCountedObjectPtr<T>& operator= (T* newT)
     {changePtr(newT); return *this;}
 
-  /*!
-  **
-  **
+  /** Sets this pointer to null.
+  
+    This will decrement the object's reference-count, and may delete it if it
+      gets to zero.
   */
   void clear()
     {changePtr(NULL);}
 
-  /*!
-  **
-  **
-  **
-  ** @return
-  */
+  /** Returns true if this pointer is non-null. */
   bool exists() const
     {return ptr != NULL;}
 
-  /*!
-  **
-  **
-  **
-  ** @return
-  */
+  /** Returns true if this pointer is non-null. */
   operator bool () const
     {return ptr != NULL;}
 
-  /*!
-  **
-  **
-  ** @param other
-  **
-  ** @return
-  */
+  /** Returns true if this pointer refers to the given object. */
   bool operator ==(const ReferenceCountedObjectPtr<T>& other) const
     {return ptr == other.ptr;}
 
-  /*!
-  **
-  **
-  ** @param other
-  **
-  ** @return
-  */
+  /** Returns true if this pointer doesn't refer to the given object. */
   bool operator !=(const ReferenceCountedObjectPtr<T>& other) const
     {return ptr != other.ptr;}
 
-  /*!
-  **
-  **
-  ** @param other
-  **
-  ** @return
-  */
+  /** Returns true if this pointer is smaller to the pointer of the given object. */
   bool operator <(const ReferenceCountedObjectPtr<T>& other) const
     {return ptr < other.ptr;}
 
-  /*!
-  **
-  **
-  **
-  ** @return
+  /** Returns the object that this pointer references.
+
+      The pointer returned may be zero, of course.
   */
   T* get() const
     {return ptr;}
 
-  /*!
-  **
-  **
-  **
-  ** @return
+  /** Returns the object that this pointer references.
+
+      The pointer returned may be zero, of course.
   */
   T* operator -> () const
     {return ptr;}
 
-  /*!
-  **
-  **
-  **
-  ** @return
-  */
+  /** Returns a reference to the object that this pointer references. */
   T& operator * () const
     {assert(ptr); return *ptr;}
 
-  /*!
-  **
-  **
-  **
-  ** @return
+  /** Dynamic cast the object that this pointer references.
+  
+    If the cast is invalid, this function returns a null pointer.
   */
   template<class O>
   inline ReferenceCountedObjectPtr<O> dynamicCast() const
@@ -269,11 +220,9 @@ struct ReferenceCountedObjectPtr
     return ReferenceCountedObjectPtr<O>();
   }
 
-  /*!
-  **
-  **
-  **
-  ** @return
+  /** Static cast the object that this pointer references.
+  
+     This cast is unchecked, so be sure of what you are doing.
   */
   template<class O>
   inline ReferenceCountedObjectPtr<O> staticCast() const
@@ -286,12 +235,7 @@ struct ReferenceCountedObjectPtr
     return ReferenceCountedObjectPtr<O>();
   }
 
-  /*!
-  **
-  **
-  **
-  ** @return
-  */
+  /** Returns true if the object that this pointer references is an instance of the given class. */
   template<class O>
   inline bool isInstanceOf() const
     {return dynamic_cast<O* >(ptr) != NULL;}
@@ -299,22 +243,9 @@ struct ReferenceCountedObjectPtr
 private:
   T* ptr;                       /*!< */
 
-  // enables the use of ReferenceCountedObjectPtrs on predeclared objects
-  /*!
-  **
-  **
-  ** @param ptr
-  **
-  ** @return
-  */
   static inline ReferenceCountedObject& cast(T* ptr)
     {return **(ReferenceCountedObject** )(&ptr);}
 
-  /*!
-  **
-  **
-  ** @param newPtr
-  */
   inline void changePtr(T* newPtr)
   {
     if (ptr != newPtr)
@@ -327,52 +258,21 @@ private:
   }
 };
 
-/*!
-** @struct StaticallyAllocatedReferenceCountedObjectPtr
-** @brief
-*/
 template<class T>
 struct StaticallyAllocatedReferenceCountedObjectPtr
 {
-  /*!
-  **
-  **
-  ** @param ptr
-  **
-  ** @return
-  */
   StaticallyAllocatedReferenceCountedObjectPtr(T& ptr) : ptr(ptr)
     {++(ptr.refCount);}
-
-  /*!
-  **
-  **
-  **
-  ** @return
-  */
   ~StaticallyAllocatedReferenceCountedObjectPtr()
     {--(ptr.refCount);}
 
-  /*!
-  **
-  **
-  **
-  ** @return
-  */
   operator ReferenceCountedObjectPtr<T>() const
     {return ReferenceCountedObjectPtr<T>(&ptr);}
-
-  /*!
-  **
-  **
-  **
-  ** @return
-  */
   T* operator -> () const
     {return &ptr;}
 
 private:
-  T& ptr;                       /*!< */
+  T& ptr;
 };
 
 }; /* namespace lbcpp */
