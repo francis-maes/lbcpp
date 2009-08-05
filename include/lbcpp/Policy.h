545b3bd127ee135800421c1e62e03c9b619b8612
froz <froz@885cb74b-98ed-4c74-c960-63734602d729>
2009-08-05 15:31:04 +0000

    [DOC] Policy.

--- include/lbcpp/Policy.h
+++ include/lbcpp/Policy.h
@@ -45,28 +45,30 @@ namespace lbcpp
 
 /*!
 ** @class Policy
-** @brief A Policy describe the way choices are made.
+** @brief A Policy is a decision-maker in a Decision
+** Process. Formally, a policy \f$ \pi \f$ can be seen as a function
+** that map states to actions: \f$ \pi:S\to A \f$.
 */
 class Policy : public Object
 {
 public:
   /**
-  ** Runs the current policy on the @a crAlgorithm from its initial state. 
+  ** Runs the current policy on the @a crAlgorithm from its initial state.
   **
   ** @param crAlgorithm : CRAlgorithm to run.
-  ** @param statistics : policy statistics container. 
+  ** @param statistics : policy statistics container.
   **
   ** @return a boolean.
   ** @see CRAlgorithm
-  ** @see PolicyStatistics 
+  ** @see PolicyStatistics
   */
   bool run(CRAlgorithmPtr crAlgorithm, PolicyStatisticsPtr statistics = PolicyStatisticsPtr());
 
   /**
-  ** Runs the current policy on the @a crAlgorithm from its initial state. 
+  ** Runs the current policy on the @a crAlgorithm from its initial state.
   **
   ** @param crAlgorithms : CRAlgorithm to run.
-  ** @param statistics : policy statistics container. 
+  ** @param statistics : policy statistics container.
   ** @param progress : progress bar callback.
   **
   ** @return a boolean.
@@ -76,10 +78,10 @@ public:
   bool run(ObjectStreamPtr crAlgorithms, PolicyStatisticsPtr statistics = PolicyStatisticsPtr(), ProgressCallbackPtr progress = ProgressCallbackPtr());
 
   /**
-  ** Runs the current policy on the @a crAlgorithm from its initial state. 
+  ** Runs the current policy on the @a crAlgorithm from its initial state.
   **
   ** @param crAlgorithms : CRAlgorithm to run.
-  ** @param statistics : policy statistics container. 
+  ** @param statistics : policy statistics container.
   ** @param progress : progress bar callback.
   **
   ** @return a boolean.
@@ -97,20 +99,20 @@ public:
 	 ** @return a PolicyStatistics instance.
 	 ** @see ObjectStream
 	 ** @see PolicyStatistics
-	 */	
+	 */
   PolicyStatisticsPtr computeStatistics(ObjectStreamPtr crAlgorithms, ProgressCallbackPtr progress = ProgressCallbackPtr())
     {PolicyStatisticsPtr res = new PolicyStatistics(); run(crAlgorithms, res, progress); return res;}
 
-	/**
-	 ** Computes policy statistics and runs the current policy.
-	 **
-	 ** @param crAlgorithms : CRAlgorithm to run.
-	 ** @param progress : progress bar callback.
-	 **
-	 ** @return a boolean.
-	 ** @see ObjectContainer
-	 ** @see PolicyStatistics
-	 */
+  /**
+   ** Computes policy statistics and runs the current policy.
+   **
+   ** @param crAlgorithms : CRAlgorithm to run.
+   ** @param progress : progress bar callback.
+   **
+   ** @return a boolean.
+   ** @see ObjectContainer
+   ** @see PolicyStatistics
+   */
   PolicyStatisticsPtr computeStatistics(ObjectContainerPtr crAlgorithms, ProgressCallbackPtr progress = ProgressCallbackPtr())
     {PolicyStatisticsPtr res = new PolicyStatistics(); run(crAlgorithms, res, progress); return res;}
 
@@ -129,29 +131,28 @@ public:
   ** Policy behaviour when entering into the @a crAlgorithm.
   **
   ** @param crAlgorithm : CRALgorithm.
-  ** @see CRAlgorithm 
+  ** @see CRAlgorithm
   */
   virtual void policyEnter(CRAlgorithmPtr crAlgorithm) {}
 
   /**
+  ** Fonction called when the CRAlgorithm encounters a @a choose keyword.
   **
-  **
-  ** @param choose : choose.
+  ** @param choose : choose encountered.
   **
   ** @return
   */
   virtual VariablePtr policyChoose(ChoosePtr choose) = 0;
 
   /**
+  ** Fonction called when the CRAlgorithm encounters a @a reward keyword.
   **
-  **
-  ** @param reward :  reward value.
+  ** @param reward : reward value.
   */
   virtual void policyReward(double reward) {}
 
   /**
-  ** Policy behaviour when leaving a scope.
-  **
+  ** Policy behaviour when leaving the CRAlgorithm.
   */
   virtual void policyLeave() {}
 
@@ -184,38 +185,63 @@ public:
 };
 
 /**
-** Returns a Policy that does randomly choices.
+** Creates a random Policy.
+**
+** The random policy selects actions randomly. Each action is selected
+** with the same probability.
 **
 ** @return a random Policy instance.
-** @see RandomPolicy 
 */
 extern PolicyPtr randomPolicy();
 
 /**
-** Returns a greedy policy instance.
+** Creates an ActionValue based Policy.
 **
-** @param actionValues : 
+** An ActionValue based policy \f$ \Pi_Q \f$ select actions that maximizes their
+** value according to the action-value \f$ Q \f$:
+** \f[ \forall s \in S, \Pi_Q(s)=\underset{a\in
+** A}{\text{argmax}}\ Q(s,a) \f]
+**
+** @param actionValues : ActionValue function that computes scores
+** of each choice.
 **
 ** @return a greedy policy instance.
-** @see GreedyPolicy 
+** @see ActionValueFunction
 */
 extern PolicyPtr greedyPolicy(ActionValueFunctionPtr actionValues);
 
 /**
-** Returns a Gibbs greedy policy instance. 
+** Creates an ActionValue based policy.
+*
+** This policy is a decorator which adds noise in the Action selection
+** process. Gibbs-greedy sampling only works with policies defined
+** through an ActionValue. Given the action-value \f$ Q \f$, the
+** probability of selecting an action \f$ a \f$ is:
+** \f[ P[a|s]=\frac{exp(Q(s,a)/T)}{Z}\f]
+** where \f$ T \f$ is the temperature parameter and \f$ Z=\sum_{a\in
+** A} exp(Q(s,a)/T) \f$ is the normalization coefficient.
 **
-** @param actionValue
-** @param temperature
+** High temperatures lead to nearly-uniform distributions
+** (exploration) whereas low temperatures lead to distribution that
+** focus on the best values of (exploitation).
 **
-** @return a Gibbs greedy policy instance. 
-** @see GibbsGreedyPolicy 
+** @param actionValue : ActionValue function that computes scores of
+** each choice.
+** @param temperature : the Temperature parameter.
+**
+** @return a Gibbs greedy policy instance.
+** @see ActionValueFunction
 */
 extern PolicyPtr gibbsGreedyPolicy(ActionValueFunctionPtr actionValue, IterationFunctionPtr temperature);
 
 /**
-** Returns a stochastic policy instance.
+** Creates an ActionValue based policy.
 **
-** @param actionProbabilities
+** Computes the selection probabilities:
+** \f[ P(s|a)=Q(s,a) \f]
+**
+** @param actionProbabilities : ActionValue function that computes
+** selection probabilities.
 **
 ** @return a stochastic policy instance.
 ** @see StochasticPolicy
@@ -223,118 +249,65 @@ extern PolicyPtr gibbsGreedyPolicy(ActionValueFunctionPtr actionValue, Iteration
 extern PolicyPtr stochasticPolicy(ActionValueFunctionPtr actionProbabilities);
 
 /**
-** Returns an epsilon-greedy policy instance.
+** Adds uniform noise to the current Policy.
 **
-** @param basePolicy
-** @param epsilon
+** Epsilon-greedy sampling selects this Policy's action with
+** probability 1 - epsilon and selects random Actions otherwise. This
+** kind of sampling is often used in reinforcement-learning in order
+** to balance exploration and exploitation.
 **
-** @return an epsilon-greedy policy instance.
-** @see EpsilonGreedyPolicy
+** @param basePolicy : current Policy.
+** @param epsilon : the probability of selecting actions randomly.
+**
+** @return a new policy decoring this one.
 */
 extern PolicyPtr epsilonGreedyPolicy(PolicyPtr basePolicy, IterationFunctionPtr epsilon);
 
 /**
-** Returns a mixture policy instance.
+** Creates a mixture of two policies.
+*
+** A mixture policy is parameterized by two sub-policies. It selects
+** actions from its first policy with a given probability and actions
+** from its second policy otherwise. Formally, given two policies \f$
+** \pi_1 \f$  and \f$ \pi_2 \f$, and the probability \f$ k \f$, we
+** have: \f[ \forall s \in S,
+** \text{mixture}_{\pi_1,\pi_2,k}(s)=\begin{cases}\pi_1(s)&\text{with
+** probility }1-k\\ \pi_2(s)&\text{with probability }k\end{cases}\f]
 **
 ** @param policy1 : first policy.
 ** @param policy2 : second policy.
 ** @param mixtureCoefficient : probability of selecting @a policy2.
 **
 ** @return a mixture policy instance.
-** @see MixturePolicy 
 */
 extern PolicyPtr mixturePolicy(PolicyPtr policy1, PolicyPtr policy2, double mixtureCoefficient = 0.5);
 
 /**
-** #FIXME
+** Decores @a policy and computes several statistics.
 **
-** @param policy : search policy.
-** @param statistics :
+** @param policy : base policy to decore.
+** @param statistics : statistics container.
 **
-** @return a
-** @see ComputeStatisticsolicy 
+** @return a decored policy instance.
 */
 extern PolicyPtr computeStatisticsPolicy(PolicyPtr policy, PolicyStatisticsPtr statistics);
 
-/**
-** Returns a QLearning policy instance.
-**
-** @param explorationPolicy
-** @param regressor
-** @param discount
-**
-** @return a QLearning policy instance.
-*/
 extern PolicyPtr qlearningPolicy(PolicyPtr explorationPolicy, RegressorPtr regressor, double discount);
 
-/**
-**
-**
-** @param explorationPolicy
-** @param regressor
-** @param discount
-**
-** @return
-*/
 extern PolicyPtr sarsaZeroPolicy(PolicyPtr explorationPolicy, RegressorPtr regressor, double discount);
 
-/**
-**
-**
-** @param explorationPolicy
-** @param regressor
-** @param discount
-**
-** @return
-*/
 extern PolicyPtr monteCarloControlPolicy(PolicyPtr explorationPolicy, RegressorPtr regressor, double discount);
 
-/**
-**
-**
-** @param explorationPolicy
-** @param classifier
-** @param supervisor
-**
-** @return
-*/
 extern PolicyPtr classificationExamplesCreatorPolicy(PolicyPtr explorationPolicy,
                         ClassifierPtr classifier,
                         ActionValueFunctionPtr supervisor = ActionValueFunctionPtr());
 
-/**
-**
-**
-** @param explorationPolicy
-** @param ranker
-** @param supervisor
-**
-** @return
-*/
 extern PolicyPtr rankingExamplesCreatorPolicy(PolicyPtr explorationPolicy,
                         RankerPtr ranker,
                         ActionValueFunctionPtr supervisor = ActionValueFunctionPtr());
 
-/**
-**
-**
-** @param classifier
-** @param beta
-** @param exploration
-**
-** @return
-*/
 extern PolicyPtr gpomdpPolicy(GeneralizedClassifierPtr classifier, double beta, double exploration = 1.0);
 
-/**
-**
-**
-** @param classifier
-** @param beta
-** @param explorationPolicy
-**
-** @return
-*/
 extern PolicyPtr gpomdpPolicy(GeneralizedClassifierPtr classifier, double beta, PolicyPtr explorationPolicy);
 
 /*!
@@ -344,64 +317,26 @@ extern PolicyPtr gpomdpPolicy(GeneralizedClassifierPtr classifier, double beta, 
 class DecoratorPolicy : public Policy
 {
 public:
-  /**
-  **
-  **
-  ** @param decorated
-  **
-  ** @return
-  */
+
   DecoratorPolicy(PolicyPtr decorated = PolicyPtr())
     : decorated(decorated) {}
 
-  /**
-  **
-  **
-  ** @param crAlgorithm
-  */
   virtual void policyEnter(CRAlgorithmPtr crAlgorithm)
     {decorated->policyEnter(crAlgorithm);}
 
-  /**
-  **
-  **
-  ** @param choose
-  **
-  ** @return
-  */
   virtual VariablePtr policyChoose(ChoosePtr choose)
     {return decorated->policyChoose(choose);}
 
-  /**
-  **
-  **
-  ** @param reward
-  */
+
   virtual void policyReward(double reward)
     {decorated->policyReward(reward);}
 
-  /**
-  **
-  **
-  */
   virtual void policyLeave()
     {decorated->policyLeave();}
 
-  /**
-  **
-  **
-  ** @param ostr
-  */
   virtual void save(std::ostream& ostr) const
     {write(ostr, decorated);}
 
-  /**
-  **
-  **
-  ** @param istr
-  **
-  ** @return
-  */
   virtual bool load(std::istream& istr)
     {return read(istr, decorated);}
 
@@ -455,21 +390,9 @@ public:
     decorated->policyLeave();
   }
 
-  /**
-  **
-  **
-  ** @param ostr
-  */
   virtual void save(std::ostream& ostr) const
     {write(ostr, decorated);}
 
-  /**
-  **
-  **
-  ** @param istr
-  **
-  ** @return
-  */
   virtual bool load(std::istream& istr)
     {return read(istr, decorated);}
 
