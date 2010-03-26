47940a2721ac87667719c2ade5a1aa5e360027a3
francis <francis@885cb74b-98ed-4c74-c960-63734602d729>
2010-03-26 12:23:18 +0000

    '''New files in Proteins and compil win32'''

--- projects/Proteins/Main.cpp
+++ projects/Proteins/Main.cpp
@@ -1,4 +1,8 @@
 #include <lbcpp/lbcpp.h>
+#include "Data/Bio/AminoAcidSequence.lh"
+#include "Data/Bio/PositionSpecificScoringMatrix.lh"
+#include "Data/Bio/SecondaryStructureSequence.lh"
+#include "Data/Bio/SolventAccesibilitySequence.lh"
 using namespace lbcpp;
 
 class NamedObjectMap : public Object
@@ -11,97 +15,6 @@ private:
   ObjectsMap objects;
 };
 
-class AminoAcid : public Object
-{
-public:
-  enum Type
-  {
-    bla,
-    fixme,
-    unknown
-  };
-  
-  AminoAcid(const AminoAcid& aa)
-    : t(aa.t) {}
-  AminoAcid(const Type& type = unknown)
-    : t(type) {}
-  
-  // TODO: properties: one-letter code, three-letter code, hydrophoby, polarity, side chain, ....
-  
-  bool operator ==(const AminoAcid& aa) const
-    {return t == aa.t;}
-    
-  bool operator !=(const AminoAcid& aa) const
-    {return t != aa.t;}
-    
-  operator char() const
-    {return (char)t;}
-
-private:
-  Type t;
-};
-
-class SequenceObject : public Object
-{
-public:
-  virtual size_t getLength() const = 0;
-  virtual ObjectPtr getObject(size_t position) const = 0;
-};
-
-class AminoAcidSequence : public SequenceObject
-{
-public:
-  virtual std::string getName() const
-    {return name;}
-
-  virtual std::string toString() const // FASTA format
-  {
-    std::string res = "> " + name + "\n";
-    for (size_t i = 0; i < sequence.size(); ++i)
-      res += sequence[i];
-    res += "\n";
-    return res;
-  }
-  
-  virtual size_t getLength() const
-    {return sequence.size();}
-  
-  virtual ObjectPtr getObject(size_t position) const
-    {return ObjectPtr(new AminoAcid(getAminoAcid(position)));}
-  
-  AminoAcid getAminoAcid(size_t position) const
-    {assert(position < sequence.size()); return AminoAcid((AminoAcid::Type)sequence[position]);}
-    
-  void setAminoAcid(size_t position, const AminoAcid& aa)
-    {assert(position < sequence.size()); sequence[position] = (char)aa;}
-
-protected:
-  virtual bool load(std::istream& istr)
-  {
-    return lbcpp::read(istr, name) && 
-           lbcpp::read(istr, sequence);
-  }
-  
-  virtual void save(std::ostream& ostr) const
-  {
-    lbcpp::write(ostr, name);
-    lbcpp::write(ostr, sequence);
-  }
-
-private:
-  std::string name;
-  std::vector<char> sequence;
-};
-
-class SecondaryStructureSequence : public SequenceObject
-{
-public:
-  virtual size_t getLength() const
-    {return 0;}
-  
-  virtual ObjectPtr getObject(size_t position) const
-    {return ObjectPtr();}
-};
 
 // Data: 
 
@@ -110,7 +23,7 @@ public:
 // VectorSequence: PositionSpecificScoringMatrix
 // LabelSequence: 3 state and 8 state SecondaryStructureSequence
 // LabelSequence: 2 state SolventAccesibilitySequence
-// ScalarSequence: regression SolventAccesibilitySequence
+// ScoreSequence: regression SolventAccesibilitySequence
 // ScalarMatrix: 2 state ResidueContactMap
 // ScalarMatrix: regression ResidueDistanceMap
 // VectorSequence: BackboneSequence
@@ -118,5 +31,6 @@ public:
 
 int main()
 {
+  // LBCPP_DECLARE_CLASS
   return 0;
 }
