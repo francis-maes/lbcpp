080fbd0be2575129858fe7ba1ec110f90789af79
francis <francis@885cb74b-98ed-4c74-c960-63734602d729>
2010-04-08 19:31:58 +0000

    Move to Inference Steps

---
+++ projects/Proteins/InferenceStep/ParallelInferenceStep.h
@@ -0,0 +1,111 @@
+/*-----------------------------------------.---------------------------------.
+| Filename: SharedParallelInferenceStep.h  | Reduction from a problem        |
+| Author  : Francis Maes                   |   to a simpler problem          |
+| Started : 08/04/2010 18:18               |                                 |
+`------------------------------------------/                                 |
+                               |                                             |
+                               `--------------------------------------------*/
+
+#ifndef LBCPP_REDUCTION_PREDICTION_PROBLEM_H_
+# define LBCPP_REDUCTION_PREDICTION_PROBLEM_H_
+
+# include "InferenceStep.h"
+
+namespace lbcpp
+{
+
+// Input: FeatureGenerator
+// Output: FeatureVector
+class ClassificationInferenceStep : public InferenceStep
+{
+public:
+  ClassificationInferenceStep(const String& name)
+    : InferenceStep(name) {}
+
+  virtual ResultCode run(InferencePolicyPtr policy, ObjectPtr input, ObjectPtr& output) 
+    {return policy->doClassification(classifier, input.dynamicCast<FeatureGenerator>(), *(FeatureVectorPtr* )&output);}
+
+protected:
+  ClassifierPtr classifier;
+};
+
+
+// Input: ObjectContainer
+// Output: inherited from ObjectContainer
+class SharedParallelInferenceStepBase : public InferenceStep
+{
+public:
+  SharedParallelInferenceStepBase(const String& name, InferenceStepPtr subInference, const String& outputClassName)
+    : InferenceStep(name), subInference(subInference), outputClassName(outputClassName) {}
+
+  virtual ResultCode run(InferencePolicyPtr policy, ObjectPtr input, ObjectPtr& output)
+  {
+    ObjectContainerPtr inputContainer = input.dynamicCast<ObjectContainer>();
+    std::vector< std::pair<InferenceStepPtr, ObjectPtr> > subInferences(inputContainer->size());
+    for (size_t i = 0; i < subInferences.size(); ++i)
+      subInferences[i] = std::make_pair(subInference, inputContainer->get(i));
+    ObjectContainerPtr outputContainer = Object::createAndCast<ObjectContainer>(outputClassName);
+    outputContainer->resize(inputContainer->size());
+    return policy->doParallelSteps(subInferences, outputContainer);
+  }
+
+protected:
+  String outputClassName;
+  InferenceStepPtr subInference;
+};
+
+class SharedParallelInferenceStep : public SharedParallelInferenceStepBase
+{
+public:
+  SharedParallelInferenceStep(const String& name, InferenceStepPtr subInference, const String& outputClassName)
+    : SharedParallelInferenceStepBase(name, subInference, outputClassName) {}
+
+  virtual size_t getNumSubObjects(ObjectPtr input, ObjectPtr output) const = 0;
+  virtual ObjectPtr getSubInput(ObjectPtr input, size_t index) const = 0;
+  virtual ObjectPtr getSubOutput(ObjectPtr output, size_t index) const = 0;
+
+  struct InputContainer : public ObjectContainer
+  {
+    InputContainer(ObjectPtr input, size_t numSubInputs, SharedParallelInferenceStep* pthis)
+      : input(input), numSubInputs(numSubInputs), pthis(pthis) {}
+
+    ObjectPtr input;
+    size_t numSubInputs;
+    SharedParallelInferenceStep* pthis;
+
+    virtual size_t size() const
+      {return numSubInputs;}
+
+    virtual ObjectPtr get(size_t index) const
+      {return pthis->getSubInput(input, index);}
+  };
+
+  struct OutputContainer : public ObjectContainer
+  {
+    OutputContainer(ObjectPtr output, size_t numSubOutput, SharedParallelInferenceStep* pthis)
+      : output(output), numSubOutput(numSubOutput), pthis(pthis) {}
+
+    ObjectPtr output;
+    size_t numSubOutput;
+    SharedParallelInferenceStep* pthis;
+
+    virtual size_t size() const
+      {return output;}
+
+    virtual ObjectPtr get(size_t index) const
+      {return pthis->getSubOutput(output, index);}
+  };
+
+  virtual ResultCode run(InferencePolicyPtr policy, ObjectPtr input, ObjectPtr& output)
+  {
+    size_t numObjects = getNumSubObjects(input, output);
+    if (output)
+      output = new OutputContainer(output, numObjects, this);
+    input = new InputContainer(input, numObjects, this);
+    return SharedParallelInferenceStepBase::run(policy, input, output);
+  }
+};
+
+}; /* namespace lbcpp */
+
+#endif //!LBCPP_REDUCTION_PREDICTION_PROBLEM_H_
---
+++ projects/Proteins/InferenceStep/SequenceInferenceStep.h
@@ -0,0 +1,68 @@
+/*-----------------------------------------.---------------------------------.
+| Filename: CompositeInferenceStep.h   | Base class for composite        |
+| Author  : Francis Maes                   |   prediction problems           |
+| Started : 08/04/2010 18:19               |                                 |
+`------------------------------------------/                                 |
+                               |                                             |
+                               `--------------------------------------------*/
+
+#ifndef LBCPP_COMPOSITE_PREDICTION_PROBLEM_H_
+# define LBCPP_COMPOSITE_PREDICTION_PROBLEM_H_
+
+# include "InferenceStep.h"
+
+namespace lbcpp
+{
+
+class CompositeInferenceStep : public InferenceStep
+{
+public:
+  CompositeInferenceStep(const String& name) : InferenceStep(name) {}
+/*
+  virtual size_t getNumSubProblems() const = 0;
+  virtual InferenceStepPtr getSubProblem(size_t index) const = 0;
+
+protected:
+  virtual bool isModelUpToDate(const File& model, const Time& lastDataModificationTime) const
+    {return model.exists() && model.isDirectory() && model.getLastModificationTime() >= lastDataModificationTime;}
+
+  virtual ObjectFunctionPtr loadPredictor(const File& model) const
+  {
+    if (!model.exists() || !model.isDirectory())
+    {
+      Object::error(T("loadPredictor"), model.getFullPathName() + T(" is not a directory"));
+      return ObjectFunctionPtr();
+    }
+  
+    // todo: load childs
+    // create CompositePredictor
+    return ObjectFunctionPtr();          
+  }
+
+  virtual void savePredictor(ObjectFunctionPtr predictor, const File& model) const
+  {
+    bool ok = model.createDirectory();
+    if (!ok)
+    {
+      Object::error(T("savePredictor"), T("Could not create directory ") + model.getFullPathName());
+      return;
+    }
+
+    // todo: save childs
+  }
+  
+  virtual ObjectFunctionPtr updatePredictor(const File& model, ObjectContainerPtr trainingData) const
+  {
+    return ObjectFunctionPtr();
+  }*/
+
+  void appendSubStep(InferenceStepPtr subStep)
+    {subSteps.push_back(subStep);}
+
+protected:
+  std::vector<InferenceStepPtr> subSteps;
+};
+
+}; /* namespace lbcpp */
+
+#endif //!LBCPP_COMPOSITE_PREDICTION_PROBLEM_H_
