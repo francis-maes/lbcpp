9e3c45b65c17e9fff025f6edf3ab3b7a85469b10
julien <julien@885cb74b-98ed-4c74-c960-63734602d729>
2010-04-14 17:49:36 +0000

    Unstable version, it's a test to cluster

--- projects/Proteins/Programs/ArgumentSet.h
+++ projects/Proteins/Programs/ArgumentSet.h
@@ -227,7 +227,7 @@ std::ostream& operator<<(std::ostream& o, ArgumentSet args)
 {
   for (size_t i = 0; i < args.arguments.size(); ++i)
   {
-    o << "    > " << args.arguments[i]->name.toUTF8();
+    o << "|> " << args.arguments[i]->name.toUTF8();
     o << ": " << args.arguments[i]->getStringValue().toUTF8() << std::endl;
   }
   
