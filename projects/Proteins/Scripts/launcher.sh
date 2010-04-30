0fc94b5024e3380158dd6646ae18c64a9a939be4
julien <julien@885cb74b-98ed-4c74-c960-63734602d729>
2010-04-30 13:30:31 +0000

    From NIC3

--- projects/Proteins/Scripts/launcher.sh
+++ projects/Proteins/Scripts/launcher.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
 function launch {
-	name="${prefix}_${target}_${nbPass}_${nbIteration}_${wAA}_${wPSSM}_${wProp}_${wSS3}_${wSS8}_${wSA}_${regularizer}_${initialLearningRate}_${numberIterationLearningRate}"
+  targetName=`echo $target | sed 's/(/./g'`
+  targetName=`echo $targetName | sed 's/)/./g'`
+	name="${prefix}${targetName}_${nbPass}_${nbIteration}_${wAA}_${wPSSM}_${wProp}_${wSS3}_${wSS8}_${wSA}_${regularizer}_${initialLearningRate}_${numberIterationLearningRate}"
 
 	expected_time=`expr $nbPass + 1`
-	#expected_time=`expr $expected_time '*' 3`
+	expected_time=`expr $expected_time '*' 2`
 	
-qsub << EOF
+cat << EOF
 #$ -l h_vmem=2G 
 #$ -l h_rt=${expected_time}:00:00
 
@@ -16,12 +18,12 @@ qsub << EOF
 #$ -cwd
 
 #$ -N ${name}
-./TrainStepByStepSS ProteinsDirectory /scratch/jbecker/CB513 PrefixResults ${name} CurrentFold 0 PredictionTarget ${target} MaximumIteration ${nbIteration} MaximumPass ${nbPass} AminoAcidWindow ${wAA} PSSMWindow ${wPSSM} PropertyWindow ${wProp} SS3Window ${wSS3} SS8Window ${wSS8} SAWindow ${wSA} Regularizer ${regularizer} InitialLearningRate ${initialLearningRate} NumberIterationLearningRate ${numberIterationLearningRate} UseSavedModel ${useSavedModel} SavedModelDirectory ${savedModelDirectory}
+./TrainStepByStepSS ProteinsDirectory /scratch/jbecker/${database} PrefixResults ${name} CurrentFold 0 PredictionTargets "${target}" MaximumIteration ${nbIteration} AminoAcidWindow ${wAA} PSSMWindow ${wPSSM} NumberOfAAIndex ${aaindex} PropertyWindow ${wProp} SS3Window ${wSS3} SS8Window ${wSS8} SAWindow ${wSA} Regularizer ${regularizer} InitialLearningRate ${initialLearningRate} NumberIterationLearningRate ${numberIterationLearningRate} UseSavedModel ${useSavedModel} SavedModelDirectory ${savedModelDirectory} ${other}
 EOF
 
 }
 
-target="SS3-SS8"
+target="(DR)10"
 nbIteration="5"
 nbPass="10"
 
@@ -30,24 +32,35 @@ wPSSM="11"
 wSS3="10"
 wSS8="10"
 wSA="10"
-wProp="0"
+wProp="50"
+aaindex="0"
 
 regularizer="10"
 initialLearningRate="2"
 numberIterationLearningRate="250000"
 
-useSavedModel="false"
-savedModelDirectory="MODEL_SS3-SS8_10_5_9_11_10_10_10_10_2_250000/decorated.inference"
+useSavedModel="true"
+savedModelDirectory="DR_PROB"
 
-prefix="TRUEAA"
+prefix=""
+database="SmallPDB/last_version"
 
-
-#for target in SS3 SS8 SA SS3-SS8 SS3-SA SS8-SA SS3-SS8-SA
+#for target in SA SS3-SA SS8-SA SS3-SS8-SA
+#for target in SS3 SS8 SA DR BBB SS3-SS8 SS3-SA SS3-DR SS3-BBB SS8-SA SS8-DR SS8-BBB SA-DR SA-BBB DR-BBB SS3-SS8-SA SS3-SS8-DR SS3-SS8-BBB SS3-SA-DR SS3-SA-BBB SS3-DR-BBB SS8-SA-DR SS8-SA-BBB SS8-DR-BBB SA-DR-BBB SS3-SS8-SA-DR SS3-SS8-SA-BBB SS3-SS8-DR-BBB SS3-SA-DR-BBB
+#for i in SegCon Pos Len SegCon-Pos SegCon-Len Pos-Len SegCon-Pos-Len
 #do
-#	launch
+#	prefix="NEW_FEATURE_${i}"
+#	savedModelDirectory="${prefix}_${target}"
+#	other="TestFeatures ${i}"
+#  launch
 #done
 
-for wProp in 2 5 10 20 50 80 100 150
+for drProb in 0.05 0.1 0.15 0.2 0.25 0.30 0.35 0.40 0.45 0.5 0.65 0.70 0.75 0.80 0.85 0.90 0.95 1
 do
+#	prefix="AAI_${aaindex}"
+#	savedModelDirectory="${prefix}_${target}"
+  prefix="DRPROB_$drProb"
+  savedModelDirectory="$prefix"
+  other="DRProbability $drProb"
 	launch
 done
