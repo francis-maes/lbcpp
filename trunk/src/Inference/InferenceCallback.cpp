6e370493e38b37d7a6589839235417789d74ad5a
francis <francis@885cb74b-98ed-4c74-c960-63734602d729>
2010-05-05 14:00:22 +0000

    Locally randomized learner

--- trunk/src/Inference/InferenceCallback.cpp
+++ trunk/src/Inference/InferenceCallback.cpp
@@ -13,6 +13,35 @@
 #include "InferenceCallback/StochasticScalarLinearInferenceLearningCallback.h"
 using namespace lbcpp;
 
+/*
+** ScalarInferenceLearningCallback
+*/
+ScalarInferenceLearningCallback::ScalarInferenceLearningCallback(LearnableAtomicInferencePtr step)
+    : step(step), epoch(1) {}
+
+void ScalarInferenceLearningCallback::postInferenceCallback(InferenceStackPtr stack, ObjectPtr input, ObjectPtr supervision, ObjectPtr& output, ReturnCode& returnCode)
+{
+  if (step == stack->getCurrentInference() && supervision && output)
+  {
+    FeatureGeneratorPtr features = input.dynamicCast<FeatureGenerator>();
+    ScalarFunctionPtr loss = supervision.dynamicCast<ScalarFunction>();
+    ScalarPtr prediction = output.dynamicCast<Scalar>();
+    jassert(features && loss && prediction);
+    epoch += learningEpoch(epoch, features, prediction->getValue(), loss);
+  }
+}
+
+void ScalarInferenceLearningCallback::updateInputSize(FeatureGeneratorPtr inputfeatures)
+{
+  // computing the l1norm() may be long, so we make more and more sparse sampling of this quantity
+  if (inputSize.getCount() < 10 ||                         // every time until having 10 samples
+      (inputSize.getCount() < 100 && (epoch % 10 == 0)) || // every 10 epochs until having 100 samples
+      (epoch % 100 == 0))                                  // every 100 epochs after that
+    inputSize.push((double)(inputfeatures->l1norm()));
+}
+
+// -
+
 InferenceCallbackPtr lbcpp::cacheInferenceCallback(InferenceResultCachePtr cache, InferencePtr parentStep)
   {return new CacheInferenceCallback(cache, parentStep);}
 
@@ -21,10 +50,11 @@ InferenceCallbackPtr lbcpp::cancelAfterStepCallback(InferencePtr lastStepBeforeB
 
 InferenceCallbackPtr lbcpp::stochasticScalarLinearInferenceLearningCallback(InferencePtr inference,
                                                                             IterationFunctionPtr learningRate,
-                                                                            ScalarFunctionPtr regularizer,
+                                                                            ScalarVectorFunctionPtr regularizer,
                                                                             bool normalizeLearningRate)
   {return new StochasticScalarLinearInferenceLearningCallback(inference.dynamicCast<LearnableAtomicInference>(),
                   learningRate, regularizer, normalizeLearningRate);}
+
 
 void declareInferenceCallbackClasses()
 {
--- trunk/src/Inference/InferenceCallback/StochasticScalarLinearInferenceLearningCallback.h
+++ trunk/src/Inference/InferenceCallback/StochasticScalarLinearInferenceLearningCallback.h
@@ -15,69 +15,33 @@
 namespace lbcpp
 {
 
-class ScalarInferenceLearningCallback : public InferenceCallback
-{
-public:
-  ScalarInferenceLearningCallback(LearnableAtomicInferencePtr step)
-    : step(step), epoch(1) {}
-
-  // epoch starts at 1
-  virtual bool learningEpoch(size_t epoch, FeatureGeneratorPtr features, double prediction, ScalarFunctionPtr loss) = 0;
-
-  virtual void postInferenceCallback(InferenceStackPtr stack, ObjectPtr input, ObjectPtr supervision, ObjectPtr& output, ReturnCode& returnCode)
-  {
-    if (step == stack->getCurrentInference() && supervision && output)
-    {
-      FeatureGeneratorPtr features = input.dynamicCast<FeatureGenerator>();
-      ScalarFunctionPtr loss = supervision.dynamicCast<ScalarFunction>();
-      ScalarPtr prediction = output.dynamicCast<Scalar>();
-      jassert(features && loss && prediction);
-      if (learningEpoch(epoch, features, prediction->getValue(), loss))
-        ++epoch;
-    }
-  }
-
-protected:
-  LearnableAtomicInferencePtr step;
-  size_t epoch;
-};
-
 class StochasticScalarLinearInferenceLearningCallback : public ScalarInferenceLearningCallback
 {
 public:
-  StochasticScalarLinearInferenceLearningCallback(LearnableAtomicInferencePtr step, IterationFunctionPtr learningRate, ScalarFunctionPtr regularizer = ScalarFunctionPtr(), bool normalizeLearningRate = true)
+  StochasticScalarLinearInferenceLearningCallback(LearnableAtomicInferencePtr step, IterationFunctionPtr learningRate, ScalarVectorFunctionPtr regularizer = ScalarVectorFunctionPtr(), bool normalizeLearningRate = true)
     : ScalarInferenceLearningCallback(step), learningRate(learningRate), regularizer(regularizer), normalizeLearningRate(normalizeLearningRate) {}
 
-  virtual bool learningEpoch(size_t epoch, FeatureGeneratorPtr features, double prediction, ScalarFunctionPtr exampleLoss)
+  virtual size_t learningEpoch(size_t epoch, FeatureGeneratorPtr features, double prediction, ScalarFunctionPtr exampleLoss)
   {
-    // computing the l1norm() may be long, so we make more and more sparse sampling of this quantity
-    if (inputSize.getCount() < 10 ||                         // every time until having 10 samples
-        (inputSize.getCount() < 100 && (epoch % 10 == 0)) || // every 10 epochs until having 100 samples
-        (epoch % 100 == 0))                                  // every 100 epochs after that
-    {
-      inputSize.push((double)(features->l1norm()));
-      //std::cout << "Alpha: " << weight * computeAlpha() << " inputSize: " << inputSize.toString() << std::endl;
-    }
-
-    if (exampleLoss->compute(1.0) > exampleLoss->compute(-1.0) && RandomGenerator::getInstance().sampleBool(0.95))
-      return true; // reject 95% of negative examples
+    updateInputSize(features);
 
     //std::cout << "Loss: " << exampleLoss->toString() << " Derivative: " << exampleLoss->computeDerivative(prediction) << " PRediction = " << prediction << std::endl;
-    double k = computeAlpha() * exampleLoss->computeDerivative(prediction);
 
-    features->addWeightedTo(step->getParameters(), - k);
-    step->validateParametersChange();
+    const size_t regularizerFrequency = 100;
 
-    //if (epoch % 1000 == 0)
-    //  std::cout << step->getParameters()->toString() << std::endl;
+    DenseVectorPtr parameters = step->getParameters();
 
-/*    size_t regularizerFrequency = 100;
-    if ((epoch % regularizerFrequency) == 0)
+    double alpha = computeAlpha();
+    features->addWeightedTo(parameters, - alpha * exampleLoss->computeDerivative(prediction));
+    if (regularizer && (epoch % regularizerFrequency) == 0)
     {
-      // todo: apply regularizer
-    }*/
-    return true;
+      FeatureGeneratorPtr regularizerGradient = regularizer->computeGradient(parameters);
+      regularizerGradient->addWeightedTo(parameters, - alpha * (double)regularizerFrequency);
+    }
+    step->validateParametersChange();
+    return 1;
   }
+
   virtual void finishInferencesCallback()
   {
     std::cout << "Epoch " << epoch << ", " << step->getParameters()->l0norm() << " parameters, L2 = " << step->getParameters()->l2norm() << std::endl;
@@ -85,7 +49,7 @@ public:
 
 protected:
   IterationFunctionPtr learningRate;
-  ScalarFunctionPtr regularizer;
+  ScalarVectorFunctionPtr regularizer;
   bool normalizeLearningRate;
   ScalarVariableMean inputSize;
 
