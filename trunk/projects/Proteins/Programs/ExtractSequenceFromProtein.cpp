754a2dbe50b758e2305c9101aa2b206e27e9964f
julien <julien@885cb74b-98ed-4c74-c960-63734602d729>
2010-05-11 12:23:21 +0000

    New program: ExtractSequenceFromProtein - Create files XXXX.seq containing the sequence of proteins XXXX.protein (useful for launch a PSI-BLAST)

---
+++ trunk/projects/Proteins/Programs/ExtractSequenceFromProtein.cpp
@@ -0,0 +1,44 @@
+
+#include <lbcpp/lbcpp.h>
+#include "../Programs/ArgumentSet.h"
+#include "Protein/Protein.h"
+
+using namespace lbcpp;
+
+extern void declareProteinClasses();
+
+int main(int argc, char** argv)
+{
+  declareProteinClasses();
+  
+  File proteinDirectory;
+  File outputDirectory;
+  
+  ArgumentSet arguments;
+  arguments.insert(new FileArgument(T("proteinDirectory"), proteinDirectory, true, true), true);
+  arguments.insert(new FileArgument(T("outputDirectory"), outputDirectory, true, true), true);
+  
+  if (!arguments.parse(argv, 1, argc-1)) {
+    std::cout << "Usage: " << argv[0] << " " << arguments.toString() << std::endl;
+    return 1;
+  }
+  
+  juce::OwnedArray<File> proteinFiles;
+  proteinDirectory.findChildFiles(proteinFiles, File::findFiles, false, T("*.protein"));
+  
+  for (size_t i = 0; i < (size_t)proteinFiles.size(); ++i)
+  {
+    ProteinPtr protein = Protein::createFromFile(*proteinFiles[i]);
+    File outputFile = outputDirectory.getChildFile(proteinFiles[i]->getFileNameWithoutExtension() + T(".seq"));
+    
+    std::cout << "Extracting " << proteinFiles[i]->getFileNameWithoutExtension() << " ... ";
+    
+    OutputStream* o = outputFile.createOutputStream();
+    *o << protein->getAminoAcidSequence()->toString();
+    delete o;
+    
+    std::cout << "OK" << std::endl;
+  }
+  
+  return 0;
+}
