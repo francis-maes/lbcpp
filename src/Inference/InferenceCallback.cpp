0c3c9c0fcb795b9c39b9093b4317ec794f28a257
francis <francis@885cb74b-98ed-4c74-c960-63734602d729>
2010-05-25 16:37:01 +0000

    New gradient descent inference callback classes

--- src/Inference/InferenceCallback.cpp
+++ src/Inference/InferenceCallback.cpp
@@ -10,40 +10,9 @@
 #include "InferenceCallback/CancelAfterStepCallback.h"
 #include "InferenceCallback/ExamplesCreatorCallback.h"
 #include "InferenceCallback/CacheInferenceCallback.h"
-#include "InferenceCallback/StochasticScalarLinearInferenceLearningCallback.h"
+#include "InferenceCallback/PerStepGradientDescentLearningCallback.h"
+#include "InferenceCallback/RandomizedPerStepGradientDescentLearningCallback.h"
 using namespace lbcpp;
-
-/*
-** ScalarInferenceLearningCallback
-*/
-ScalarInferenceLearningCallback::ScalarInferenceLearningCallback(LearnableAtomicInferencePtr step)
-    : step(step), epoch(1) {}
-
-void ScalarInferenceLearningCallback::postInferenceCallback(InferenceStackPtr stack, ObjectPtr input, ObjectPtr supervision, ObjectPtr& output, ReturnCode& returnCode)
-{
-  if (step == stack->getCurrentInference() && supervision && output)
-  {
-    currentParentStep = stack->getParentInference();
-    FeatureGeneratorPtr features = input.dynamicCast<FeatureGenerator>();
-    ScalarFunctionPtr loss = supervision.dynamicCast<ScalarFunction>();
-    ScalarPtr prediction = output.dynamicCast<Scalar>();
-    jassert(features && loss && prediction);
-    epoch += postInferenceCallback(epoch, features, prediction->getValue(), loss);
-  }
-  else if (stack->getCurrentInference() == currentParentStep)
-    epoch += postEpisodeCallback();
-}
-
-void ScalarInferenceLearningCallback::updateInputSize(FeatureGeneratorPtr inputfeatures)
-{
-  // computing the l1norm() may be long, so we make more and more sparse sampling of this quantity
-  if (inputSize.getCount() < 10 ||                         // every time until having 10 samples
-      (inputSize.getCount() < 100 && (epoch % 10 == 0)) || // every 10 epochs until having 100 samples
-      (epoch % 100 == 0))                                  // every 100 epochs after that
-    inputSize.push((double)(inputfeatures->l1norm()));
-}
-
-// -
 
 InferenceCallbackPtr lbcpp::cacheInferenceCallback(InferenceResultCachePtr cache, InferencePtr parentStep)
   {return new CacheInferenceCallback(cache, parentStep);}
@@ -51,13 +20,43 @@ InferenceCallbackPtr lbcpp::cacheInferenceCallback(InferenceResultCachePtr cache
 InferenceCallbackPtr lbcpp::cancelAfterStepCallback(InferencePtr lastStepBeforeBreak)
   {return new CancelAfterStepCallback(lastStepBeforeBreak);}
 
-InferenceCallbackPtr lbcpp::stochasticScalarLinearInferenceLearningCallback(InferencePtr inference,
-                                                                            IterationFunctionPtr learningRate,
-                                                                            ScalarVectorFunctionPtr regularizer,
-                                                                            bool normalizeLearningRate)
-  {return new StochasticScalarLinearInferenceLearningCallback(inference.dynamicCast<LearnableAtomicInference>(),
-                  learningRate, regularizer, normalizeLearningRate);}
 
+/*
+** LearningInferenceCallback
+*/
+void LearningInferenceCallback::finishInferencesCallback()
+  {passFinishedCallback();}
+
+void LearningInferenceCallback::postInferenceCallback(InferenceStackPtr stack, ObjectPtr input, ObjectPtr supervision, ObjectPtr& output, ReturnCode& returnCode)
+{
+  if (inference == stack->getCurrentInference() && supervision && output)
+  {
+    currentParentStep = stack->getParentInference();
+    stepFinishedCallback(input, supervision, output);
+  }
+  else if (stack->getCurrentInference() == currentParentStep)
+    episodeFinishedCallback();
+}
+
+LearningInferenceCallbackPtr lbcpp::stochasticDescentLearningCallback(InferencePtr inference, 
+                                                            LearningInferenceCallback::UpdateFrequency learningUpdateFrequency,
+                                                            IterationFunctionPtr learningRate,
+                                                            bool normalizeLearningRate,
+                                                            LearningInferenceCallback::UpdateFrequency randomizationFrequency,
+                                                            LearningInferenceCallback::UpdateFrequency regularizerUpdateFrequency,
+                                                            ScalarVectorFunctionPtr regularizer)
+{
+  bool hasRandomization = (randomizationFrequency != LearningInferenceCallback::never && randomizationFrequency != LearningInferenceCallback::perStep);
+  if (learningUpdateFrequency == LearningInferenceCallback::perStep)
+  {
+    if (hasRandomization)
+      return new RandomizedPerStepGradientDescentLearningCallback(inference, learningRate, normalizeLearningRate, randomizationFrequency, regularizerUpdateFrequency, regularizer);
+    else
+      return new PerStepGradientDescentLearningCallback(inference, learningRate, normalizeLearningRate, regularizerUpdateFrequency, regularizer);
+  }
+  jassert(false); // Missing implementation
+  return InferenceCallbackPtr();
+}
 
 void declareInferenceCallbackClasses()
 {
---
+++ src/Inference/InferenceCallback/GradientDescentLearningCallback.h
@@ -0,0 +1,69 @@
+/*-----------------------------------------.---------------------------------.
+| Filename: GradientDescentLearningCallback.h| Base class for gradient       |
+| Author  : Francis Maes                   |  descent learners               |
+| Started : 25/05/2010 18:58               |                                 |
+`------------------------------------------/                                 |
+                               |                                             |
+                               `--------------------------------------------*/
+
+#ifndef LBCPP_INFERENCE_CALLBACK_GRADIENT_DESCENT_LEARNING_H_
+# define LBCPP_INFERENCE_CALLBACK_GRADIENT_DESCENT_LEARNING_H_
+
+# include <lbcpp/Inference/InferenceCallback.h>
+# include <lbcpp/Inference/InferenceBaseClasses.h>
+
+namespace lbcpp
+{
+
+class IterativeLearningInferenceCallback : public LearningInferenceCallback
+{
+public:
+  IterativeLearningInferenceCallback(InferencePtr inference, IterationFunctionPtr learningRate, bool normalizeLearningRate);
+
+protected:
+  IterationFunctionPtr learningRate;
+  bool normalizeLearningRate;
+  ScalarVariableMean numberOfActiveFeatures;
+  size_t epoch;
+ 
+  double computeLearningRate() const;
+  void updateNumberOfActiveFeatures(FeatureGeneratorPtr features);
+};
+
+class GradientDescentLearningCallback : public IterativeLearningInferenceCallback
+{
+public:
+  GradientDescentLearningCallback(LearnableAtomicInferencePtr inference,
+                                  UpdateFrequency learningUpdateFrequency,
+                                  IterationFunctionPtr learningRate, bool normalizeLearningRate, 
+                                  UpdateFrequency randomizationFrequency,
+                                  UpdateFrequency regularizerUpdateFrequency, ScalarVectorFunctionPtr regularizer);
+
+  virtual void episodeFinishedCallback();
+  virtual void passFinishedCallback();
+  
+  LearnableAtomicInferencePtr getInference() const
+    {return inference.staticCast<LearnableAtomicInference>();}
+
+  DenseVectorPtr getParameters() const
+    {return getInference()->getParameters();}
+
+protected:
+  UpdateFrequency learningUpdateFrequency;
+  UpdateFrequency randomizationFrequency;
+  UpdateFrequency regularizerUpdateFrequency;
+  ScalarVectorFunctionPtr regularizer;
+  ScalarVariableMean lossValue;
+
+  FeatureGeneratorPtr getExampleGradient(ObjectPtr input, ObjectPtr supervision, ObjectPtr predictedOutput);
+  bool shouldApplyRegularizerAfterStep(size_t epoch) const;
+  void applyExample(ObjectPtr input, ObjectPtr supervision, ObjectPtr predictedOutput);
+  void applyRegularizer();
+  void parametersChanged();
+
+  virtual void finishInferencesCallback();
+};
+
+}; /* namespace lbcpp */
+
+#endif // !LBCPP_INFERENCE_CALLBACK_GRADIENT_DESCENT_LEARNING_H_
