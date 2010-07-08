889cbe0249867331d40d9301006da0f9bd236fda
francis <francis@885cb74b-98ed-4c74-c960-63734602d729>
2010-07-08 10:00:15 +0000

    Missing Values example

--- trunk/projects/Proteins/Programs/AttributeValueSandBox.cpp
+++ trunk/projects/Proteins/Programs/AttributeValueSandBox.cpp
@@ -325,48 +325,13 @@ protected:
   std::map<size_t, size_t> subIndexToIndexMap;
 };
 
-/////////////////////////////////////////
-
-class A : public Object
-{
-public:
-  virtual String toString() const
-    {return T("aaa  ");}
-};
+///////////////////////////////////////// 
 
 int main(int argc, char** argv)
 {
   lbcpp::initialize();
   declareProteinClasses();
   Class::declare(new ProteinResidueInputAttributesClass(13));
-  LBCPP_DECLARE_CLASS(A, Object);
-
-  Variable myBoolean(true);
-  Variable myMissingBoolean = Variable::missingValue(booleanType());
-  Variable myInt(51);
-  Variable myMissingInt = Variable::missingValue(integerType());
-  Variable myDouble(8.6);
-  Variable myMissingDouble = Variable::missingValue(doubleType());
-  Variable myString(T("Hello"));
-  Variable myMissingString = Variable::missingValue(stringType());
-  Variable myObject(new A());
-  Variable myMissingObject = Variable::missingValue(Class::get(T("A")));
-  Variable myPair = Variable::pair(myBoolean, myObject);
-  Variable myMissingPair = Variable::missingValue(pairType(booleanType(), Class::get(T("A"))));
-  
-  std::cout << myBoolean << " " << myMissingBoolean << std::endl
-            << myInt << " " << myMissingInt << std::endl
-            << myDouble << " " << myMissingDouble << std::endl
-            << myString << " " << myMissingString << std::endl
-            << myObject << " " << myMissingObject << std::endl
-            << myPair << " " << myMissingPair << std::endl;
-      
-  std::cout << myBoolean.isMissingValue() << " " << myMissingBoolean.isMissingValue() << std::endl
-            << myInt.isMissingValue() << " " << myMissingInt.isMissingValue() << std::endl
-            << myDouble.isMissingValue() << " " << myMissingDouble.isMissingValue() << std::endl
-            << myString.isMissingValue() << " " << myMissingString.isMissingValue() << std::endl
-            << myObject.isMissingValue() << " " << myMissingObject.isMissingValue() << std::endl
-            << myPair.isMissingValue() << " " << myMissingPair.isMissingValue() << std::endl;
   
 
   /*
@@ -390,8 +355,8 @@ int main(int argc, char** argv)
   VectorPtr proteins = convertProteins(oldStyleProteins);
   oldStyleProteins = ObjectContainerPtr();
   std::cout << proteins->size() << " proteins" << std::endl;
-  proteins->getVariable(0).saveToFile(workingDirectory.getChildFile(T("NewProt.xml")));
-  return 0;
+  //proteins->getVariable(0).saveToFile(workingDirectory.getChildFile(T("NewProt.xml")));
+  //return 0;
   //PrintObjectVisitor::print(proteins->getVariable(2), std::cout, 2);
 
   // make secondary structure classification examples
