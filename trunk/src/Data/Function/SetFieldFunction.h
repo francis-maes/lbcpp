0f1dee0a56beffb992d33ad6d3f5ef9c9cc68e73
francis <francis@885cb74b-98ed-4c74-c960-63734602d729>
2010-08-19 10:38:22 +0000

    '''XML introspection for lbcpp Function'''

---
+++ trunk/src/Data/Function/SetFieldFunction.h
@@ -0,0 +1,50 @@
+/*-----------------------------------------.---------------------------------.
+| Filename: SetFieldFunction.h             | Set Field Function              |
+| Author  : Francis Maes                   |                                 |
+| Started : 19/08/2010 13:06               |                                 |
+`------------------------------------------/                                 |
+                               |                                             |
+                               `--------------------------------------------*/
+
+#ifndef LBCPP_DATA_FUNCTION_SET_FIELD_H_
+# define LBCPP_DATA_FUNCTION_SET_FIELD_H_
+
+# include <lbcpp/Data/Function.h>
+
+namespace lbcpp
+{
+
+// Input: (a,b)
+// do: a[fieldIndex] = b if fieldIndex, nothing otherwise
+// Output: a
+class SetFieldFunction : public Function
+{
+public:
+  SetFieldFunction(size_t fieldIndex = 0)
+    : fieldIndex(fieldIndex) {}
+
+  virtual TypePtr getInputType() const
+    {return pairType(objectClass(), anyType());}
+  
+  virtual TypePtr getOutputType(TypePtr inputType) const
+    {return inputType->getTemplateArgument(0);}
+
+protected:
+  friend class SetFieldFunctionClass;
+
+  size_t fieldIndex;
+
+  virtual Variable computeFunction(const Variable& input, ErrorHandler& callback) const
+  {
+    ObjectPtr object = input[0].getObject();
+    if (object && input[1])
+      object->setVariable(fieldIndex, input[1]);
+    else if (!object)
+      callback.warningMessage(T("SetFieldFunction::computeFunction"), T("Null object"));
+    return input[0];
+  }
+};
+
+}; /* namespace lbcpp */
+
+#endif // !LBCPP_DATA_FUNCTION_SET_FIELD_H_
