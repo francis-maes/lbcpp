130ca1293e8c34931a1d7b2391f84747f63cac1b
francis <francis@885cb74b-98ed-4c74-c960-63734602d729>
2010-08-25 16:36:17 +0000

    Compilation Linux

--- projects/Proteins/Programs/SandBox.cpp
+++ projects/Proteins/Programs/SandBox.cpp
@@ -38,7 +38,7 @@ public:
   virtual PerceptionPtr createPerception(const String& targetName, bool is1DTarget, bool is2DTarget) const
   {
     PerceptionPtr res = ProteinInferenceFactory::createPerception(targetName, is1DTarget, is2DTarget);
-    return res ? perceptionToFeatures(res) : PerceptionPtr();
+    return res ? (PerceptionPtr)perceptionToFeatures(res) : PerceptionPtr();
   }
   
 public:
