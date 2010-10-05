5e4ff6a49c50f2dc53903cfa92995d5f9c77c869
francis <francis@885cb74b-98ed-4c74-c960-63734602d729>
2010-10-05 12:08:24 +0000

    Split ProteinPerception.h

--- trunk/projects/Proteins/Perception/ProteinPerception.h
+++ trunk/projects/Proteins/Perception/ProteinPerception.h
@@ -18,7 +18,7 @@ namespace lbcpp
 /*
 ** ProteinFunction
 */
-extern FunctionPtr proteinToVariableFunction(int);
+extern FunctionPtr proteinToVariableFunction(int variableNumber);
 extern FunctionPtr residueToSelectPairSequencesFunction(int index1, int index2);
 
 /*
@@ -44,90 +44,10 @@ public:
 };
 
 typedef ReferenceCountedObjectPtr<ResiduePerception> ResiduePerceptionPtr;
-  
-class PositionResiduePerception : public ResiduePerception
-{
-public:
-  virtual size_t getNumOutputVariables() const
-    {return 1;}
-  
-  virtual TypePtr getOutputVariableType(size_t index) const
-    {return probabilityType();}
-  
-  virtual String getOutputVariableName(size_t index) const
-    {return T("Position");}
-  
-  virtual void computePerception(const Variable& input, PerceptionCallbackPtr callback) const
-  {
-    ProteinPtr protein = input[0].getObjectAndCast<Protein>();
-    jassert(protein);
-    callback->sense(0, Variable((double)input[1].getInteger() / protein->getLength(), probabilityType()));
-  }
-};
-  
-typedef ReferenceCountedObjectPtr<PositionResiduePerception> PositionResiduePerceptionPtr;
 
 extern ResiduePerceptionPtr positionResiduePerception();
-
-class IndexResiduePerception : public ResiduePerception
-{
-public:
-  virtual size_t getNumOutputVariables() const
-    {return 1;}
-  
-  virtual TypePtr getOutputVariableType(size_t index) const
-    {return integerType();}
-  
-  virtual String getOutputVariableName(size_t index) const
-    {return T("Index");}
-  
-  virtual void computePerception(const Variable& input, PerceptionCallbackPtr callback) const
-    {callback->sense(0, input[1]);}
-};
-
-typedef ReferenceCountedObjectPtr<IndexResiduePerception> IndexResiduePerceptionPtr;
-
 extern ResiduePerceptionPtr indexResiduePerception();
-
-class TerminusProximityResiduePerception : public ResiduePerception
-{
-public:
-  TerminusProximityResiduePerception(size_t outOfBoundWindowSize = 10)
-    : outOfBoundWindowSize(outOfBoundWindowSize) {}
-  
-  virtual size_t getNumOutputVariables() const
-    {return 1;}
-  
-  virtual TypePtr getOutputVariableType(size_t index) const
-    {return probabilityType();}
-  
-  virtual String getOutputVariableName(size_t index) const
-    {return T("Terminus");}
-  
-  virtual void computePerception(const Variable& input, PerceptionCallbackPtr callback) const
-  {
-    ProteinPtr protein = input[0].getObjectAndCast<Protein>();
-    jassert(protein);
-    size_t n = protein->getLength();
-    size_t index = input[1].getInteger();
-    
-    if (index < outOfBoundWindowSize)
-      callback->sense(0, Variable(index * (0.5 / outOfBoundWindowSize), probabilityType()));
-    else if (n - index - 1 < outOfBoundWindowSize)
-      callback->sense(0, Variable(0.5 + (outOfBoundWindowSize - (n - index - 1)) * (0.5 / outOfBoundWindowSize), probabilityType()));
-    else
-      callback->sense(0, Variable(0.5, probabilityType()));
-  }
-
-protected:
-  friend class TerminusProximityResiduePerceptionClass;
-
-  size_t outOfBoundWindowSize;
-};
-
-typedef ReferenceCountedObjectPtr<TerminusProximityResiduePerception> TerminusProximityResiduePerceptionPtr;
-
-extern ResiduePerceptionPtr terminusProximityResiduePerception(size_t outOfBoundWindowSize);
+extern ResiduePerceptionPtr boundsProximityResiduePerception(size_t outOfBoundWindowSize);
   
 class ResidueCompositePerception : public CompositePerception
 {
