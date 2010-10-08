d1563c46029ed71b2499bdc44f1e825291d464af
francis <francis@885cb74b-98ed-4c74-c960-63734602d729>
2010-10-08 19:45:50 +0000

    optimisation

--- trunk/src/Data/Object.cpp
+++ trunk/src/Data/Object.cpp
@@ -14,6 +14,44 @@ using namespace lbcpp;
 
 extern void declareLBCppClasses();
 
+void ReferenceCountedObject::incrementReferenceCounter()
+{
+  double startTime = Time::getMillisecondCounterHiRes();
+  juce::atomicIncrement(refCount);
+  double deltaTime = Time::getMillisecondCounterHiRes() - startTime;
+  static int counter = 0;
+  static double sum = 0.0;
+  sum += deltaTime;
+  ++counter;
+  if (counter == 1000)
+  {
+    std::cout << "incrementReferenceCounter: " << counter << "ms" << std::endl;
+    sum = 0.0;
+    counter = 0;
+  }
+}
+
+/** Decrements the object's reference count.  */
+void ReferenceCountedObject::decrementReferenceCounter()
+{
+  double startTime = Time::getMillisecondCounterHiRes();
+  
+  if (juce::atomicDecrementAndReturn(refCount) == 0)
+    delete this;
+  double deltaTime = Time::getMillisecondCounterHiRes() - startTime;
+  static int counter = 0;
+  static double sum = 0.0;
+  sum += deltaTime;
+  ++counter;
+  if (counter == 1000)
+  {
+    std::cout << "decrementReferenceCounter: " << counter << "ms" << std::endl;
+    sum = 0.0;
+    counter = 0;
+  }
+}
+
+
 String Object::getClassName() const
 {
   if (thisClass)
