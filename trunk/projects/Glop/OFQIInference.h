f9114ccf8724a13664da2503bd3f63b1b0d49a64
francis <francis@885cb74b-98ed-4c74-c960-63734602d729>
2010-10-18 15:56:30 +0000

    et hop

---
+++ trunk/projects/Glop/OFQIInference.h
@@ -0,0 +1,55 @@
+#ifndef GLOP_OFQI_INFERENCE_H_
+# define GLOP_OFQI_INFERENCE_H_
+
+# include <lbcpp/Inference/SequentialInference.h>
+
+namespace lbcpp
+{
+
+class OFQIInference : public VectorSequentialInference
+{
+public:
+  OFQIInference(size_t horizon, double discount, InferencePtr regressorModel)
+    : VectorSequentialInference(T("OFQI")), discount(discount)
+  {
+    for (size_t i = 0; i < horizon; ++i)
+      {
+	InferencePtr regressor = regressorModel->cloneAndCast<Inference>();
+	regressor->setName(T("OFQI - ") + String((int)i));
+	appendInference(regressor);
+      }
+  }
+
+  OFQIInference() {}
+
+  virtual TypePtr getInputType() const
+    {return anyType;}
+
+  virtual TypePtr getSupervisionType() const
+    {return doubleType;}
+
+  virtual TypePtr getOutputType() const
+    {return doubleType;}  
+
+  virtual void prepareSubInference(InferenceContextPtr context, SequentialInferenceStatePtr state, size_t index, ReturnCode& returnCode)
+  {
+    Variable supervision;
+    if (state->getSupervision().exists())
+    {
+      double value = state->getSupervision().getDouble();
+      if (index > 0)
+        value += state->getSubOutput().getDouble() * discount;  
+      supervision = Variable(value);
+    }
+    state->setSubInference(subInferences[index], state->getInput(), supervision);
+  }
+
+private:
+  friend class OFQIInferenceClass;
+
+  double discount;
+};
+
+}; /* namespace lbcpp */
+
+#endif // !GLOP_OFQI_INFERENCE_H_
