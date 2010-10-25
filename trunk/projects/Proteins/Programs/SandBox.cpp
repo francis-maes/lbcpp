1c9ee2b15f19df0b305e36a323d5de796b001ffd
francis <francis@885cb74b-98ed-4c74-c960-63734602d729>
2010-10-25 13:56:31 +0000

    RankingBasedExtractionInference, DisorderedRegionInference

--- trunk/projects/Proteins/Programs/SandBox.cpp
+++ trunk/projects/Proteins/Programs/SandBox.cpp
@@ -17,6 +17,143 @@ using namespace lbcpp;
 
 extern void declareProteinClasses();
 
+class RankingBasedExtractionInference : public VectorSequentialInference
+{
+public:
+  RankingBasedExtractionInference(const String& name, InferencePtr rankingInference, InferencePtr cutoffInference)
+    : VectorSequentialInference(name)
+  {
+    appendInference(rankingInference);
+    appendInference(cutoffInference);
+  }
+
+  virtual TypePtr getInputType() const
+    {return subInferences[0]->getInputType();}
+
+  virtual TypePtr getSupervisionType() const
+    {return subInferences[0]->getSupervisionType();}
+
+  virtual TypePtr getOutputType(TypePtr inputType) const
+    {return subInferences[0]->getOutputType(inputType);}
+
+  virtual ContainerPtr createRankingInputs(const Variable& input) const;
+  virtual ContainerPtr createRankingCosts(const Variable& supervision) const = 0;
+  virtual Variable createCutoffInput(const Variable& input) const
+    {return input;}
+  virtual double computeBestCutoff(const ContainerPtr& scores, const ContainerPtr& costs) const = 0;
+
+  virtual Variable computeOutput(const ContainerPtr& scores, double cutoff) const = 0;
+
+  struct State : public SequentialInferenceState
+  {
+    State(const Variable& input, const Variable& supervision)
+      : SequentialInferenceState(input, supervision) {}
+
+    ContainerPtr scores;
+  };
+
+  typedef ReferenceCountedObjectPtr<State> StatePtr;
+
+  virtual SequentialInferenceStatePtr prepareInference(const InferenceContextPtr& context, const Variable& input, const Variable& supervision, ReturnCode& returnCode)
+  {
+    SequentialInferenceStatePtr state = new State(input, supervision);
+    state->setSubInference(subInferences[0], createRankingInputs(input), supervision.exists() ? createRankingCosts(supervision) : ContainerPtr());
+    return state;
+  }
+
+  virtual void prepareSubInference(InferenceContextPtr context, SequentialInferenceStatePtr s, size_t index, ReturnCode& returnCode)
+  {
+    jassert(index == 1);
+    const StatePtr& state = s.staticCast<State>();
+    state->scores = state->getSubOutput().getObjectAndCast<Container>();
+    jassert(state->scores);
+
+    Variable supervision;
+    if (state->getSupervision().exists())
+      supervision = computeBestCutoff(state->getSubOutput().getObjectAndCast<Container>(), state->getSubSupervision().getObjectAndCast<Container>());
+    state->setSubInference(subInferences[1], createCutoffInput(state->getInput()), supervision);
+  }
+
+  virtual Variable finalizeInference(const InferenceContextPtr& context, SequentialInferenceStatePtr finalState, ReturnCode& returnCode)
+  {
+    const StatePtr& state = finalState.staticCast<State>();
+    Variable predictedCutoff = state->getSubOutput();
+    double cutoff = predictedCutoff.exists() ? predictedCutoff.getDouble() : 0.0;
+    return computeOutput(state->scores, cutoff);
+  }
+};
+
+class DisorderedRegionInference : public RankingBasedExtractionInference
+{
+public:
+  DisorderedRegionInference(const String& name, InferencePtr rankingInference, InferencePtr cutoffInference)
+    : RankingBasedExtractionInference(name, rankingInference, cutoffInference)
+  {
+    checkInheritance(rankingInference->getInputType(), containerClass(pairClass(proteinClass, positiveIntegerType)));
+    checkInheritance(cutoffInference->getInputType(), proteinClass);
+  }
+
+  virtual TypePtr getInputType() const
+    {return proteinClass;}
+
+  virtual TypePtr getSupervisionType() const
+    {return containerClass(probabilityType);}
+
+  virtual TypePtr getOutputType() const
+    {return containerClass(probabilityType);}
+
+  virtual ContainerPtr createRankingInputs(const Variable& input) const
+  {
+    const ProteinPtr& protein = input.getObjectAndCast<Protein>();
+    size_t n = protein->getLength();
+
+    TypePtr elementsType = pairClass(proteinClass, positiveIntegerType);
+    ContainerPtr res = objectVector(elementsType, n);
+    for (size_t i = 0; i < n; ++i)
+      res->setElement(i, Variable::pair(input, i, elementsType));
+    return res;
+  }
+
+  virtual ContainerPtr createRankingCosts(const Variable& sup) const
+  {
+    const ContainerPtr& supervision = sup.getObjectAndCast<Container>();
+    size_t n = supervision->getNumElements();
+    ContainerPtr res = vector(doubleType, n);
+    for (size_t i = 0; i < n; ++i)
+      res->setElement(i, supervision->getElement(i).getDouble() < 0.5 ? 1.0 : 0.0);
+    return res;
+  }
+
+  virtual Variable createCutoffInput(const Variable& input) const
+    {return input;}
+
+  virtual double computeBestCutoff(const ContainerPtr& scores, const ContainerPtr& costs) const
+  {
+    ROCAnalyse roc;
+    size_t n = scores->getNumElements();
+    jassert(n == costs->getNumElements());
+    for (size_t i = 0; i < n; ++i)
+      roc.addPrediction(scores->getElement(i).getDouble(), costs->getElement(i).getDouble() == 0.0);
+    double bestMCC;
+    double res = roc.findThresholdMaximisingMCC(bestMCC);
+    MessageCallback::info(T("computeBestCutoff: ") + String(res) + T(" (MCC = ") + String(bestMCC) + T(")"));
+    return res;
+  }
+
+  virtual Variable computeOutput(const ContainerPtr& scores, double cutoff) const
+  {
+    size_t n = scores->getNumElements();
+    VectorPtr res = vector(probabilityType, n);
+    for (size_t i = 0; i < n; ++i)
+    {
+      static const double temperature = 1.0;
+      double score = scores->getElement(i).getDouble() - cutoff;
+      double probability = 1.0 / (1.0 + exp(-score * temperature));
+      res->setElement(i, probability);
+    }
+    return res;
+  }
+};
 
 ///////////////////////////////////////////////
 
