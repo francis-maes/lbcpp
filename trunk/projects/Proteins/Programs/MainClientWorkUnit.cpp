366acd8e184179a34fbfc59e563233d907d129cc
julien <julien@885cb74b-98ed-4c74-c960-63734602d729>
2010-12-03 18:48:27 +0000

    Forgot file

---
+++ trunk/projects/Proteins/Programs/MainClientWorkUnit.cpp
@@ -0,0 +1,78 @@
+
+#include <lbcpp/lbcpp.h>
+#include "ClientServerWorkUnit.h"
+
+namespace lbcpp
+{
+
+class SgeExecutionContext : public ExecutionContext
+{
+public:
+  SgeExecutionContext(const File& workUnitDirectory)
+  : workUnitDirectory(workUnitDirectory), lastFileID(0) {}
+  
+  virtual bool isMultiThread() const
+  {return false;}
+  
+  virtual bool isCanceled() const
+  {return false;}
+  
+  virtual bool isPaused() const
+  {return false;}
+  
+  virtual bool run(const CompositeWorkUnitPtr& workUnits)
+  {return run((WorkUnitPtr)workUnits);}
+  
+  virtual void pushWorkUnit(const WorkUnitPtr& workUnit)
+  {
+    /* Save work unit */
+    File workUnitFile(String(getFileID()) + T(".workUnit"));
+    if (workUnitFile.exists())
+    {
+      warningCallback(T("SgeExecutionContext::pushWorkUnit"), T("File ") + workUnitFile.getFileName().quoted() + T(" already exists"));
+      workUnitFile.deleteFile();
+    }
+    workUnit->saveToFile(*this, workUnitFile);
+  }
+  
+  lbcpp_UseDebuggingNewOperator
+  
+protected:
+  File workUnitDirectory;
+  juce::int64 lastFileID;
+  
+  juce::int64 getFileID()
+  {
+    juce::int64 res = Time::currentTimeMillis();
+    if (res == lastFileID)
+    {
+      juce::Thread::sleep(1);
+      res = Time::currentTimeMillis();
+    }
+    lastFileID = res;
+    return res;
+  }
+};
+
+};
+
+using namespace lbcpp;
+
+extern void declareProteinClasses(ExecutionContext& context);
+extern void declareProgramClasses(ExecutionContext& context);
+
+int main(int argc, char** argv)
+{
+  lbcpp::initialize(argv[0]);
+  ExecutionContextPtr context = new SgeExecutionContext(T("/u/jbecker/.WorkUnit/Waiting"));
+  declareProteinClasses(*context);
+  declareProgramClasses(*context);
+  
+  int exitCode;
+  {
+    exitCode = WorkUnit::main(*context, new ClientWorkUnit(), argc, argv);
+  }
+
+  lbcpp::deinitialize();
+  return exitCode;
+}
