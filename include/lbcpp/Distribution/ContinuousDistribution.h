ad8a0cbb01724204eedae650f0599c812a616107
francis <francis@885cb74b-98ed-4c74-c960-63734602d729>
2010-12-21 23:26:51 +0000

    '''Rename ProbabilityDistribution into Distribution'''

---
+++ include/lbcpp/Distribution/ContinuousDistribution.h
@@ -0,0 +1,84 @@
+/*-----------------------------------------.---------------------------------.
+| Filename: ContinuousProbabilityDistri...h| Continuous Probability          |
+| Author  : Julien Becker                  | Distributions                   |
+| Started : 06/07/2010 15:15               |                                 |
+`------------------------------------------/                                 |
+                               |                                             |
+                               `--------------------------------------------*/
+
+#ifndef LBCPP_CONTINUOUS_PROBABILITY_DISTRIBUTION_H_
+# define LBCPP_CONTINUOUS_PROBABILITY_DISTRIBUTION_H_
+
+# include <lbcpp/Distribution/Distribution.h>
+
+namespace lbcpp
+{
+
+class ContinuousDistribution : public Distribution
+{
+public:
+  virtual void sampleUniformly(size_t numSamples, std::vector<double>& res) const = 0;
+
+  juce_UseDebuggingNewOperator
+};
+
+typedef ReferenceCountedObjectPtr<ContinuousDistribution> ContinuousDistributionPtr;
+
+class UniformDistribution : public ContinuousDistribution
+{
+public:
+  UniformDistribution(double minimum, double maximum)
+    : minimum(minimum), maximum(maximum) {}
+  UniformDistribution() : minimum(0.0), maximum(0.0) {}
+
+  virtual double computeEntropy() const;
+  virtual double compute(ExecutionContext& context, const Variable& value) const;
+  virtual Variable sample(RandomGeneratorPtr random) const;
+  virtual void sampleUniformly(size_t numSamples, std::vector<double>& res) const;
+
+  double getMinimum() const
+    {return minimum;}
+
+  double getMaximum() const
+    {return maximum;}
+
+  juce_UseDebuggingNewOperator
+
+protected:
+  friend class UniformDistributionClass;
+
+  double minimum;
+  double maximum;
+};
+
+class GaussianDistribution : public ContinuousDistribution
+{
+public:
+  GaussianDistribution(double mean = 0.0, double variance = 0.0)
+    : mean(mean), variance(variance) {}
+
+  virtual double computeEntropy() const;
+  virtual double compute(ExecutionContext& context, const Variable& value) const;
+  virtual Variable sample(RandomGeneratorPtr random) const;
+  virtual void sampleUniformly(size_t numSamples, std::vector<double>& res) const;
+
+  double getMean() const
+    {return mean;}
+
+  double getVariance() const
+    {return variance;}
+
+  juce_UseDebuggingNewOperator
+
+protected:
+  friend class GaussianDistributionClass;
+
+  double mean;
+  double variance;
+};
+
+typedef ReferenceCountedObjectPtr<GaussianDistribution> GaussianDistributionPtr;
+  
+}; /* namespace lbcpp */
+
+#endif // !LBCPP_CONTINUOUS_PROBABILITY_DISTRIBUTION_H_
---
+++ src/Distribution/ContinuousDistribution.cpp
@@ -0,0 +1,67 @@
+/*-----------------------------------------.---------------------------------.
+| Filename: ContinuousProbabilityDist...cpp| Continuous Probability          |
+| Author  : Julien Becker                  | Distributions                   |
+| Started : 06/07/2010 15:15               |                                 |
+`------------------------------------------/                                 |
+                               |                                             |
+                               `--------------------------------------------*/
+
+#include <lbcpp/Core/Variable.h>
+#include <lbcpp/Distribution/ContinuousDistribution.h>
+using namespace lbcpp;
+
+/*
+** UniformDistribution
+*/
+double UniformDistribution::computeEntropy() const
+  {return log(maximum - minimum);}
+
+double UniformDistribution::compute(ExecutionContext& context, const Variable& value) const
+{
+  jassert(minimum < maximum);
+  double d = value.getDouble();
+  if (d < minimum || d > maximum)
+    return 0.0;
+  else
+    return 1.0 / (maximum - minimum);
+}
+
+Variable UniformDistribution::sample(RandomGeneratorPtr random) const
+  {return random->sampleDouble(minimum, maximum);}
+
+void UniformDistribution::sampleUniformly(size_t numSamples, std::vector<double>& res) const
+{
+  jassert(minimum < maximum);
+  jassert(numSamples);
+  res.resize(numSamples);
+  double k = (maximum - minimum) / (numSamples - 1.0);
+  for (size_t i = 0; i < res.size(); ++i)
+    res[i] = minimum + i * k;
+}
+
+/*
+** GaussianDistribution
+*/
+double GaussianDistribution::computeEntropy() const
+  {return 0.5 * log(2 * M_PI * exp(1.0) * getVariance());}
+
+double GaussianDistribution::compute(ExecutionContext& context, const Variable& value) const
+{
+  jassert(value.isDouble());
+  double mean = getMean();
+  double variance = getVariance(); // FIXME: Variance or Standard Deviation ?????
+  double squaredNumerator = value.getDouble() - mean;
+  squaredNumerator *= squaredNumerator;
+  double squaredDenominator = 2 * variance;
+  squaredDenominator *= squaredDenominator;
+  return 1 / sqrt(variance * 2 * M_PI) * exp(-squaredNumerator/squaredDenominator);
+}
+
+Variable GaussianDistribution::sample(RandomGeneratorPtr random) const
+  {return Variable(random->sampleDoubleFromGaussian(getMean(), getVariance()), doubleType);} // FIXME: variance or stddev ?
+
+void GaussianDistribution::sampleUniformly(size_t numSamples, std::vector<double>& res) const
+{
+  jassert(false); // not implemented !
+}
+
