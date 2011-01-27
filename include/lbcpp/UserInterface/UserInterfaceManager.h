552cfa077436ac6884e687418eec11f980c3fd44
francis <francis@885cb74b-98ed-4c74-c960-63734602d729>
2011-01-27 17:03:30 +0000

    New generic component: ContainerTableListBox

--- include/lbcpp/UserInterface/UserInterfaceManager.h
+++ include/lbcpp/UserInterface/UserInterfaceManager.h
@@ -42,6 +42,7 @@ public:
   juce::Component* createComponentIfExists(ExecutionContext& context, const ObjectPtr& object, const String& name = String::empty) const;
   juce::TreeView* createVariableTreeView(ExecutionContext& context, const Variable& variable, const String& name = String::empty,
                                           bool showTypes = true, bool showShortSummaries = true, bool showMissingVariables = false, bool makeRootNodeVisible = true) const;
+  juce::TableListBox* createContainerTableListBox(ExecutionContext& context, const ContainerPtr& container) const;
 
 private:
   UserInterfaceThread* userInterfaceThread;
