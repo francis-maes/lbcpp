afc462a0112ccb3f891d7fb09222b2804d32acf5
arnaud <arnaud@885cb74b-98ed-4c74-c960-63734602d729>
2011-02-14 20:04:51 +0000

    unzipper usage

--- trunk/unzipper/Main.cpp
+++ trunk/unzipper/Main.cpp
@@ -1,5 +1,5 @@
 /*-----------------------------------------.---------------------------------.
- | Filename: RunWorkUnit.cpp                | A program to launch work units  |
+ | Filename: RunWorkUnit.cpp                | A program to unzip zip files    |
  | Author  : Arnaud Schoofs                 |                                 |
  | Started : 30/10/2010 12:00               |                                 |
  `------------------------------------------/                                 |
@@ -9,16 +9,33 @@
 #include <lbcpp/library.h>
 using namespace lbcpp;
 
+void usage()
+{
+  std::cerr << "Usage: unzipper /absolute/path/to/source.zip /absolute/path/to/destination/directory/" << std::endl;
+}
+
 int mainImpl(int argc, char** argv) {
-	File file(argv[1]);
-	if (!file.existsAsFile()) {
-		std::cerr << "ZipFile not found:" << argv[1] << std::endl;
-		return -1;
+	if (argc != 3) {
+		usage();
+		return 1;
 	}
 	
+	File file(argv[1]);
+	if (!file.existsAsFile()) {
+		std::cerr << "ZipFile not found: " << argv[1] << std::endl;
+		usage();
+		return 1;
+	}
 	ZipFile zippy(file);
+	
 	File target(argv[2]);
-	zippy.uncompressTo(target, false);
+	if (!file.exists()) {
+		std::cerr << "Destination doesn't exist: " << argv[2] << std::endl;
+		usage();
+		return 1;
+	}
+	
+	zippy.uncompressTo(target, false);	// don't overwrite files that already exist
 	
 	return 0;
 }
