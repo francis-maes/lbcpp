e402010532f88217bcd3b43950dbea77eeffa681
francis <francis@885cb74b-98ed-4c74-c960-63734602d729>
2011-02-16 22:59:52 +0000

    '''Validate TrainTestLearningMachine on binary classification'''
     * Add an optional "scopeName" argument to Function::evaluate() and Function::train()
     * TrainTestLearningMachine: make it work on binary classification

--- src/Learning/Numerical/LinearLearningMachine.h
+++ src/Learning/Numerical/LinearLearningMachine.h
@@ -24,17 +24,26 @@ public:
     : SupervisedNumericalFunction(learnerParameters), lossFunctionClass(lossFunctionClass) {}
   SupervisedLinearNumericalFunction() {}
 
+  virtual FunctionPtr createPostProcessing() const
+    {return FunctionPtr();}
+
   virtual TypePtr initializeFunction(ExecutionContext& context, const std::vector<VariableSignaturePtr>& inputVariables, String& outputName, String& outputShortName)
   {
     EnumerationPtr featuresEnumeration = DoubleVector::getElementsEnumeration(inputVariables[0]->getType());
 
     frameClass = new FrameClass(getClassName() + T("Frame"));
-    frameClass->addMemberVariable(context, inputVariables[0]->getType(), T("input"));
-    frameClass->addMemberVariable(context, inputVariables[1]->getType(), T("supervision"));
-    frameClass->addMemberOperator(context, createObjectFunction(lossFunctionClass), 1);
+    frameClass->addMemberVariable(context, inputVariables[0]->getType(), T("input"));             // 0: input
+    frameClass->addMemberVariable(context, inputVariables[1]->getType(), T("supervision"));       // 1: supervision
+    frameClass->addMemberOperator(context, createObjectFunction(lossFunctionClass), 1);           // 2: loss(supervision)
+
     FunctionPtr linearFunction = new LinearLearnableFunction();
     linearFunction->setOnlineLearner(learnerParameters->createOnlineLearner());
-    frameClass->addMemberOperator(context, linearFunction, 0, 2);
+    frameClass->addMemberOperator(context, linearFunction, 0, 2);                                 // 3: linearFunction(0,2)
+
+    FunctionPtr postProcessing = createPostProcessing();
+    if (postProcessing)
+      frameClass->addMemberOperator(context, postProcessing, 3);                                  // 4: postProcess(3)          
+
     setBatchLearner(learnerParameters->createBatchLearner());
     return FrameBasedFunction::initializeFunction(context, inputVariables, outputName, outputShortName);
   }
@@ -56,6 +65,32 @@ public:
     {return doubleType;}
 };
 
+class SignedScalarToProbabilityFunction : public Function
+{
+public:
+  virtual size_t getNumRequiredInputs() const
+    {return 1;}
+
+  virtual TypePtr getRequiredInputType(size_t index, size_t numInputs) const
+    {return doubleType;}
+
+  virtual String getOutputPostFix() const
+    {return T("Prob");}
+
+  virtual TypePtr initializeFunction(ExecutionContext& context, const std::vector<VariableSignaturePtr>& inputVariables, String& outputName, String& outputShortName)
+    {return probabilityType;}
+
+  virtual Variable computeFunction(ExecutionContext& context, const Variable& input) const
+  {
+    if (!input.exists())
+      return Variable::missingValue(probabilityType);
+
+    double score = input.getDouble();
+    static const double temperature = 1.0;
+    return Variable(1.0 / (1.0 + exp(-score * temperature)), probabilityType);
+  }
+};
+
 class LinearBinaryClassifier : public SupervisedLinearNumericalFunction
 {
 public:
@@ -64,7 +99,10 @@ public:
   LinearBinaryClassifier() {}
 
   virtual TypePtr getSupervisionType() const
-    {return probabilityType;}
+    {return booleanType;}
+
+  virtual FunctionPtr createPostProcessing() const
+    {return new SignedScalarToProbabilityFunction();}
 };
 
 class LinearLearningMachine : public ProxyFunction
