aea70c6a13c293c049f1e0588ccfb69e75a614fd
julien <julien@885cb74b-98ed-4c74-c960-63734602d729>
2011-03-01 10:33:11 +0000

    '''Network'''  * Better description of my changes in Juce  * Add a cron handler for the manager. It just start the manager when it don't yet run ... manager is not perfect, there are still some cases (not easy to reproduce) where it crash (malloc/free crash, glic detection)

---
+++ src/Network/Deamons/Manager/cronHandler.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+managerDirectory="/home/jbecker/Manager"
+programDirectory="/home/jbecker/Workspace/LBC++/bin/Release/"
+
+cd $managerDirectory
+
+if [ -f .lock ]
+then
+  exit
+fi
+
+touch .lock
+
+startDate=`date`
+echo "$startDate - Start Manager" >> .cronHandler
+
+cd $programDirectory
+./RunWorkUnit ManagerWorkUnit --projectDirectory $managerDirectory --numThreads 1 2>&1 >> $managerDirectory/.cronHandler
+
+cd $managerDirectory
+endDate=`date`
+echo "$endDate - Stop Manager" >> .cronHandler
+
+rm .lock
+
