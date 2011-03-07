e398e3695b3df62b9f3bb892cb34d91b337cdeb0
arnaud <arnaud@885cb74b-98ed-4c74-c960-63734602d729>
2011-03-07 00:00:23 +0000

    '''ProteinGridEvoOptimizer'''  * Some refactoring

--- trunk/projects/Proteins/Optimizer/ProteinGridEvoOptimizer.cpp
+++ trunk/projects/Proteins/Optimizer/ProteinGridEvoOptimizer.cpp
@@ -10,6 +10,36 @@
 #include "ProteinGridEvoOptimizer.h"
 #include <map>
 
+
+ProteinGridEvoOptimizerState::ProteinGridEvoOptimizerState(IndependentMultiVariateDistributionPtr distributions) : distributions(distributions) 
+{
+  // TODO arnaud : if state.xml loaded
+  totalNumberGeneratedWUs = 0;
+  totalNumberEvaluatedWUs = 0;
+  distributionsBuilders = objectVector(distributionBuilderClass, 14);
+  distributionsBuilders->setElement(0, gaussianDistributionBuilder());
+  distributionsBuilders->setElement(1, gaussianDistributionBuilder());
+  distributionsBuilders->setElement(2, gaussianDistributionBuilder());
+  distributionsBuilders->setElement(3, gaussianDistributionBuilder());
+  distributionsBuilders->setElement(4, gaussianDistributionBuilder());
+  distributionsBuilders->setElement(5, gaussianDistributionBuilder());
+  distributionsBuilders->setElement(6, gaussianDistributionBuilder());
+  distributionsBuilders->setElement(7, gaussianDistributionBuilder());
+  distributionsBuilders->setElement(8, gaussianDistributionBuilder());
+  distributionsBuilders->setElement(9, gaussianDistributionBuilder());
+  distributionsBuilders->setElement(10, bernoulliDistributionBuilder());
+  distributionsBuilders->setElement(11, gaussianDistributionBuilder());
+  distributionsBuilders->setElement(12, gaussianDistributionBuilder());
+  distributionsBuilders->setElement(13, gaussianDistributionBuilder());
+}
+
+void ProteinGridEvoOptimizerState::clearBuilders()
+{
+  for (size_t i = 0; i < distributionsBuilders->getNumElements(); ++i) {
+    distributionsBuilders->getAndCast<DistributionBuilder>(i)->clear();
+  }
+}
+
 Variable ProteinGridEvoOptimizer::optimize(ExecutionContext& context, const FunctionPtr& objective, const Variable& apriori) const
 {
   /** 
@@ -36,42 +66,41 @@ Variable ProteinGridEvoOptimizer::optimize(ExecutionContext& context, const Func
   
   ProteinGridEvoOptimizerStatePtr state = new ProteinGridEvoOptimizerState(apriori.dynamicCast<IndependentMultiVariateDistribution>());
   
-  // generate
-  while (state->getNumberGeneratedWU() < 10) {
-    state->generateSampleWU(context, String((int) state->getNumberGeneratedWU()) + T(".xml"));
-    state->inProgressWUs.insert(String((int) state->getNumberGeneratedWU()) + T(".xml"));
+  // Init generation
+  while (state->totalNumberGeneratedWUs < 10) {
+    state->generateSampleWU(context, String((int) state->totalNumberGeneratedWUs));
   }
-  
-  // send WUs
-  
-  /*while (state->getNumberEvaluated() < 10) {
-    juce::Thread::sleep(5000);
-  }*/
-  
-  ObjectVectorPtr builders = objectVector(gaussianDistributionBuilderClass, 1);
-  builders->setElement(0, gaussianDistributionBuilder());
-  
-  std::multimap<double, String> myMultiMap;
-  for (int i = 0; i < 10; ++i) {
-    // TODO arnaud : XmlSerialisation bug with createObject
-    File file(String("/Users/arnaudschoofs/Proteins/traces/") + String(i) + T(".trace"));
-    XmlImporter importer(context, file);
-    if (!importer.isOpened())
-    {jassertfalse;}
-    if (!importer.enter("shared"))
-    {jassertfalse;}
-    double score = ((importer.getCurrentElement())->getNextElement()->getChildElementAllSubText(String("variable"), String("error"))).getDoubleValue();
     
-    state->evaluatedWUs.insert(std::pair<double, String>(score, String(i)));
+  // handle finished WU's
+  while (state->currentEvaluatedWUs.size() < 10) {
+    juce::Thread::sleep(5000);
+    std::set<String>::iterator it;
+    for(it = state->inProgressWUs.begin(); it != state->inProgressWUs.end(); )
+    {
+      File f(String("/Users/arnaudschoofs/Proteins/traces/") + *it + T(".trace"));
+      if (f.existsAsFile())
+      {
+        XmlImporter importer(context, f);
+        if (!importer.isOpened())
+          {jassertfalse;}
+        if (!importer.enter("shared"))
+          {jassertfalse;}
+        double score = ((importer.getCurrentElement())->getNextElement()->getChildElementAllSubText(String("variable"), String("error"))).getDoubleValue();
+        state->currentEvaluatedWUs.insert(std::pair<double, String>(score,*it));
+        state->inProgressWUs.erase(it++);
+      }
+      else ++it;
+    }
   }
-  
+    
+  // TODO arnaud : select best results, update distributions, builder for IntegerGaussian
   std::multimap<double, String>::reverse_iterator it;
-  for ( it=state->evaluatedWUs.rbegin(); it != state->evaluatedWUs.rend(); it++ )
+  for ( it=state->currentEvaluatedWUs.rbegin(); it != state->currentEvaluatedWUs.rend(); it++ )
   {
-    builders->getAndCast<DistributionBuilder>(0)->addElement(5.0);
+    state->distributionsBuilders->getAndCast<DistributionBuilder>(0)->addElement(5.0);
     std::cout << (*it).first << " => " << (*it).second << std::endl;
   }
-  builders->getAndCast<DistributionBuilder>(0)->build(context);
+  state->distributionsBuilders->getAndCast<DistributionBuilder>(0)->build(context);
   
   return Variable(0); 
 }
@@ -83,9 +112,10 @@ void ProteinGridEvoOptimizerState::generateSampleWU(ExecutionContext& context, c
   WorkUnitPtr wu = new ProteinLearner();
   // TODO arnaud : set some parameters to 0 (disabled)
   //wu->parseArguments(context, T("-s /Users/arnaudschoofs/Proteins/PDB30Boinc -i /Users/arnaudschoofs/Proteins/PDB30BoincInitialProteins/ -p \"numerical(") + sampleParameters()->toString() + T(",sgd)\" -t ss3 -n 1 -m 20"));
-  wu->parseArguments(context, T("-s /Users/arnaudschoofs/Proteins/PDB30Boinc -i /Users/arnaudschoofs/Proteins/PDB30BoincInitialProteins/ -p \"numerical((1,3,5,3,5,3,2,3,5,5,True,15,15,50),sgd)\" -t ss3 -n 1 -m 250"));
-  wu->saveToFile(context, File(T("/Users/arnaudschoofs/Proteins/wu/") + name));
-  numberGeneratedWU++;
+  wu->parseArguments(context, T("-s /Users/arnaudschoofs/Proteins/PDB30Boinc -i /Users/arnaudschoofs/Proteins/PDB30BoincInitialProteins/ -p \"numerical((1,3,5,3,5,3,2,3,5,5,True,15,15,50),sgd)\" -t ss3 -n 1 -m 10"));
+  wu->saveToFile(context, File(T("/Users/arnaudschoofs/Proteins/wu/") + name + T(".xml")));
+  totalNumberGeneratedWUs++;
+  inProgressWUs.insert(name);
 }
 
 NumericalProteinFeaturesParametersPtr ProteinGridEvoOptimizerState::sampleParameters() const 
