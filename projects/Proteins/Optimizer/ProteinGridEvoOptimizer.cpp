df06860be8f617adf0d5161793df58c7b59b0335
arnaud <arnaud@885cb74b-98ed-4c74-c960-63734602d729>
2011-03-09 12:31:29 +0000

    '''GridEvoOptimizer'''  * Network part started and partially tested  * Bug in Manager discovered (handling trace files)

--- projects/Proteins/Optimizer/ProteinGridEvoOptimizer.cpp
+++ projects/Proteins/Optimizer/ProteinGridEvoOptimizer.cpp
@@ -61,26 +61,40 @@ Variable ProteinGridEvoOptimizer::optimize(ExecutionContext& context, const Func
   
   ProteinGridEvoOptimizerStatePtr state = new ProteinGridEvoOptimizerState(apriori.dynamicCast<IndependentMultiVariateDistribution>());
   
-  // Init generation and send
-  
+  // Init generation and send : OK
+  String projectName(T("BoincFirstStage"));
+  String source(T("boincadm@boinc.run"));
+  String destination(T("boincadm@boinc.run"));
+  String managerHostName(T("monster24.montefiore.ulg.ac.be"));
+  size_t managerPort = 1664;
+  size_t requiredCpus = 1;
+  size_t requiredMemory = 2;
+  size_t requiredTime = 10;
+  /*
   NetworkClientPtr client = blockingNetworkClient(context);
-  if (!client->startClient(T("monster24.montefiore.ulg.ac.be"), 1664));
+  if (!client->startClient(managerHostName, managerPort))
   {
     context.errorCallback(T("SendWorkUnit::run"), T("Not connected !"));
     return false;
   }
-  context.informationCallback(T("monster24.montefiore.ulg.ac.be"), T("Connected !"));
-  ManagerNodeNetworkInterfacePtr interface = new ClientManagerNodeNetworkInterface(context, client, T("GridEvoOptimizer"));
+  context.informationCallback(managerHostName, T("Connected !"));
+  
+  ManagerNodeNetworkInterfacePtr interface = new ClientManagerNodeNetworkInterface(context, client, source);
   interface->sendInterfaceClass();
-  while (state->totalNumberGeneratedWUs < 10) {
+  
+  while (state->totalNumberGeneratedWUs < 1) {
     WorkUnitPtr wu = state->generateSampleWU(context, String((int) state->totalNumberGeneratedWUs));
-    NetworkRequestPtr request = new NetworkRequest(context, T("BoincFirstStage"), T("GridEvoOptimizer"), T("boincadm@boinc.run"), wu, 1, 1, 1);
+    
+    NetworkRequestPtr request = new NetworkRequest(context, projectName, source, destination, wu, requiredCpus, requiredMemory, requiredTime);
     String res = interface->pushWorkUnit(request);
+    
     if (res == T("Error"))
     {
       context.errorCallback(T("SendWorkUnit::run"), T("Touble - We didn't correclty receive the acknowledgement"));
-      break;
+      interface->closeCommunication();
+      return false;
     }
+    
     state->totalNumberGeneratedWUs++;
     state->inProgressWUs.insert(res);
     
@@ -91,16 +105,58 @@ Variable ProteinGridEvoOptimizer::optimize(ExecutionContext& context, const Func
     //request->saveToFile(*context, f);
   }
   interface->closeCommunication();
+  */
   
-  /*
-    
+  
+  state->inProgressWUs.insert(T("1299675529047"));
+  state->inProgressWUs.insert(T("1299675566221"));
+  /*state->inProgressWUs.insert(T("1299672901904"));
+  state->inProgressWUs.insert(T("1299672903018"));
+  state->inProgressWUs.insert(T("1299672904089"));
+  state->inProgressWUs.insert(T("1299672905175"));
+  state->inProgressWUs.insert(T("1299672906244"));
+  state->inProgressWUs.insert(T("1299672907319"));
+  state->inProgressWUs.insert(T("1299672908395"));
+  state->inProgressWUs.insert(T("1299672910479"));*/
+
+  
   // handle finished WU's
   while (state->currentEvaluatedWUs.size() < 10) {
-    juce::Thread::sleep(1000);
+    juce::Thread::sleep(5000);
+    
+    NetworkClientPtr client = blockingNetworkClient(context);
+    if (!client->startClient(managerHostName, managerPort))
+    {
+      context.errorCallback(T("SendWorkUnit::run"), T("Not connected !"));
+      return false;
+    }
+    context.informationCallback(managerHostName, T("Connected !"));
+    
+    ManagerNodeNetworkInterfacePtr interface = new ClientManagerNodeNetworkInterface(context, client, source);
+    interface->sendInterfaceClass();
+    
     std::set<String>::iterator it;
     for(it = state->inProgressWUs.begin(); it != state->inProgressWUs.end(); )
     {
-      File f(String("/Users/arnaudschoofs/Proteins/traces/") + *it + T(".trace"));
+      
+      if(interface->isFinished(*it))
+      {
+        ExecutionTracePtr trace = interface->getExecutionTrace(*it)->getExecutionTrace(context);
+        File f(T("/Users/arnaudschoofs/Proteins/traces/") + (*it) + T(".trace"));
+        trace->saveToFile(context,f);
+        XmlImporter importer(context, f);
+        if (!importer.isOpened())
+          {jassertfalse;}
+        if (!importer.enter("shared"))
+          {jassertfalse;}
+        double score = ((importer.getCurrentElement())->getNextElement()->getChildElementAllSubText(String("variable"), String("error"))).getDoubleValue();
+        state->currentEvaluatedWUs.insert(std::pair<double, String>(score,*it));
+        state->inProgressWUs.erase(it++);
+      }
+      else 
+        ++it;
+      
+      /*File f(String("/Users/arnaudschoofs/Proteins/traces/") + *it + T(".trace"));
       if (f.existsAsFile())
       {
         XmlImporter importer(context, f);
@@ -112,12 +168,15 @@ Variable ProteinGridEvoOptimizer::optimize(ExecutionContext& context, const Func
         state->currentEvaluatedWUs.insert(std::pair<double, String>(score,*it));
         state->inProgressWUs.erase(it++);
       }
-      else ++it;
+      else ++it;*/
+  
     }
   }
-    
+  
+  
+
   // TODO arnaud : select best results, update distributions
-  size_t nb = 0;
+  /*size_t nb = 0;
   std::multimap<double, String>::reverse_iterator it;
   for ( it=state->currentEvaluatedWUs.rbegin(); it != state->currentEvaluatedWUs.rend() && nb < 5; it++ )
   {
@@ -133,7 +192,6 @@ Variable ProteinGridEvoOptimizer::optimize(ExecutionContext& context, const Func
   
   IndependentMultiVariateDistributionPtr newDistri = state->distributionsBuilder->build(context);
   std::cout << newDistri->sample(RandomGenerator::getInstance()).toString() << std::endl;
-  
   */
    
   return Variable(0); 
