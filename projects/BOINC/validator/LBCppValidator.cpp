2ecff611200ec033fbb37702c4d0bdbf2cd35baf
arnaud <arnaud@885cb74b-98ed-4c74-c960-63734602d729>
2011-03-17 00:20:12 +0000

    '''EvoOptimizer'''  * comments added

--- projects/BOINC/validator/LBCppValidator.cpp
+++ projects/BOINC/validator/LBCppValidator.cpp
@@ -132,7 +132,7 @@ int compare_results(RESULT & r1, void* data1, RESULT const& r2, void* data2, boo
   }
 
   for (unsigned i = 0; i < scores1->size(); ++i)
-    if (fabs(scores1->at(i) - scores2->at(i)) > 0.00001)
+    if (fabs(scores1->at(i) - scores2->at(i)) > 0.0001)
     {
       match = false;
       return 0;
