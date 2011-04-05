1c642d8fc4a265627c71e22d18fb1e29cfb0ae0d
arnaud <arnaud@885cb74b-98ed-4c74-c960-63734602d729>
2011-04-05 12:24:19 +0000

    '''DebugWorkUnit'''  * debug work unit to track "Conditional jump or move depends on uninitialised value"

---
+++ projects/Proteins/WorkUnit/DebugWorkUnit.h
@@ -0,0 +1,40 @@
+/*
+ *  DebugWorkUnit.h
+ *  LBCpp
+ *
+ *  Created by Arnaud Schoofs on 5/04/11.
+ *  Copyright 2011 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#ifndef LBCPP_DEBUG_WORK_UNIT_H_
+# define LBCPP_DEBUG_WORK_UNIT_H_
+
+# include <lbcpp/lbcpp.h>
+# include "../Optimizer/ProteinGridEvoOptimizer.h"
+
+namespace lbcpp
+{
+class DebugWorkUnit : public WorkUnit
+{
+public:
+  virtual Variable run(ExecutionContext& context) 
+  {
+  
+    ProteinGridEvoOptimizerStatePtr state = Object::createFromFile(context, File::getCurrentWorkingDirectory().getChildFile(T("GridEvoOptimizerState.xml"))).staticCast<ProteinGridEvoOptimizerState>();
+    foo(context, state);
+    return Variable();
+  }
+  
+private:
+  void foo(ExecutionContext& context, const GridOptimizerStatePtr& state_)
+  {
+    GridEvoOptimizerStatePtr state = state_.dynamicCast<GridEvoOptimizerState>();
+    state->saveToFile(context, File::getCurrentWorkingDirectory().getChildFile(T("GridEvoOptimizerState.xml")));
+  }
+};
+
+};
+
+
+#endif //!LBCPP_DEBUG_WORK_UNIT_H_
