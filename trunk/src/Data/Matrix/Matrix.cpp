f8a414926cb28ed3ec5068bb318861d14ecd7bf3
francis <francis@885cb74b-98ed-4c74-c960-63734602d729>
2011-04-05 21:59:14 +0000

    '''Serialize GoStates'''
     * generic Matrix::loadFromXml() and saveToXml() functions

--- trunk/src/Data/Matrix/Matrix.cpp
+++ trunk/src/Data/Matrix/Matrix.cpp
@@ -6,6 +6,7 @@
                                |                                             |
                                `--------------------------------------------*/
 #include "precompiled.h"
+#include <lbcpp/Core/XmlSerialisation.h>
 #include <lbcpp/Data/Matrix.h>
 #include <lbcpp/Data/SymmetricMatrix.h>
 using namespace lbcpp;
@@ -25,6 +26,21 @@ String Matrix::toString() const
   
 String Matrix::toShortString() const
   {return String((int)getNumRows()) + T(" x ") + String((int)getNumColumns()) + T(" matrix");}
+
+void Matrix::saveToXml(XmlExporter& exporter) const
+{
+  Container::saveToXml(exporter);
+  exporter.setAttribute(T("numRows"), getNumRows());
+  exporter.setAttribute(T("numColumns"), getNumColumns());
+}
+
+bool Matrix::loadFromXml(XmlImporter& importer)
+{
+  size_t numRows = (size_t)importer.getIntAttribute(T("numRows"));
+  size_t numColumns = (size_t)importer.getIntAttribute(T("numColumns"));
+  setSize(numRows, numColumns);
+  return Container::loadFromXml(importer);
+}
 
 /* Matrix Constructor Method */
 namespace lbcpp
