69c127cabc2cf05602b17c050532428b98293ac8
francis <francis@885cb74b-98ed-4c74-c960-63734602d729>
2011-06-25 00:16:35 +0000

    '''Lua Code Editor'''
     * LuaCodeEditor: select some code, do ctrl + enter => the code is directly executed

--- explorer/Components/LuaCodeEditorComponent.h
+++ explorer/Components/LuaCodeEditorComponent.h
@@ -13,16 +13,18 @@
 
 # include <lbcpp/UserInterface/VariableSelector.h>
 # include <lbcpp/UserInterface/ObjectEditor.h>
+# include <lbcpp/Lua/Lua.h>
 # include "../../juce/juce_CodeEditorComponent.h"
 # include "../../juce/juce_LuaCodeTokeniser.h"
+# include "../Explorer/ExplorerProject.h"
 
 namespace lbcpp
 {
 
-class LuaCodeEditorComponent : public Component, public ComponentWithPreferedSize
+class LuaCodeEditorComponent : public Component, public ComponentWithPreferedSize, public VariableSelector
 {
 public:
-  LuaCodeEditorComponent(const File& luaFile)
+  LuaCodeEditorComponent(const File& luaFile) : name(luaFile.getFileName()), context(ExplorerProject::currentProject->workUnitContext), luaState(*context)
   {
     InputStream* istr = luaFile.createInputStream();
     if (istr)
@@ -35,7 +37,7 @@ public:
       document.replaceAllContent(luaCode);
     }
     else
-      defaultExecutionContext().errorCallback(T("Could not open file ") + luaFile.getFullPathName());
+      context->errorCallback(T("Could not open file ") + luaFile.getFullPathName());
 
     addAndMakeVisible(codeEditor = new juce::CodeEditorComponent(document, &tokeniser));
   }
@@ -46,10 +48,44 @@ public:
   virtual void resized()
     {codeEditor->setBoundsRelative(0, 0, 1, 1);}
 
+  void executeSelectedCode()
+  {
+    String code = codeEditor->getSelectedText().trim();
+    if (code.isEmpty())
+      code = document.getAllContent().trim();
+    if (code.isEmpty())
+      return;
+
+    if (!trace)
+    {
+      trace = new ExecutionTrace(context->toString());
+      sendSelectionChanged(trace, name);
+    }
+
+    luaState.execute(code, name);
+  }
+ 
+  virtual bool keyPressed(const juce::KeyPress& key)
+  {
+    if (key.getKeyCode() == juce::KeyPress::returnKey && key.getModifiers().isCommandDown())
+    {
+      executeSelectedCode();
+      return true;
+    }
+    return false;
+  }
+ 
+  virtual juce::Component* createComponentForVariable(ExecutionContext& context, const Variable& variable, const String& name)
+    {return userInterfaceManager().createExecutionTraceInteractiveTreeView(context, trace, this->context);}
+
 protected:
+  String name;
+  ExecutionContextPtr context;  
   juce::CodeEditorComponent* codeEditor;
   juce::CodeDocument document;
   juce::LuaCodeTokeniser tokeniser;
+  ExecutionTracePtr trace;
+  LuaState luaState;
 };
 
 }; /* namespace lbcpp */
