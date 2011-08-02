6b322d34cebbd29e58e8c99d437fea8ad3f8c05a
francis <francis@885cb74b-98ed-4c74-c960-63734602d729>
2011-08-02 12:09:16 +0000

    '''Lua wrapper for ScalarVariableStatistics'''
     * New Lua functions: DoubleVector::l0norm() and DoubleVector::l1norm()
     * New Lua file: Statistics.lua
     * New Lua functions in ScalarVariableStatistics: getMean(), getStandardDeviation(),
                          getVariance(), getMinimum(), getMaximum(), getSum(), getCount()
     * New Lua functions in ScalarVariableStatistics: observe() and clear()

--- projects/Lua/lib/SandBox.lua
+++ projects/Lua/lib/SandBox.lua
@@ -3,7 +3,7 @@ require 'Dictionary'
 require 'Parser'
 require 'Data'
 require 'Context'
-
+require 'Statistics'
 filename = "C:/Projets/lbcpp/projects/Examples/Data/BinaryClassification/a1a.test"
 labels = Dictionary.new()
 examples = Data.load(Parser.libSVMClassification, 100, filename, labels)
@@ -16,15 +16,35 @@ print (#examples .. " examples, " .. labels:size() .. " labels")
 --  end
 --end
 
+
 function learnBinaryClassifier(examples)
 
+  -- parameters
+  --parameter rate = {default = function (i) return 1 end}
+  --parameter normalizeRate = {default = true}
+  local rate = function (i) return 1 end
+  local normalizeRate = true
+  --
+
   local parameters = Vector.newDense()
+  local epoch = 0
+  local numExamples = #examples
+
+  local rateNormalizer = 1
+  if normalizeRate then
+    -- make statistics on features sparsity
+    local featureStats = Statistics.meanAndVariance()
+    for i=1,numExamples do
+      featureStats:observe(examples[i][1]:l0norm())
+    end
+    context:result("featureStats", featureStats)
+    rateNormalizer = 1 / featureStats:getMean()
+  end
 
   local function iteration(parameters, iteration)
     context:result("iteration", iteration)
 
     local totalHingeLoss = 0.0
-    local numExamples = #examples
     for i=1,#examples do
       local example = examples[i]
        --print (parameters, example[1])
@@ -34,12 +54,16 @@ function learnBinaryClassifier(examples)
       local hingeLoss = math.max(0, 1 - score)
       totalHingeLoss = totalHingeLoss + hingeLoss
       if score < 1 then
-        parameters:add(example[1], 0.001 * sign)
+        parameters:add(example[1], rateNormalizer * rate(epoch) * sign)
       end
+      epoch = epoch + 1
     end
     context:result("mean hinge loss", totalHingeLoss / numExamples)
-    context:result("parameters norm", parameters:l2norm())
+    context:result("parameters l0norm", parameters:l0norm())
+    context:result("parameters l1norm", parameters:l1norm())
+    context:result("parameters l2norm", parameters:l2norm())
     context:result("parameters", parameters:clone())
+    context:result("epoch", epoch)
   end
 
   for i = 1,10 do
---
+++ projects/Lua/lib/Statistics.lua
@@ -0,0 +1,36 @@
+-- Francis Maes, 01/08/2011
+-- Incremental Statistics Counters
+
+--[[
+Interface:
+ 
+  Statistics.mean()
+  Statistics.meanAndVariance()
+  Statistics.meanVarianceAndBounds()
+  
+  Statistics:observe(value, weight = 1.0)
+  Statistics:clear()
+  
+  Statistics:getMean()              -- sum of values / sum of weights
+  Statistics:getStandardDeviation() -- standard deviation
+  Statistics:getVariance()          -- variance
+  Statistics:getMinimum()           -- minimum observed value
+  Statistics:getMaximum()           -- maximum observed value
+  Statistics:getSum()               -- sum of values
+  Statistics:getCount()             -- sum of weights
+  
+]]
+
+module("Statistics", package.seeall)
+
+function mean() 
+  return lbcpp.Object.create("ScalarVariableMean")
+end
+
+function meanAndVariance()
+  return lbcpp.Object.create("ScalarVariableMeanAndVariance")
+end
+
+function meanVarianceAndBounds()
+  return lbcpp.Object.create("ScalarVariableStatistics")
+end
