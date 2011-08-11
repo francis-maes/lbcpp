24f5d786dd6c65cdee6b9cc206f7e5b55fd94979
francis <francis@885cb74b-98ed-4c74-c960-63734602d729>
2011-08-11 14:51:42 +0000

    lua eda

--- trunk/projects/Lua/Tests/SimpleEDA.lua
+++ trunk/projects/Lua/Tests/SimpleEDA.lua
@@ -2,15 +2,15 @@ require 'Statistics'
 require 'Sampler'
 require 'Vector'
 require 'Optimizer'
-
+require 'StoppingCriterion'
 
 objective = |input| input:l1norm()
 
-eda = Optimizer.EstimationOfDistributionAlgorithm{numIterations=10, numCandidates=100, numBests=10}
+crit = StoppingCriterion.MaxIterationsWithoutImprovement{}
+eda = Optimizer.EstimationOfDistributionAlgorithm{stoppingCriterion = crit, maxIterations=0, numCandidates=100, numBests=10}
 
 local samplers = {}
-for i=1,100 do
+for i=1,5 do
   samplers[i] = Sampler.Gaussian{}
 end
 context:call("EDA", eda, objective, Sampler.IndependentVector{samplers = samplers})
-
