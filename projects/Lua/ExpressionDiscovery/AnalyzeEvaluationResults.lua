c1670baa3c4aa9600c3070aeef49c4a0459398ba
francis <francis@885cb74b-98ed-4c74-c960-63734602d729>
2011-09-03 13:48:34 +0000

    hop

--- projects/Lua/ExpressionDiscovery/AnalyzeEvaluationResults.lua
+++ projects/Lua/ExpressionDiscovery/AnalyzeEvaluationResults.lua
@@ -1,7 +1,9 @@
-local filename = "results.txt"
+local filename = "bandits_hor1000_size6.txt"
 
 local bestScore = math.huge
 local bestSolution
+
+local allScores = {}
 
 for line in io.lines(filename) do
   local scoreString = string.match(line, "%d+%.%d*")
@@ -11,6 +13,7 @@ for line in io.lines(filename) do
     
     local score = tonumber(scoreString)
     local solution = remainingLine
+    table.insert(allScores, {score=score, solution=solution})
     --print (score, solution)
     if score < bestScore then
       bestScore = score
@@ -19,4 +22,13 @@ for line in io.lines(filename) do
   end
 end
 
-print ("Best score:", bestScore, "Best solution:", bestSolution)
+table.sort(allScores, |a,b| a.score < b.score)
+
+local i = 1
+for k,v in ipairs(allScores) do
+  print (v.solution .. " [" .. v.score .. "]")
+  i = i + 1
+  if i == 10 then break end
+end
+
+--print ("Best score:", bestScore, "Best solution:", bestSolution)
