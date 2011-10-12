19133e044757d3c66a18a28e976968af5e038701
francis <francis@885cb74b-98ed-4c74-c960-63734602d729>
2011-10-12 09:08:11 +0000

    New CompareBanditFormulaSearchPolicies experiment

--- projects/SequentialDecision/GP/BanditFormulaSearchToLatex.lua
+++ projects/SequentialDecision/GP/BanditFormulaSearchToLatex.lua
@@ -4,13 +4,6 @@ local numBests = 5
 local outputFile = "BanditFormulaSearchTest.tex"
 
 
-local trace = lbcpp.Object.fromFile(traceFile)
-local traceRoot = trace.root.subItems[1]
-local traceRootItems = traceRoot.subItems
-
-local message = traceRootItems[1]
-print (message.what)
-
 local function getAttributes(node)
   local res = {}
   local n = #(node.results)
@@ -21,16 +14,49 @@ local function getAttributes(node)
   return res
 end
 
+local function findSubNode(node, description)
+  local n = #(node.subItems)
+  for i=1,n do
+    if node.subItems[i]:toShortString() == description then
+      return node.subItems[i]
+    end
+  end
+  return nil
+end
+
+local trace = lbcpp.Object.fromFile(traceFile)
+local traceRoot = trace.root.subItems[1]
+local traceRootItems = traceRoot.subItems
+
+local message = traceRootItems[1]
+print (message.what)
+local rootAttributes = getAttributes(traceRoot)
+
+
 local function regretToLatex(regret)
   return string.format(regret > 10 and "%.1f" or (regret > 1 and "%.2f" or "%.3f"), regret)
 end
 
 local function formulaToLatex(formula)
 
+  local exprToLatex
+
   local variables = {"\\RewardMean", "\\RewardStddev", "\\PlayedCount", "\\TimeStep"} 
-  local unaryOperators = {"id", "opp", "inv", "sqrt", "log", "exp", "abs"}
+  local unaryOperators = {"id", "opp", "inv", "sqrt", "ln", "exp", "abs"}
   local binaryOperators = {"add", "sub", "mul", "div", "min", "max", "pow", "lt"}
-  local function exprToLatex(expr)
+
+  local function exprToLatexWithParent(expr)
+    local res = exprToLatex(expr)
+    if expr.className == "BinaryGPExpression" then
+      local op = binaryOperators[1+expr.op]
+      if op == "add" or op == "sub" then
+        res = "(" .. res .. ")"
+      end
+    end
+    return res
+  end  
+
+  exprToLatex =  function (expr)
     local cn = expr.className
     if cn == "ConstantGPExpression" then
       return tostring(expr.value)
@@ -48,50 +74,85 @@ local function formulaToLatex(formula)
       end
     elseif cn == "BinaryGPExpression" then
       local op = binaryOperators[1+expr.op]
-      local left = exprToLatex(expr.left)
-      local right = exprToLatex(expr.right)
+      local left = exprToLatexWithParent(expr.left)
+      local right = exprToLatexWithParent(expr.right)
       if op == "add" then
         return left .. " + " .. right
       elseif op == "sub" then
         return left .. " - " .. right
       elseif op == "mul" then
-        return left .. right
+        if expr.right.className == "ConstantGPExpression" then
+          return right .. " " .. left
+        else
+          return left .. " " .. right
+        end
       elseif op == "div" then
-        return "\\frac{" .. left .. "}{" .. right .. "}"
+        return left .. " / " .. right
+        --return "\\frac{" .. left .. "}{" .. right .. "}"
       else
-        return op .. "(" .. left .. ", " .. right .. ")"
+        return op .. "(" .. exprToLatex(expr.left) .. ", " .. exprToLatex(expr.right) .. ")" -- no parenthesis
       end
     end
   end
 
-  return "\n\\scriptsize{$" .. exprToLatex(formula) .. "$}"
+  return "$" .. exprToLatex(formula) .. "$"
 end
 
-local function entryToLatex(rank, formula, meanRegret, meanReward, playedCount, target)
 
-  table.insert(target, formulaToLatex(formula))
-  table.insert(target, regretToLatex(meanRegret))
-  --table.insert(target, tostring(playedCount))
+local function baselineToLatex(baseline)
+  if baseline == "uniform" then
+    return "\\textsc{Uniform}"
+  elseif baseline == "greedy" then
+    return "\\textsc{Greedy}"
+  elseif baseline == "ucb1(2)" then
+    return "\\ucbone"
+  elseif baseline == "ucb1Bernoulli" then
+    return "\\ucbonetuned"
+  elseif baseline == "klUcb(0)" then
+    return "\\klucb"
+  else
+    return baseline
+  end
 end
 
-local function resultsNodeToLatex(results, target)
+local function entryToLatex(attributes, target, doBaselines)
+
+  if doBaselines then
+    table.insert(target, baselineToLatex(attributes.baseline))
+  else
+    table.insert(target, formulaToLatex(attributes.formula))
+  end
+  table.insert(target, regretToLatex(attributes.meanRegret))
+  table.insert(target, regretToLatex(attributes.regretStddev))
+  if doBaselines then
+    table.insert(target, "")
+  else
+    table.insert(target, tostring(attributes.playedCount))
+  end
+end
+
+local function resultsNodeToLatex(results, target, doBaselines, count)
 
   local desc = results.description
-  print (desc)
+  print ("Processing: " .. desc)
 
-  for i=1,numBests do
-    local attr = getAttributes(results.subItems[i])
-    entryToLatex(attr.rank, attr.formula, attr.meanRegret, attr.meanReward, attr.playedCount, target[i])
+  local first = doBaselines and 9 or 0
+  for i=1,count do
+    local attr = getAttributes(results.subItems[first + i])
+    entryToLatex(attr, target[i], doBaselines)
   end  
 end
 
-local lines = {}
-for i=1,numBests do
-  table.insert(lines, {tostring(i)})
-end
+local function resultsNodePairToLatex(results1, results2, doBaselines, count)
 
-for i=2,#traceRootItems do
-  resultsNodeToLatex(traceRootItems[i], lines)  
+  local lines = {}
+  for i=1,count do
+    local rank = doBaselines and "" or tostring(i)
+    table.insert(lines, {rank})
+  end
+  resultsNodeToLatex(results1, lines, doBaselines, count)
+  resultsNodeToLatex(results2, lines, doBaselines, count)
+  return lines 
 end
 
 
@@ -99,24 +160,43 @@ local f = assert(io.open(outputFile, "w"))
 local function writeLine(line)
   f:write(line)
   f:write("\n")
-  print (line)
+  print (">>" .. line)
 end
 
-
-writeLine("\\begin{tabular}{|c|c|c|c|c|c|c|c|c|}")
+writeLine("\\begin{tabular}{|c||c|cc|c||c|cc|c|}")
 writeLine("\\hline")
-writeLine(" & \\multicolumn{4}{c|}{Cumulative Regret} & \\multicolumn{4}{c|}{Simple Regret} \\\\")
-writeLine(" & \\multicolumn{2}{c|}{Mean} & \\multicolumn{2}{c|}{Worst} & \\multicolumn{2}{c|}{Mean} & \\multicolumn{2}{c|}{Worst} \\\\")
+writeLine(" & \\multicolumn{4}{c||}{Cumulative Regret} & \\multicolumn{4}{c|}{Simple Regret} \\\\")
 writeLine("\\hline")
-writeLine("Rank & Formula & Regret & Formula & Regret & Formula & Regret & Formula & Regret\\\\")
+writeLine("Rank & Formula & Regret & stddev. & $\\PlayedCount$ & Formula & Regret & stddev. & $\\PlayedCount$ \\\\")
 writeLine("\\hline")
 
-for lineNumber,lineParts in ipairs(lines) do
-  local line = table.concat(lineParts, " & ") .. "\\\\"
-  writeLine(line)
+local horizon = rootAttributes.minHorizon
+
+while horizon <= rootAttributes.maxHorizon do
+  
+  writeLine("\\multicolumn{9}{|c|}{Horizon " .. horizon .. "} \\\\")
+  writeLine("\\hline")
+  local resultsNode1 = findSubNode(traceRoot, "Horizon " .. horizon .. " Cumulative Regret")
+  local resultsNode2 = findSubNode(traceRoot, "Horizon " .. horizon .. " Simple Regret")
+  assert(resultsNode1)
+  assert(resultsNode2)
+  local lines = resultsNodePairToLatex(resultsNode1, resultsNode2, false, numBests)
+  for lineNumber,lineParts in ipairs(lines) do
+    local line = table.concat(lineParts, " & ") .. "\\\\"
+    writeLine(line)
+  end
+  writeLine("\\hline")
+
+  lines = resultsNodePairToLatex(resultsNode1, resultsNode2, true, 5)
+  for lineNumber,lineParts in ipairs(lines) do
+    local line = table.concat(lineParts, " & ") .. "\\\\"
+    writeLine(line)
+  end
+  writeLine("\\hline")
+
+  horizon = horizon * 10
 end
 
-writeLine("\\hline")
 writeLine("\\end{tabular}")
 
 f:close()
