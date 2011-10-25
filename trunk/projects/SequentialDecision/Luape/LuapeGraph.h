eb2d5a845f48e675a5b858d0c5a3230c693068e7
francis <francis@885cb74b-98ed-4c74-c960-63734602d729>
2011-10-25 17:55:02 +0000

    '''Luape AdaBoost'''
     * New files: LuapeBatchLearner.h, LuapeGraph.h and LuapeGraphBuilder.h
     * New abstract class LuapeFunction and implementation LuapeBinaryClassifier
     * Make LuapeBatchLearner inherit from BatchLearner
     * Make AdaBoost work and evaluate on train and test data systematically

---
+++ trunk/projects/SequentialDecision/Luape/LuapeGraph.h
@@ -0,0 +1,399 @@
+/*-----------------------------------------.---------------------------------.
+| Filename: LuapeGraph.h                   | Luape Graph                     |
+| Author  : Francis Maes                   |                                 |
+| Started : 25/10/2011 18:49               |                                 |
+`------------------------------------------/                                 |
+                               |                                             |
+                               `--------------------------------------------*/
+
+#ifndef LBCPP_LUAPE_GRAPH_H_
+# define LBCPP_LUAPE_GRAPH_H_
+
+namespace lbcpp
+{
+
+class LuapeGraph;
+typedef ReferenceCountedObjectPtr<LuapeGraph> LuapeGraphPtr;
+
+
+class LuapeGraphCallback
+{
+public:
+  virtual ~LuapeGraphCallback() {}
+
+  virtual void valueYielded(const Variable& value) {}
+};
+
+typedef LuapeGraphCallback* LuapeGraphCallbackPtr;
+
+
+class LuapeNodeCache : public Object
+{
+public:
+  LuapeNodeCache() : convertibleToDouble(false), score(0.0) {}
+
+  void initialize(TypePtr type)
+  {
+    examples = vector(type, 0);
+    convertibleToDouble = type->isConvertibleToDouble();
+  }
+  
+  void reserveExamples(size_t count)
+    {examples->reserve(count);}
+
+  void addExample(const Variable& value)
+  {
+    jassert(examples);
+    jassert(value.exists());
+
+    examples->append(value);
+    if (convertibleToDouble)
+      doubleValues.insert(value.toDouble());
+  }
+
+  size_t getNumExamples() const
+    {return examples->getNumElements();}
+
+  Variable getExample(size_t index) const
+    {return examples->getElement(index);}
+
+  const VectorPtr& getExamples() const
+    {return examples;}
+
+  void setScore(double score)
+    {this->score = score;}
+
+  double getScore() const
+    {return score;}
+
+protected:
+  VectorPtr examples;
+  bool convertibleToDouble;
+  std::set<double> doubleValues; // only if isConvertibleToDouble
+  double score; // only for 'yield' nodes
+};
+
+typedef ReferenceCountedObjectPtr<LuapeNodeCache> LuapeNodeCachePtr;
+
+
+//////  GRAPH NODES
+
+class LuapeNode;
+typedef ReferenceCountedObjectPtr<LuapeNode> LuapeNodePtr;
+
+class LuapeNode : public NameableObject
+{
+public:
+  LuapeNode(const TypePtr& type, const String& name)
+    : NameableObject(name), type(type) {}
+  LuapeNode() {}
+
+  const TypePtr& getType() const
+    {return type;}
+
+  virtual bool initialize(ExecutionContext& context, const std::vector<LuapeNodePtr>& allNodes)
+  {
+    if (!type)
+      return false;
+    if (!cache)
+    {
+      cache = new LuapeNodeCache();
+      cache->initialize(type);
+    }
+    return true;
+  }
+
+  virtual Variable compute(ExecutionContext& context, const std::vector<Variable>& state, LuapeGraphCallbackPtr callback) const = 0;
+
+  VariableSignaturePtr getSignature() const
+    {return new VariableSignature(type, name);}
+
+  void reserveExamples(size_t count)
+    {jassert(cache); cache->reserveExamples(count);}
+
+  void addExample(const Variable& value)
+  {
+    jassert(cache);
+    jassert(value.getType()->inheritsFrom(type));
+    cache->addExample(value);
+  }
+
+  const LuapeNodeCachePtr& getCache() const
+    {return cache;}
+
+  virtual void clone(ExecutionContext& context, const ObjectPtr& t) const
+  {
+    const LuapeNodePtr& target = t.staticCast<LuapeNode>();
+    target->name = name;
+    target->type = type;
+    target->cache = cache;
+  }
+
+protected:
+  friend class LuapeNodeClass;
+
+  TypePtr type;
+  LuapeNodeCachePtr cache;
+};
+
+extern ClassPtr luapeNodeClass;
+
+class LuapeInputNode : public LuapeNode
+{
+public:
+  LuapeInputNode(const TypePtr& type, const String& name)
+    : LuapeNode(type, name) {}
+  LuapeInputNode() {}
+
+  virtual String toShortString() const
+    {return T("input ") + getName() + T(" (type = ") + type->getName() + T(")");}
+
+  virtual Variable compute(ExecutionContext& context, const std::vector<Variable>& state, LuapeGraphCallbackPtr callback) const
+    {jassert(false); return Variable();}
+};
+
+typedef ReferenceCountedObjectPtr<LuapeInputNode> LuapeInputNodePtr;
+
+class LuapeFunctionNode;
+typedef ReferenceCountedObjectPtr<LuapeFunctionNode> LuapeFunctionNodePtr;
+
+class LuapeFunctionNode : public LuapeNode
+{
+public:
+  LuapeFunctionNode(const FunctionPtr& function, const std::vector<size_t>& arguments)
+    : function(function->cloneAndCast<Function>()), arguments(arguments) {}
+  LuapeFunctionNode() {}
+
+  virtual String toShortString() const
+  {
+    String res = function->toShortString() + T("(");
+    for (size_t i = 0; i < arguments.size(); ++i)
+    {
+      res += String((int)arguments[i]);
+      if (i < arguments.size() - 1)
+        res += T(", ");
+    }
+    return res + T(")");
+  }
+
+  virtual bool initialize(ExecutionContext& context, const std::vector<LuapeNodePtr>& allNodes)
+  {
+    std::vector<VariableSignaturePtr> inputs(arguments.size());
+    inputNodes.resize(arguments.size());
+    for (size_t i = 0; i < inputs.size(); ++i)
+    {
+      inputNodes[i] = allNodes[arguments[i]];
+      inputs[i] = inputNodes[i]->getSignature();
+    }
+    if (!function->initialize(context, inputs))
+      return false;
+    type = function->getOutputType();
+    name = function->getOutputVariable()->getName();
+    if (!LuapeNode::initialize(context, allNodes))
+      return false;
+    propagateCache(context);
+    return true;
+  }
+
+  virtual Variable compute(ExecutionContext& context, const std::vector<Variable>& state, LuapeGraphCallbackPtr callback) const
+  {
+    size_t n = arguments.size();
+    std::vector<Variable> inputs(n);
+    for (size_t i = 0; i < n; ++i)
+      inputs[i] = state[arguments[i]];
+    return function->compute(context, inputs);
+  }
+
+  
+  virtual void clone(ExecutionContext& context, const ObjectPtr& t) const
+  {
+    LuapeNode::clone(context, t);
+    const LuapeFunctionNodePtr& target = t.staticCast<LuapeNode>();
+    target->function = function;
+    target->arguments = arguments;
+  }
+
+protected:
+  friend class LuapeFunctionNodeClass;
+
+  FunctionPtr function;
+  std::vector<size_t> arguments;
+
+  std::vector<LuapeNodePtr> inputNodes;
+
+  void propagateCache(ExecutionContext& context)
+  {
+    jassert(inputNodes.size());
+    size_t minCacheSize = inputNodes[0]->getCache()->getNumExamples();
+    for (size_t i = 1; i < inputNodes.size(); ++i)
+    {
+      size_t cacheSize = inputNodes[i]->getCache()->getNumExamples();
+      if (cacheSize < minCacheSize)
+        minCacheSize = cacheSize;
+    }
+
+    size_t currentSize = cache->getNumExamples();
+    if (minCacheSize > currentSize)
+    {
+      cache->reserveExamples(minCacheSize);    
+      for (size_t i = currentSize; i < minCacheSize; ++i)
+      {
+        std::vector<Variable> inputs(inputNodes.size());
+        for (size_t j = 0; j < inputs.size(); ++j)
+          inputs[j] = inputNodes[j]->getCache()->getExample(i);
+        cache->addExample(function->compute(context, inputs));
+      }
+    }
+  }
+};
+
+class LuapeYieldNode : public LuapeNode
+{
+public:
+  LuapeYieldNode(size_t argument = 0)
+    : argument(argument) {}
+
+  virtual String toShortString() const
+    {return T("yield(") + String((int)argument) + T(")");}
+
+  virtual bool initialize(ExecutionContext& context, const std::vector<LuapeNodePtr>& allNodes)
+  {
+    inputNode = allNodes[argument];
+    type = nilType;
+    name = inputNode->getName();
+    return LuapeNode::initialize(context, allNodes);
+  }
+
+  size_t getArgument() const
+    {return argument;}
+
+  virtual Variable compute(ExecutionContext& context, const std::vector<Variable>& state, LuapeGraphCallbackPtr callback) const
+  {
+    if (callback)
+      callback->valueYielded(state[argument]);
+    return state[argument];
+  }
+
+  virtual void clone(ExecutionContext& context, const ObjectPtr& t) const
+  {
+    LuapeNode::clone(context, t);
+    t.staticCast<LuapeYieldNode>()->argument = argument;
+  }
+
+protected:
+  friend class LuapeYieldNodeClass;
+
+  size_t argument;
+  LuapeNodePtr inputNode;
+};
+
+typedef ReferenceCountedObjectPtr<LuapeYieldNode> LuapeYieldNodePtr;
+
+//////  GRAPH
+
+class LuapeGraph : public Object
+{
+public:
+  LuapeGraph() : numExamples(0) {}
+
+  virtual String toShortString() const
+    {return graphToString(0);}
+  
+  String graphToString(size_t firstNodeIndex = 0) const
+  {
+    String res;
+    for (size_t i = firstNodeIndex; i < nodes.size(); ++i)
+      res += String((int)i) + T(" ") + nodes[i]->toShortString() + T("\n");
+    return res;
+  }
+
+  size_t getNumNodes() const
+    {return nodes.size();}
+
+  LuapeNodePtr getNode(size_t index) const
+    {jassert(index < nodes.size()); return nodes[index];}
+
+  TypePtr getNodeType(size_t index) const
+    {return getNode(index)->getType();}
+
+  LuapeNodePtr getLastNode() const
+    {return nodes.back();}
+
+  bool pushNode(ExecutionContext& context, const LuapeNodePtr& node)
+  {
+    if (!node->initialize(context, nodes))
+      return false;
+    nodes.push_back(node);
+    return true;
+  }
+
+  void popNode()
+    {jassert(nodes.size()); nodes.pop_back();}
+
+  void reserveExamples(size_t count)
+  {
+    for (size_t i = 0; i < nodes.size(); ++i)
+      nodes[i]->reserveExamples(count);
+  }
+
+  void addExample(const std::vector<Variable>& example)
+  {
+    jassert(example.size() <= nodes.size());
+    for (size_t i = 0; i < example.size(); ++i)
+    {
+      LuapeInputNodePtr node = nodes[i].dynamicCast<LuapeInputNode>();
+      jassert(node);
+      node->addExample(example[i]);
+    }
+    ++numExamples;
+  }
+
+  void addExample(const ContainerPtr& example)
+  {
+    size_t n = example->getNumElements();
+    jassert(n <= nodes.size());
+    for (size_t i = 0; i < n; ++i)
+    {
+      LuapeInputNodePtr node = nodes[i].dynamicCast<LuapeInputNode>();
+      jassert(node);
+      node->addExample(example->getElement(i));
+    }
+    ++numExamples;
+  }
+
+  void compute(ExecutionContext& context, std::vector<Variable>& state, size_t firstNodeIndex = 0, LuapeGraphCallbackPtr callback = 0) const
+  {
+    for (size_t i = firstNodeIndex; i < nodes.size(); ++i)
+      state[i] = nodes[i]->compute(context, state, callback);
+  }
+
+  virtual void clone(ExecutionContext& context, const ObjectPtr& t) const
+  {
+    const LuapeGraphPtr& target = t.staticCast<LuapeGraph>();
+    target->numExamples = numExamples;
+    size_t n = nodes.size();
+    target->nodes.reserve(n);
+    for (size_t i = 0; i < n; ++i)
+      target->pushNode(context, nodes[i]->cloneAndCast<LuapeNode>());      
+  }
+
+protected:
+  friend class LuapeGraphClass;
+
+  std::vector<LuapeNodePtr> nodes;
+  size_t numExamples;
+};
+
+extern ClassPtr luapeGraphClass;
+
+////////// CACHE
+
+class LuapeGraphCache : public Object
+{
+};
+
+typedef ReferenceCountedObjectPtr<LuapeGraphCache> LuapeGraphCachePtr;
+
+}; /* namespace lbcpp */
+
+#endif // !LBCPP_LUAPE_GRAPH_H_
--- trunk/projects/SequentialDecision/Luape/LuapeSandBox.h
+++ trunk/projects/SequentialDecision/Luape/LuapeSandBox.h
@@ -11,6 +11,7 @@
 
 # include <lbcpp/Data/Stream.h>
 # include "LuapeProblem.h"
+# include "LuapeBatchLearner.h"
 
 namespace lbcpp
 {
@@ -51,236 +52,10 @@ protected:
   double threshold;
 };
 
-
-/////////// LuapeLearner
-
-class LuapeLearner : public Object
-{
-public:
-  LuapeLearner(LuapeProblemPtr problem = LuapeProblemPtr())
-    : problem(problem) {}
-
-  virtual LuapeGraphPtr learnGraph(ExecutionContext& context, LuapeGraphPtr initialGraph, const ContainerPtr& examples, DenseDoubleVectorPtr& alphas) const = 0;
-
-protected:
-  LuapeProblemPtr problem;
-};
-
-typedef ReferenceCountedObjectPtr<LuapeLearner> LuapeLearnerPtr;
-
-class BoostingLuapeLearner : public LuapeLearner
-{
-public:
-  BoostingLuapeLearner(LuapeProblemPtr problem, size_t maxSteps, size_t maxIterations)
-    : LuapeLearner(problem), maxSteps(maxSteps), maxIterations(maxIterations) {}
-  BoostingLuapeLearner() : maxIterations(0) {}
-
-  virtual DenseDoubleVectorPtr makeInitialWeights(const ContainerPtr& examples) const = 0;
-
-  // the absolute value of this quantity should be maximized
-  virtual double computeWeakObjective(const ContainerPtr& predictions, const ContainerPtr& supervisions, const DenseDoubleVectorPtr& weights) const = 0;
-  virtual bool shouldStop(double weakObjectiveValue) const = 0;
-  virtual double computeAlpha(double weakObjectiveValue) const = 0;
-  virtual double updateWeight(const Variable& prediction, const Variable& supervision, double currentWeight, double alpha) const = 0;
-
-  virtual LuapeGraphPtr learnGraph(ExecutionContext& context, LuapeGraphPtr initialGraph, const ContainerPtr& examples, DenseDoubleVectorPtr& alphas) const
-  {
-    DenseDoubleVectorPtr weights = makeInitialWeights(examples);
-    LuapeGraphPtr graph = initialGraph->cloneAndCast<LuapeGraph>();
-    VectorPtr supervisions;
-    addExamplesToGraph(examples, graph, supervisions);
-    alphas = new DenseDoubleVector(0, 0.0);
-
-    bool stopped = false;
-    for (size_t i = 0; i < maxIterations && !stopped; ++i)
-    {
-      context.enterScope(T("Iteration ") + String((int)i+1));
-      context.resultCallback(T("iteration"), i + 1);
-
-      // weak graph completion
-      LuapeGraphPtr newGraph = learnWeakModel(context, graph, weights, supervisions);
-      LuapeYieldNodePtr yieldNode = newGraph->getLastNode().dynamicCast<LuapeYieldNode>();
-      jassert(yieldNode);
-      double score = yieldNode->getCache()->getScore();
-      LuapeNodeCachePtr cache = newGraph->getNode(yieldNode->getArgument())->getCache();
-
-      // stop test
-      if (shouldStop(score))
-      {
-        context.informationCallback(T("Stopping, score = ") + String(score));
-        stopped = true;
-      }
-      else
-      {
-        graph = newGraph;
-
-        // compute alpha
-        double alpha = computeAlpha(score);
-        alphas->appendValue(alpha);
-
-        // update weights
-        updateWeights(cache->getExamples(), supervisions, weights, alpha);
-
-        context.resultCallback("score", score);
-        context.resultCallback("alpha", alpha);
-        context.resultCallback("weights", weights->cloneAndCast<DoubleVector>());
-      }
-
-      context.leaveScope();
-      context.progressCallback(new ProgressionState(i+1, maxIterations, T("Iterations")));
-    }
-    return graph;
-  }
-
-protected:
-  size_t maxSteps;
-  size_t maxIterations;
-
-  void addExamplesToGraph(const ContainerPtr& examples, LuapeGraphPtr graph, VectorPtr& supervisions) const
-  {
-    size_t n = examples->getNumElements();
-    graph->reserveExamples(n);
-    supervisions = vector(examples->getElementsType()->getTemplateArgument(1), n);
-    for (size_t i = 0; i < n; ++i)
-    {
-      PairPtr example = examples->getElement(i).getObjectAndCast<Pair>();
-      graph->addExample(example->getFirst().getObjectAndCast<Container>());
-      supervisions->setElement(i, example->getSecond());
-    }
-  }
-
-  void updateWeights(const ContainerPtr& predictions, const ContainerPtr& supervisions, const DenseDoubleVectorPtr& weights, double alpha) const
-  {
-    size_t n = predictions->getNumElements();
-    jassert(n == supervisions->getNumElements());
-    jassert(n == weights->getNumElements());
-
-    double* values = weights->getValuePointer(0);
-    double sum = 0.0;
-    for (size_t i = 0; i < n; ++i)
-    {
-      values[i] = updateWeight(predictions->getElement(i), supervisions->getElement(i), values[i], alpha);
-      sum += values[i];
-    }
-    weights->multiplyByScalar(1.0 / sum);
-  }
-
-  void exhaustiveSearch(ExecutionContext& context, LuapeGraphBuilderStatePtr state, const FunctionPtr& objective, double& bestScore, LuapeGraphPtr& bestGraph) const
-  {
-    ContainerPtr actions = state->getAvailableActions();
-    size_t n = actions->getNumElements();
-    for (size_t i = 0; i < n; ++i)
-    {
-      Variable action = actions->getElement(i);
-      double reward;
-      //context.enterScope(action.toShortString());
-      state->performTransition(context, action, reward);
-
-      if (!state->isFinalState())
-        exhaustiveSearch(context, state, objective, bestScore, bestGraph);
-      else
-      {
-        double score = objective->compute(context, state->getGraph()).toDouble();
-        if (score != DBL_MAX)
-        {
-          //context.informationCallback(state->toShortString() + T(" => ") + String(score));
-          if (score < bestScore)
-          {
-            bestScore = score;
-            bestGraph = state->getGraph()->cloneAndCast<LuapeGraph>();
-            context.informationCallback(T("New best: ") + state->toShortString() + T(" [") + String(score) + T("]"));
-          }
-        }
-      }
-      state->undoTransition(context, action);
-      //context.leaveScope(true);
-    }
-  }
-
-
-  struct Objective : public SimpleUnaryFunction
-  {
-    Objective(const BoostingLuapeLearner* pthis, ContainerPtr supervisions, DenseDoubleVectorPtr weights)
-      : SimpleUnaryFunction(luapeGraphClass, doubleType), pthis(pthis), supervisions(supervisions), weights(weights) {}
-
-    virtual Variable computeFunction(ExecutionContext& context, const Variable& input) const
-    {
-      const LuapeGraphPtr& graph = input.getObjectAndCast<LuapeGraph>();
-      LuapeYieldNodePtr yieldNode = graph->getLastNode().dynamicCast<LuapeYieldNode>();
-      if (!yieldNode)
-        return DBL_MAX; // non-terminal state
-
-      LuapeNodePtr valueNode = graph->getNode(yieldNode->getArgument());
-      LuapeNodeCachePtr cache = valueNode->getCache();
-      double score = pthis->computeWeakObjective(cache->getExamples(), supervisions, weights);
-      yieldNode->getCache()->setScore(score);
-      return -fabs(score);
-    }
-
-  protected:
-    const BoostingLuapeLearner* pthis;
-    ContainerPtr supervisions;
-    DenseDoubleVectorPtr weights;
-  };
-
-  LuapeGraphPtr learnWeakModel(ExecutionContext& context, LuapeGraphPtr graph, const DenseDoubleVectorPtr& weights, const ContainerPtr& supervisions) const
-  {
-    FunctionPtr objective = new Objective(this, supervisions, weights);
-    LuapeGraphCachePtr cache = new LuapeGraphCache();
-    LuapeGraphBuilderStatePtr state = new LuapeGraphBuilderState(problem, graph, cache, maxSteps);
-    double bestScore = DBL_MAX;
-
-    LuapeGraphPtr bestGraph;
-    exhaustiveSearch(context, state, objective, bestScore, bestGraph);
-    return bestGraph;
-  }
-};
-
-class AdaBoostLuapeLearner : public BoostingLuapeLearner
-{
-public:
-  AdaBoostLuapeLearner(LuapeProblemPtr problem, size_t maxSteps, size_t maxIterations)
-    : BoostingLuapeLearner(problem, maxSteps, maxIterations) {}
-  AdaBoostLuapeLearner() {}
-
-  virtual DenseDoubleVectorPtr makeInitialWeights(const ContainerPtr& examples) const
-    {size_t n = examples->getNumElements(); return new DenseDoubleVector(n, 1.0 / n);}
-
-  virtual double computeWeakObjective(const ContainerPtr& predictions, const ContainerPtr& supervisions, const DenseDoubleVectorPtr& weights) const
-  {
-    size_t numExamples = predictions->getNumElements();
-    jassert(numExamples == supervisions->getNumElements());
-    jassert(numExamples == weights->getNumElements());
-
-    double accuracy = 0.0;
-    for (size_t i = 0; i < numExamples; ++i)
-      if (predictions->getElement(i) == supervisions->getElement(i))
-        accuracy += weights->getValue(i);
-    return accuracy - 0.5;
-  }
-
-  virtual bool shouldStop(double weakObjectiveValue) const
-    {return weakObjectiveValue == 0.0 || weakObjectiveValue == 0.5;}
-
-  virtual double computeAlpha(double weakObjectiveValue) const
-  {
-    double accuracy = weakObjectiveValue + 0.5;
-    if (accuracy == 0.0)
-      return -1.0;
-    else if (accuracy == 1.0)
-      return 1.0;
-    else
-      return 0.5 * log((1.0 - accuracy) / accuracy);
-  }
-
-  virtual double updateWeight(const Variable& prediction, const Variable& supervision, double currentWeight, double alpha) const
-    {return currentWeight * exp(alpha * (supervision.getBoolean() == prediction.getBoolean() ? 1.0 : -1.0));}
-};
-
 class LuapeSandBox : public WorkUnit
 {
 public:
-  LuapeSandBox() : maxExamples(0), maxSteps(3) {}
+  LuapeSandBox() : maxExamples(0), maxSteps(3), maxIterations(10) {}
 
   virtual Variable run(ExecutionContext& context)
   {
@@ -296,18 +71,27 @@ public:
       String((int)features->getNumElements()) + T(" features"));
 
     LuapeProblemPtr problem = createProblem(features);
+
+    LuapeFunctionPtr classifier = new LuapeBinaryClassifier();
+    classifier->setBatchLearner(new AdaBoostLuapeLearner(problem, maxSteps, maxIterations));
+    classifier->setEvaluator(binaryClassificationEvaluator());
+
+    classifier->train(context, trainData, testData, T("Training"), true);
+    classifier->evaluate(context, trainData, EvaluatorPtr(), T("Evaluating on training data"));
+    classifier->evaluate(context, testData, EvaluatorPtr(), T("Evaluating on testing data"));
+/*
     LuapeGraphPtr initialGraph = problem->createInitialGraph(context);
 //    ContainerPtr supervisions;
 //    addExamplesToGraph(trainData, features, initialGraph, supervisions);
 
-    LuapeLearnerPtr learner = new AdaBoostLuapeLearner(problem, maxSteps, 10);
+    LuapeBatchLearnerPtr learner = new AdaBoostLuapeLearner(problem, maxSteps, 10);
     DenseDoubleVectorPtr alphas;
     LuapeGraphPtr graph = learner->learnGraph(context, initialGraph, trainData, alphas);
     context.resultCallback("initialGraph", initialGraph);
     context.resultCallback("graph", graph);
     context.resultCallback("alphas", alphas);
     evaluateGraph(context, graph, trainData, features, "training data");
-    evaluateGraph(context, graph, testData, features, "testing data");
+    evaluateGraph(context, graph, testData, features, "testing data");*/
     return true;
   }
 
@@ -318,6 +102,7 @@ protected:
   File testFile;
   size_t maxExamples;
   size_t maxSteps;
+  size_t maxIterations;
 
   ContainerPtr loadData(ExecutionContext& context, const File& file, DefaultEnumerationPtr features) const
   {
