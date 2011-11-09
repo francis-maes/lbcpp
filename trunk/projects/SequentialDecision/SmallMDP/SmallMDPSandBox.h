592fa20e0a1f75d8a393e5579b0383f2e4afe61c
francis <francis@885cb74b-98ed-4c74-c960-63734602d729>
2011-11-09 11:13:49 +0000

    '''SmallMDP: setup experiment to perform grid search over policy parameters + RTDP-Rmax policy'''

--- trunk/projects/SequentialDecision/SmallMDP/SmallMDPSandBox.h
+++ trunk/projects/SequentialDecision/SmallMDP/SmallMDPSandBox.h
@@ -16,43 +16,107 @@
 namespace lbcpp
 {
 
+class SparseSmallMDPSampler : public Sampler
+{
+public:
+  SparseSmallMDPSampler(size_t numStates = 20, size_t numActions = 4, double discount = 0.9, size_t numSuccessorsPerState = 5, double nonNullRewardProbability = 0.1)
+    : numStates(numStates), numActions(numActions), discount(discount), numSuccessorsPerState(numSuccessorsPerState), nonNullRewardProbability(nonNullRewardProbability) {}
+
+  virtual Variable sample(ExecutionContext& context, const RandomGeneratorPtr& random, const Variable* inputs = NULL) const
+    {return new GeneratedSparseSmallMDP(random, numStates, numActions, discount, numSuccessorsPerState, nonNullRewardProbability);}
+ 
+protected:
+  friend class SparseSmallMDPSamplerClass;
+
+  size_t numStates;
+  size_t numActions;
+  double discount;
+  size_t numSuccessorsPerState;
+  double nonNullRewardProbability;
+};
+
 class SmallMDPSandBox : public WorkUnit
 {
 public:
-  SmallMDPSandBox() : numStates(20), numActions(4), discount(0.9), numSuccessorsPerState(5), nonNullRewardProbability(0.1) {}
+  SmallMDPSandBox() : mdpSampler(new SparseSmallMDPSampler()), numTimeSteps(100000), numRuns(1000) {}
   
   virtual Variable run(ExecutionContext& context)
   {
-    //SmallMDPPtr mdp = new GeneratedSparseSmallMDP(context.getRandomGenerator(), numStates, numActions, discount, numSuccessorsPerState, nonNullRewardProbability);
+    testPolicy(context, "optimal", new OptimalSmallMDPPolicy());
+    testPolicy(context, "random", new RandomSmallMDPPolicy());
 
-    SmallMDPPtr mdp = new LongChainMDP();
+    std::vector<SmallMDPPolicyPtr> policies;
+    for (size_t m = 1; m < 50; ++m)
+      policies.push_back(new RMaxSmallMDPPolicy(m));
+    findBestPolicy(context, "rmax", policies);
+
+    policies.clear();
+    for (size_t m = 1; m < 50; ++m)
+      policies.push_back(new RTDPRMaxSmallMDPPolicy(m));
+    findBestPolicy(context, "RTDP-rmax", policies);
+    
+    policies.clear();
+    for (double beta = 0.0; beta <= 2.0; beta += 0.1)
+      policies.push_back(new QLearningSmallMDPPolicy(constantIterationFunction(0.0), beta));
+    findBestPolicy(context, "QLearning", policies);
+
+    /*
+    //SmallMDPPtr mdp = new LongChainMDP();
     //SmallMDPPtr mdp = new BanditMDP();
-    testPolicy(context, "MBIE-EB(10000, 0.16)", new MBIEEBSmallMDPPolicy(10000, 0.16), mdp);
-    testPolicy(context, "optimal", new OptimalSmallMDPPolicy(), mdp);
-    testPolicy(context, "rmax(2)", new RMaxSmallMDPPolicy(2), mdp);
+    //testPolicy(context, "MBIE-EB(10000, 0.16)", new MBIEEBSmallMDPPolicy(10000, 0.16), mdp);
+    
+    testPolicy(context, "rmax(2)", new RMaxSmallMDPPolicy(2));
+    testPolicy(context, "RTDP-rmax(2)", new RTDPRMaxSmallMDPPolicy(2));
 //    testPolicy(context, "rmax(5)", new RMaxSmallMDPPolicy(5), mdp);
     //testPolicy(context, "rmax(10)", new RMaxSmallMDPPolicy(10), mdp);
-    testPolicy(context, "qlearning w=1", new QLearningSmallMDPPolicy(constantIterationFunction(0.0), 1.0), mdp);   
+    testPolicy(context, "qlearning w=1", new QLearningSmallMDPPolicy(constantIterationFunction(0.0), 1.0));   
     //testPolicy(context, "qlearning w=0.5", new QLearningSmallMDPPolicy(constantIterationFunction(0.0), 0.5), mdp);   
-    testPolicy(context, "random", new RandomSmallMDPPolicy(), mdp);
+    testPolicy(context, "random", new RandomSmallMDPPolicy());
+    */
     return true;
   }
 
-  void testPolicy(ExecutionContext& context, const String& name, const SmallMDPPolicyPtr& policy, const SmallMDPPtr& mdp) const
+  SmallMDPPolicyPtr findBestPolicy(ExecutionContext& context, const String& name, const std::vector<SmallMDPPolicyPtr>& policies) const
   {
-    static const size_t numTimeSteps = 100000;
-    static const size_t numRuns = 10;
+    double bestScore = -DBL_MAX;
+    SmallMDPPolicyPtr bestPolicy;
 
     context.enterScope(name);
+    for (size_t i = 0; i < policies.size(); ++i)
+    {
+      SmallMDPPolicyPtr policy = policies[i];
+      double score = testPolicy(context, policy->toShortString(), policy);
+      if (score > bestScore)
+      {
+        bestScore = score;
+        bestPolicy = policy;
+      }
+    }
+    context.leaveScope(bestScore);
+    return bestPolicy;
+  }
+
+  double testPolicy(ExecutionContext& context, const String& name, const SmallMDPPolicyPtr& policy) const
+  {
+    context.enterScope(name);
+
+    for (size_t i = 0; i < policy->getNumVariables(); ++i)
+      if (policy->getVariableType(i)->isConvertibleToDouble())
+        context.resultCallback(policy->getVariableName(i), policy->getVariable(i));
+
     ScalarVariableStatisticsPtr stats = new ScalarVariableStatistics("toto");
     for (size_t i = 0; i < numRuns; ++i)
     {
+      SmallMDPPtr mdp = mdpSampler->sample(context, context.getRandomGenerator()).getObjectAndCast<SmallMDP>();
       double score = runPolicy(context, policy->cloneAndCast<SmallMDPPolicy>(), mdp, numTimeSteps);
       stats->push(score);
       context.progressCallback(new ProgressionState(i+1, numRuns, T("Runs")));
       context.informationCallback("Score: " + String(score));
     }
+
+    context.resultCallback("mean cumulative reward", stats->getMean());
     context.leaveScope(stats);
+    return stats->getMean();
   }
   
   double runPolicy(ExecutionContext& context, const SmallMDPPolicyPtr& policy, const SmallMDPPtr& mdp, size_t numTimeSteps) const
@@ -75,11 +139,9 @@ public:
 protected:
   friend class SmallMDPSandBoxClass;
   
-  size_t numStates;
-  size_t numActions;
-  double discount;
-  size_t numSuccessorsPerState;
-  double nonNullRewardProbability;
+  SamplerPtr mdpSampler;
+  size_t numTimeSteps;
+  size_t numRuns;
 };
 
 }; /* namespace lbcpp */
