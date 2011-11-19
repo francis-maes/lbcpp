3f74f1844ae37ec185ef3883359e5edd6e84155c
francis <francis@885cb74b-98ed-4c74-c960-63734602d729>
2011-11-19 19:46:27 +0000

    '''Luape: RewardStorage based policy'''
     * Nouvelle structure de données pour la collecte de résultats
     * Fonction indicatrice de paires état-action
     * Politique en dur qui utilise la structure de données et la fonction indicatrice

--- projects/SequentialDecision/Luape/LuapePolicyBasedGBLearner.h
+++ projects/SequentialDecision/Luape/LuapePolicyBasedGBLearner.h
@@ -11,6 +11,7 @@
 
 # include "LuapeGraphLearner.h"
 # include "../Core/Policy.h"
+# include <list>
 
 namespace lbcpp
 {
@@ -18,58 +19,231 @@ namespace lbcpp
 class LuapeRewardStorage : public Object
 {
 public:
-  LuapeRewardStorage(size_t maximumDeltaTime)
-    : maximumDeltaTime(maximumDeltaTime) {}
+  LuapeRewardStorage(EnumerationPtr features, size_t memorySize)
+    : features(features), memorySize(memorySize) {}
 
-  void observe(const String& name, double reward, size_t time)
+  void startNewEpisode()
   {
-    SparseRewardSequenceMap::iterator it = m.find(name);
-    if (it == m.end())
-    {
-      SparseRewardSequence sequence;
-      observe(sequence, reward, time);
-      m[name] = sequence;
-    }
-    else
-      observe(it->second, reward, time);
+    observations.push_back(std::vector<ScalarVariableMeanAndVariance>(features->getNumElements()));
+    if (observations.size() > memorySize)
+      observations.pop_front();
   }
 
-  typedef std::list<std::pair<size_t, double> > SparseRewardSequence;
-
-  SparseRewardSequence getRecentRewards(const String& name) const
+  void observe(const DenseDoubleVectorPtr& counts, double reward)
   {
-    SparseRewardSequenceMap::const_iterator it = m.find(name);
-    return it == m.end() ? SparseRewardSequence() : it->second;
-  }
-
-private:
-  typedef std::map<String, SparseRewardSequence> SparseRewardSequenceMap;
-
-  size_t maximumDeltaTime;
-  SparseRewardSequenceMap m;
-
-  void observe(SparseRewardSequence& sequence, double reward, size_t time)
-  {
-    sequence.push_back(std::make_pair(time, reward));
-    if (time > maximumDeltaTime)
+    jassert(observations.size());
+    std::vector<ScalarVariableMeanAndVariance>& currentObservations = observations.back();
+    size_t n = counts->getNumValues();
+    if (currentObservations.size() < n)
+      currentObservations.resize(n);
+    for (size_t i = 0; i < n; ++i)
     {
-      while (sequence.front().first < time - maximumDeltaTime)
-        sequence.pop_front();
+      double weight = counts->getValue(i);
+      if (weight)
+        currentObservations[i].push(reward, weight);
     }
   }
+
+  double getExpectedReward(size_t featureIndex, double discount, double& sampleCount) const
+  {
+    double res = 0.0;
+    sampleCount = 0.0;
+    double weight = 1.0;
+    ObservationList::const_reverse_iterator it = observations.rbegin(); // skip first element
+    for (++it; it != observations.rend(); ++it)
+      if (featureIndex < it->size())
+      {
+        const ScalarVariableMeanAndVariance& stats = (*it)[featureIndex];
+        double count = stats.getCount();
+        double reward = stats.getMean();
+        res += reward * count * weight;
+        sampleCount += count * weight;
+        weight *= discount;
+      }
+    if (sampleCount)
+      res /= sampleCount;
+    return res;
+  }
+
+protected:
+  EnumerationPtr features;
+  size_t memorySize;
+
+  typedef std::list< std::vector<ScalarVariableMeanAndVariance> > ObservationList;
+
+  ObservationList observations;
 };
 
-  /*
-class Policy : public Object
+typedef ReferenceCountedObjectPtr<LuapeRewardStorage> LuapeRewardStoragePtr;
+
+class LuapeRewardStorageBasedPolicy : public Policy
 {
 public:
-  virtual Variable policyStart(ExecutionContext& context, const Variable& state, const ContainerPtr& actions) = 0;
-  virtual Variable policyStep(ExecutionContext& context, double reward, const Variable& state, const ContainerPtr& actions) = 0;
-  virtual void policyEnd(ExecutionContext& context, double reward, const Variable& finalState) {}
+  LuapeRewardStorageBasedPolicy()
+  {
+    features = new DefaultEnumeration();
+    rewards = new LuapeRewardStorage(features, 1000);
+  }
+
+  /////// FEATURES //////////
+  void addFeature(ExecutionContext& context, const String& name, const SparseDoubleVectorPtr& res) const
+  {
+    size_t index = features.staticCast<DefaultEnumeration>()->findOrAddElement(context, name);
+    res->setElement(index, 1.0);
+  }
+
+  void addPredicateConjunctionsToFeatures(ExecutionContext& context, const std::vector<String>& predicates, const SparseDoubleVectorPtr& res,
+                                            size_t predicateIndex, std::vector<bool>& included, size_t includedCount, String& name) const
+  {
+    if (predicateIndex == predicates.size())
+    {
+      addFeature(context, name, res);
+    }
+    else
+    {
+      // subsets without this predicate
+      included[predicateIndex] = false;
+      addPredicateConjunctionsToFeatures(context, predicates, res, predicateIndex + 1, included, includedCount, name);
+      
+      // subsets with this predicate
+      if (includedCount < 3)
+      {
+        included[predicateIndex] = true;
+        String previousName = name;
+        name = (name.isEmpty() ? "" : name + T(" ")) + predicates[predicateIndex];
+        addPredicateConjunctionsToFeatures(context, predicates, res, predicateIndex + 1, included, includedCount + 1, name);
+        name = previousName;
+      }
+    }
+  }
+
+  SparseDoubleVectorPtr makeFeatures(ExecutionContext& context, const DecisionProblemStatePtr& s, const Variable& a) const
+  {
+    const LuapeGraphBuilderStatePtr& state = s.staticCast<LuapeGraphBuilderState>();
+    const LuapeGraphBuilderActionPtr& action = a.getObjectAndCast<LuapeGraphBuilderAction>();
+
+    SparseDoubleVectorPtr res(new SparseDoubleVector(features, doubleType));
+
+    std::vector<String> predicates;
+    predicates.push_back("step=" + String(state->getCurrentStep()));
+    predicates.push_back("stacksize=" + String(state->getStackSize()));
+
+    if (action->isNewNode)
+    {
+      if (action->nodeToAdd.isInstanceOf<LuapeYieldNode>())
+      {
+        predicates.push_back("kind=yield");
+      }
+      else
+      {
+        LuapeFunctionNodePtr functionNode = action->nodeToAdd.staticCast<LuapeFunctionNode>();
+
+        predicates.push_back("kind=apply");
+        predicates.push_back("fun=" + functionNode->getFunction()->getClassName());
+        for (size_t i = 0; i < functionNode->getNumArguments(); ++i)
+          predicates.push_back("arg" + String((int)i) + "type=" + functionNode->getArgument(i)->getType()->getName());
+      }
+    }
+    else
+    {
+      jassert(action->nodeToAdd);
+
+      predicates.push_back("kind=push");
+      predicates.push_back("argtype=" + action->nodeToAdd->getType()->getName());
+      if (state->getStackSize() > 0)
+        predicates.push_back("prevargtype=" + state->getStackElement(state->getStackSize() - 1)->getType()->getName());
+      else
+        predicates.push_back("firstarg");
+    }
+
+    std::vector<bool> included(predicates.size(), false);
+    String name;
+    size_t includedCount = 0;
+    addPredicateConjunctionsToFeatures(context, predicates, res, 0, included, includedCount, name);
+
+    return res;
+  }
+
+  void beginEpisode()
+  {
+    rewards->startNewEpisode();
+  }
+
+  /////// POLICY //////////
+  virtual Variable policyStart(ExecutionContext& context, const Variable& state, const ContainerPtr& actions)
+  {
+    trajectoryFeatures = new DenseDoubleVector(features, doubleType);
+    return selectAction(context, state.getObjectAndCast<DecisionProblemState>(), actions);
+  }
+
+  virtual Variable policyStep(ExecutionContext& context, double reward, const Variable& state, const ContainerPtr& actions)
+    {return selectAction(context, state.getObjectAndCast<DecisionProblemState>(), actions);}
+
+  virtual void policyEnd(ExecutionContext& context, double reward, const Variable& finalState)
+    {rewards->observe(trajectoryFeatures, reward);}
+
+  Variable selectAction(ExecutionContext& context, const DecisionProblemStatePtr& state, const ContainerPtr& actions)
+  {
+    Variable action = sampleAction(context, state, actions);
+    SparseDoubleVectorPtr features = makeFeatures(context, state, action);
+    features->addTo(trajectoryFeatures);
+    return action;
+  }
+
+  Variable sampleAction(ExecutionContext& context, const DecisionProblemStatePtr& state, const ContainerPtr& actions)
+  {
+    size_t n = actions->getNumElements();
+    std::vector<size_t> bestActions;
+    double bestScore = -DBL_MAX;
+    for (size_t i = 0; i < n; ++i)
+    {
+      double score = computeActionScore(context, state, actions->getElement(i));
+      if (score >= bestScore)
+      {
+        if (score > bestScore)
+        {
+          bestScore = score;
+          bestActions.clear();
+        }
+        bestActions.push_back(i);
+      }
+    }
+    RandomGeneratorPtr random = context.getRandomGenerator();
+    if (bestActions.size())
+      return actions->getElement(bestActions[random->sampleSize(bestActions.size())]);
+    else
+      return actions->getElement(random->sampleSize(n));
+  }
+
+  double computeActionScore(ExecutionContext& context, const DecisionProblemStatePtr& state, const Variable& action)
+  {
+    SparseDoubleVectorPtr features = makeFeatures(context, state, action);
+    double sampleCount = 0.0;
+    double sumOfRewards = 0.0;
+    double sumOfWeights = 0.0;
+
+    for (size_t i = 0; i < features->getNumValues(); ++i)
+    {
+      const std::pair<size_t, double>& feature = features->getValue(i);
+      double samples;
+      double reward = rewards->getExpectedReward(feature.first, 1.0, samples);
+      sampleCount += feature.second * samples;
+      sumOfRewards += feature.second * reward;
+      sumOfWeights += feature.second;
+    }
+    sampleCount /= sumOfWeights;
+    sumOfRewards /= sumOfWeights;
+
+    return sumOfRewards + 1.5 / (1.0 + sampleCount);
+  }
 
   lbcpp_UseDebuggingNewOperator
+
+protected:
+  EnumerationPtr features;
+  DenseDoubleVectorPtr trajectoryFeatures;
+  LuapeRewardStoragePtr rewards;
 };
-*/
 
 class LuapePolicyBasedGBLearner : public LuapeGradientBoostingLearner
 {
@@ -85,6 +259,11 @@ public:
     findOptimalWeakLearner(context, new LuapeGraphBuilderState(problem, graph->cloneAndCast<LuapeGraph>(), maxDepth), optimalReward, optimalWeakLearner);
     context.leaveScope(optimalReward);
 
+    // FIXME !
+    if (policy.dynamicCast<LuapeRewardStorageBasedPolicy>())
+      policy.staticCast<LuapeRewardStorageBasedPolicy>()->beginEpisode();
+    // -
+
     context.enterScope(T("Weak Learning"));
     double bestReward = -DBL_MAX;
     LuapeNodePtr bestWeakLearner;
@@ -97,7 +276,11 @@ public:
       context.progressCallback(new ProgressionState(i+1, budget, T("Trajectories")));
     }
     context.leaveScope(bestReward);
-    context.informationCallback(T("Weak learner: ") + bestWeakLearner->toShortString() + T(" [") + String(bestReward) + T("]"));
+    if (bestWeakLearner)
+      context.informationCallback(T("Weak learner: ") + bestWeakLearner->toShortString() + T(" [") + String(bestReward) + T("]"));
+    else
+      context.errorCallback(T("Failed to find a weak learner"));
+
     context.informationCallback(T("Optimal weak learner: ") + optimalWeakLearner->toShortString() + T(" [") + String(optimalReward) + T("]"));
     context.resultCallback(T("regret"), optimalReward - bestReward);
     const_cast<LuapePolicyBasedGBLearner* >(this)->regret.push(optimalReward - bestReward);
