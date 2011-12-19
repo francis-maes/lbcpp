d91dc9879054eca5a8d4629a18835717d479af77
alejandro <alejandro@885cb74b-98ed-4c74-c960-63734602d729>
2011-12-19 10:12:42 +0000

    Implementing Pose object (wrapper from Rosetta source to lbcpp)

--- projects/Proteins/Rosetta/Data/Pose.h
+++ projects/Proteins/Rosetta/Data/Pose.h
@@ -15,36 +15,47 @@
 #  undef T
 #  include <core/chemical/ChemicalManager.hh>
 #  include <core/chemical/util.hh>
+#  include <core/conformation/Residue.hh>
 #  include <core/io/pdb/pose_io.hh>
 #  include <core/pose/Pose.hh>
+#  include <core/scoring/ScoreFunction.hh>
+#  include <core/scoring/ScoreFunctionFactory.hh>
+#  include <numeric/xyzVector.hh>
 #  define T JUCE_T
 # endif // LBCPP_PROTEIN_ROSETTA
 
 namespace lbcpp
 {
+class Pose;
+typedef ReferenceCountedObjectPtr<Pose> PosePtr;
 
 class Pose : public Object
 {
 public:
+  Pose();
+  Pose(const PosePtr& copy);
+
   void createFromSequence(const String& sequence);
   void createFromPDB(const File& pdbFile);
-  //createFromXml
 
-  //saveToPDB
-  //saveToXml
-  //
-  //getPhi
-  //getPsi
-  //setPhi
-  //setPsi
-  //
-  //getRawEnergy
-  //getCorrectedEnergy
-  //
-  //getDistanceCorrectionFactor
-  //getCollisionCorrectionFactor
+  void saveToPDB(const File& pdbFile);
+
+  Pose& operator=(const Pose& copy);
+
+  double getPhi(size_t residue);
+  double getPsi(size_t residue);
+  void setPhi(size_t residue, double phi);
+  void setPsi(size_t residue, double psi);
+  SymmetricMatrixPtr createBackboneDistanceMatrix();
+
+  double getEnergy();
+  double getCorrectedEnergy();
+  double getDistanceCorrectionFactor();
+  double getCollisionCorrectionFactor();
+
 protected:
   friend class PoseClass;
+
 # ifdef LBCPP_PROTEIN_ROSETTA
   core::pose::PoseOP pose;
 # endif //! LBCPP_PROTEIN_ROSETTA
