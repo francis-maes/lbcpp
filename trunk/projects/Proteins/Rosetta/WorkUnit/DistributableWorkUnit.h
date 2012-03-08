7774eae5f059ed9b8df79478806b9bcb9f5934ad
alejandro <alejandro@885cb74b-98ed-4c74-c960-63734602d729>
2012-03-08 14:31:51 +0000

    Minor changes in DistributableWorkUnit

--- trunk/projects/Proteins/Rosetta/WorkUnit/DistributableWorkUnit.h
+++ trunk/projects/Proteins/Rosetta/WorkUnit/DistributableWorkUnit.h
@@ -24,13 +24,11 @@ public:
   virtual void workUnitFinished(const WorkUnitPtr& workUnit, const Variable& result, const ExecutionTracePtr& trace)
   {
     savedResult = result;
-    File fR(context.getProjectDirectory().getFullPathName() + T("/") + projectName + T("/")
-        + workUnit->toString() + T(".xml"));
-    result.saveToFile(context, fR);
+    File fR(context.getProjectDirectory().getFullPathName() + T("/") + projectName + T("/") + workUnit->toString() + T(".xml"));
+    savedResult.saveToFile(context, fR);
 
     savedTrace = trace;
-    File fT(context.getProjectDirectory().getFullPathName() + T("/") + projectName + T("/")
-        + workUnit->toString() + T(".trace"));
+    File fT(context.getProjectDirectory().getFullPathName() + T("/") + projectName + T("/") + workUnit->toString() + T(".trace"));
     savedTrace->saveToFile(context, fT);
   }
 
@@ -39,10 +37,7 @@ public:
     VariableVectorPtr results = variableVector(callbacks.getNumElements());
 
     for (size_t i = 0; i < callbacks.getNumElements(); i++)
-    {
-      results->setElement(i, callbacks.getElement(i).getObjectAndCast<
-          DistributableExecutionContextCallback> ()->savedResult);
-    }
+      results->setElement(i, callbacks.getElement(i).getObjectAndCast<DistributableExecutionContextCallback> ()->savedResult);
 
     return results;
   }
@@ -60,8 +55,7 @@ typedef ReferenceCountedObjectPtr<DistributableExecutionContextCallback> Distrib
 class DistributableWorkUnit : public WorkUnit
 {
 public:
-  DistributableWorkUnit() : name(T("DistributableWorkUnit")) {}
-  DistributableWorkUnit(String name) : name(name) {}
+  DistributableWorkUnit() {workUnits = new CompositeWorkUnit(T("DistributableWorkUnit"));}
 
   /**
    * Each subworkunit should open a scope for itself to avoid undesired effects
@@ -71,7 +65,7 @@ public:
    * workUnits->addWorkUnit(new WorkUnit());
    *
    */
-  virtual void initializeWorkUnits(ExecutionContext& context)=0;
+  virtual void initializeWorkUnits(ExecutionContext& context) = 0;
 
   /**
    * Manage the VariableVector output by run.
@@ -93,7 +87,7 @@ public:
   virtual Variable run(ExecutionContext& context)
   {
     // initialization
-    context.enterScope(T("Initializing distributable work unit::local"));
+    context.enterScope(T("DistributableWorkUnit::local : Initialization"));
     initializeWorkUnits(context);
     context.leaveScope();
     if (workUnits->getNumWorkUnits() == 0)
@@ -103,40 +97,34 @@ public:
     }
 
     // computing time
-    context.enterScope(name);
+    context.enterScope(T("DistributableWorkUnit::local : Computing"));
     context.informationCallback(T("Treating ") + String((int)workUnits->getNumWorkUnits())
         + T(" work units."));
     Variable result = context.run(workUnits, true);
     context.leaveScope();
 
     // show results
-    context.enterScope(T("Results"));
+    context.enterScope(T("DistributableWorkUnit::local : singleResultCallback"));
     for (size_t i = 0; i < workUnits->getNumWorkUnits(); i++)
     {
       context.enterScope(workUnits->getWorkUnit(i));
-      context.leaveScope(singleResultCallback(context,
-          result.getObjectAndCast<VariableVector> ()->getElement(i)));
+      context.leaveScope(singleResultCallback(context, result.getObjectAndCast<VariableVector> ()->getElement(i)));
     }
     context.leaveScope();
 
     // managing results
-    context.enterScope(T("Managing results"));
-    Variable gatheredResult = multipleResultCallback(context, *result.getObjectAndCast<
-        VariableVector> ());
+    context.enterScope(T("DistributableWorkUnit::local : multipleResultCallback"));
+    Variable gatheredResult = multipleResultCallback(context, *result.getObjectAndCast<VariableVector> ());
     context.leaveScope(gatheredResult);
 
-    context.informationCallback(T("local"), T("Distributable work unit done."));
+    context.informationCallback(T("DistributableWorkUnit::local : Done"));
 
     return gatheredResult;
   }
 
-  String getName()
-    {return name;}
-
 protected:
   friend class DistributableWorkUnitClass;
 
-  String name;
   CompositeWorkUnitPtr workUnits;
 };
 
@@ -148,44 +136,29 @@ class DistributeToClusterWorkUnit : public WorkUnit
 public:
   virtual Variable run(ExecutionContext& context)
   {
-    if (distributable->getName().isEmpty())
-    {
-      context.errorCallback(T("Name of distributable should not be empty."));
-      return Variable();
-    }
-
-    ExecutionContextPtr remoteContext = distributedExecutionContext(context, remoteHostName,
-        remoteHostPort, distributable->getName(), localHostLogin, clusterLogin,
-        fixedResourceEstimator(1, memory, time), true);
+    ExecutionContextPtr remoteContext = distributedExecutionContext(context, remoteHostName, remoteHostPort, distributable->toString(), localHostLogin, clusterLogin, fixedResourceEstimator(1, memory,
+        time), true);
 
     File distributableWorkUnitDirectory = context.getFile(distributable->toString());
     if (!distributableWorkUnitDirectory.exists())
       distributableWorkUnitDirectory.createDirectory();
 
     // initialization
-    context.enterScope(T("Initializing distributable work unit::distributed"));
+    context.enterScope(T("DistributableWorkUnit::cluster : Initialization"));
     distributable->initializeWorkUnits(context);
     context.leaveScope();
     CompositeWorkUnitPtr units = distributable->getWorkUnits();
-    if (units->getNumWorkUnits() == 0)
-    {
-      context.informationCallback(T("No work units to treat."));
-      return Variable();
-    }
 
     // computing time
-    context.enterScope(distributable->getName());
-    context.informationCallback(T("Treating ") + String((int)units->getNumWorkUnits())
-        + T(" work units."));
+    context.enterScope(T("DistributableWorkUnit::cluster : Computing"));
+    context.informationCallback(T("Treating ") + String((int)units->getNumWorkUnits()) + T(" work units."));
 
     // check for already solved workunits and execute unsolved ones
     for (size_t i = 0; i < units->getNumWorkUnits(); i++)
     {
-      File wuResult(distributableWorkUnitDirectory.getFullPathName() + T("/") + units->getWorkUnit(
-          i)->toString() + T(".xml"));
+      File wuResult(distributableWorkUnitDirectory.getFullPathName() + T("/") + units->getWorkUnit(i)->toString() + T(".xml"));
       if (!wuResult.exists())
-        remoteContext->pushWorkUnit(units->getWorkUnit(i),
-            new DistributableExecutionContextCallback(context, distributable->toString()));
+        remoteContext->pushWorkUnit(units->getWorkUnit(i), new DistributableExecutionContextCallback(context, distributable->toString()));
     }
     remoteContext->waitUntilAllWorkUnitsAreDone();
 
@@ -195,14 +168,12 @@ public:
     for (size_t i = 0; i < units->getNumWorkUnits(); i++)
     {
       // reload results
-      File wuResult(distributableWorkUnitDirectory.getFullPathName() + T("/") + units->getWorkUnit(
-          i)->toString() + T(".xml"));
+      File wuResult(distributableWorkUnitDirectory.getFullPathName() + T("/") + units->getWorkUnit(i)->toString() + T(".xml"));
       Variable singleSolution = Variable::createFromFile(context, wuResult);
       result.getObjectAndCast<VariableVector> ()->setElement(i, singleSolution);
 
       // reload traces
-      File wuTrace(distributableWorkUnitDirectory.getFullPathName() + T("/")
-          + units->getWorkUnit(i)->toString() + T(".trace"));
+      File wuTrace(distributableWorkUnitDirectory.getFullPathName() + T("/") + units->getWorkUnit(i)->toString() + T(".trace"));
       Variable singleTrace;
       if (wuTrace.exists())
         singleTrace = Variable::createFromFile(context, wuTrace);
@@ -210,30 +181,27 @@ public:
         singleTrace = ExecutionTracePtr();
       traces.getObjectAndCast<VariableVector> ()->setElement(i, singleTrace);
     }
-
     context.leaveScope();
 
     // show results
-    context.enterScope(T("Results"));
+    context.enterScope(T("DistributableWorkUnit::cluster : singleResultCallback"));
     for (size_t i = 0; i < distributable->getWorkUnits()->getNumWorkUnits(); i++)
     {
       context.enterScope(distributable->getWorkUnits()->getWorkUnit(i));
       context.enterScope(T("Trace"));
       context.leaveScope(traces.getObjectAndCast<VariableVector> ()->getElement(i));
-      context.enterScope(T("Single result callback"));
-      context.leaveScope(distributable->singleResultCallback(context, result.getObjectAndCast<
-          VariableVector> ()->getElement(i)));
+      context.enterScope(T("Result callback"));
+      context.leaveScope(distributable->singleResultCallback(context, result.getObjectAndCast<VariableVector> ()->getElement(i)));
       context.leaveScope();
     }
     context.leaveScope();
 
     // managing results
-    context.enterScope(T("Managing results"));
-    Variable gatheredResult = distributable->multipleResultCallback(context,
-        *result.getObjectAndCast<VariableVector> ());
+    context.enterScope(T("DistributableWorkUnit::cluster : multipleResultCallback"));
+    Variable gatheredResult = distributable->multipleResultCallback(context, *result.getObjectAndCast<VariableVector> ());
     context.leaveScope(gatheredResult);
 
-    context.informationCallback(T("cluster"), T("Distributable work unit done."));
+    context.informationCallback(T("DistributableWorkUnit::cluster : Done"));
 
     return gatheredResult;
   }
