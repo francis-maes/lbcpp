a5169c0e628f0ac0bb8618a59e38477b9ba6fd25
francis <francis@885cb74b-98ed-4c74-c960-63734602d729>
2012-03-14 11:12:57 +0000

    Experiments exploration/exploitation challenge

--- projects/SequentialDecision/WorkUnit/ExploChallengeSandBox.h
+++ projects/SequentialDecision/WorkUnit/ExploChallengeSandBox.h
@@ -21,15 +21,20 @@ class ExploChallengeReverseSampler : public IndependentDoubleVectorSampler
 {
 public:
   ExploChallengeReverseSampler()
-    : IndependentDoubleVectorSampler(exploChallengeFormulaObjectiveParametersEnumeration, gaussianSampler(0.5, 0.5)) {}
+  {
+    elementsEnumeration = exploChallengeFormulaObjectiveParametersEnumeration;
+    outputType = denseDoubleVectorClass(elementsEnumeration, doubleType);
+    size_t n = elementsEnumeration->getNumElements();
+    const double* params = ExploChallengeFormulaObjective::getInitialSamplerParameters();
+    samplers.resize(n);
+    for (size_t i = 0; i < n; ++i)
+      samplers[i] = gaussianSampler(params[i*2], params[i*2+1]);
+  }
 
   virtual Variable sample(ExecutionContext& context, const RandomGeneratorPtr& random, const Variable* inputs = NULL) const
   {
     DenseDoubleVectorPtr res = IndependentDoubleVectorSampler::sample(context, random).getObjectAndCast<DenseDoubleVector>();
-    res->setValue(0, juce::jlimit(0.0, 1.0, res->getValue(0)));
-    res->setValue(1, juce::jlimit(0.0, 1.0, res->getValue(1)));
-    res->setValue(2, juce::jlimit(0.0, 1.0, res->getValue(2)));
-    res->setValue(3, juce::jlimit(0.0, res->getValue(2), res->getValue(3)));
+    ExploChallengeFormulaObjective::applyConstraints(res);
     return res;
   }
 };
@@ -43,17 +48,7 @@ public:
   FunctionPtr createFormulaObjective(const Variable& params) const
   {
     DenseDoubleVectorPtr parameters = params.getObjectAndCast<DenseDoubleVector>();
-
-    //size_t totalNumArms = 246;
-    //size_t numArmsPerRound = 30;
-    size_t totalNumArms = surrogateNumDocuments;
-    size_t numDocumentsAliveSimultaneously = (size_t)(parameters->getValue(2) * totalNumArms); // 80
-    size_t numArmsPerRound = (size_t)(parameters->getValue(3) * totalNumArms);
-    double minRewardExpectation = juce::jlimit(0.0, 1.0, parameters->getValue(0));
-    double maxRewardExpectation = juce::jlimit(0.0, 1.0, parameters->getValue(1));
-
-    return new ExploChallengeFormulaObjective
-      (surrogateHorizon, totalNumArms,  numArmsPerRound, numDocumentsAliveSimultaneously, minRewardExpectation, maxRewardExpectation);
+    return new ExploChallengeFormulaObjective(surrogateHorizon, surrogateNumDocuments, parameters);
   }
 
   virtual Variable computeFunction(ExecutionContext& context, const Variable& input) const
@@ -62,10 +57,14 @@ public:
     double res = 0.0;
     for (size_t i = 0; i < scores.size(); ++i)
     {
-      double predictedScore = formulaObjective->compute(context, scores[i].first).toDouble();
-      res += fabs(scores[i].second - predictedScore);
+      size_t numRuns = 1;
+      double predictedScore = 0.0;
+      for (size_t j = 0; j < numRuns; ++j)
+        predictedScore += formulaObjective->compute(context, scores[i].first).toDouble();
+      predictedScore /= numRuns;
+      res = juce::jmax(res, fabs(scores[i].second - predictedScore));
     }
-    return res / scores.size();
+    return res;
   }
 
   void addReferenceScore(const GPExpressionPtr& formula, double score)
@@ -73,14 +72,15 @@ public:
 
   std::vector< std::pair<GPExpressionPtr, double> > scores;
 
-  double validate(ExecutionContext& context, const Variable& solution) const
+  Variable validate(ExecutionContext& context, const Variable& solution) const
   {
     FunctionPtr formulaObjective = createFormulaObjective(solution);
-    double res = 0.0;
+    ScalarVariableStatisticsPtr stats = new ScalarVariableStatistics("errors");
     for (size_t i = 0; i < scores.size(); ++i)
     {
       GPExpressionPtr formula = scores[i].first;
       context.enterScope(formula->toShortString());
+      context.resultCallback(T("formula number"), i);
       context.informationCallback(T("Target score: ") + String(scores[i].second));
       ScalarVariableMean mean;
       for (size_t j = 0; j < 100; ++j)
@@ -90,10 +90,10 @@ public:
         context.informationCallback(T("Predicted score: ") + String(predictedScore));
       }
       double delta = mean.getMean() - scores[i].second;
-      res += fabs(delta);
+      stats->push(fabs(delta));
       context.leaveScope(delta);
     }
-    return res / scores.size();
+    return stats;
   }
 
 private:
@@ -108,9 +108,49 @@ class ExploChallengeSandBox : public WorkUnit
 public:
   ExploChallengeSandBox() : surrogateHorizon(307000), surrogateNumDocuments(100)
   {
-
-
   }
+
+  struct Policies
+  {
+    static GPExpressionPtr random()
+      {return new ConstantGPExpression(1.0);}
+
+    static GPExpressionPtr presentedCount()
+      {return new VariableGPExpression(Variable(0, exploChallengeFormulaVariablesEnumeration));}
+
+    static GPExpressionPtr rewardMean()
+      {return new VariableGPExpression(Variable(1, exploChallengeFormulaVariablesEnumeration));}
+
+    static GPExpressionPtr playedCount()
+      {return new VariableGPExpression(Variable(2, exploChallengeFormulaVariablesEnumeration));}
+
+    static GPExpressionPtr relativeIndex()
+      {return new VariableGPExpression(Variable(3, exploChallengeFormulaVariablesEnumeration));}
+
+    static GPExpressionPtr logPresentedCount()
+      {return new UnaryGPExpression(gpLog, presentedCount());}
+
+    static GPExpressionPtr ucb1(double C) // rk + sqrt(C log(t) / tk)
+      {return new BinaryGPExpression(rewardMean(), gpAddition,
+              new UnaryGPExpression(gpSquareRoot,
+                new BinaryGPExpression(
+                  new BinaryGPExpression(new ConstantGPExpression(C), gpMultiplication, logPresentedCount()),
+                  gpDivision,
+                  playedCount())));}
+
+    static GPExpressionPtr minimalUCB(double C) // rk + C / tk
+      {return new BinaryGPExpression(rewardMean(), gpAddition,
+        new BinaryGPExpression(new ConstantGPExpression(C), gpDivision, playedCount()));}
+
+    static GPExpressionPtr minimalUCB2(double C) // rk + C / sqrt(tk)
+      {return new BinaryGPExpression(rewardMean(), gpAddition,
+        new BinaryGPExpression(new ConstantGPExpression(C), gpDivision,
+          new UnaryGPExpression(gpSquareRoot, playedCount())));}
+
+    static GPExpressionPtr maxRkC(double C)  // max(rk, C)
+      {return new BinaryGPExpression(rewardMean(), gpMax, new ConstantGPExpression(C));}
+
+  };
 
   ExploChallengeReverseObjectivePtr createObjective() const
   {
@@ -122,30 +162,52 @@ public:
     VariableGPExpressionPtr relativeIndex = new VariableGPExpression(Variable(3, exploChallengeFormulaVariablesEnumeration));
 
     // baselines
-    objective->addReferenceScore(rewardMean, 711.1); // Greedy
+    objective->addReferenceScore(Policies::random(), 366.7);
+    objective->addReferenceScore(Policies::rewardMean(), 711.1); // Greedy
     //objective->addReferenceScore(relativeIndex, 266.7); // Always first
-    objective->addReferenceScore(new ConstantGPExpression(1.0), 366.7); // C(1)
-    objective->addReferenceScore(new ConstantGPExpression(1.0), 366.7); // C(1)
-    objective->addReferenceScore(new ConstantGPExpression(1.0), 366.7); // C(1)
-    objective->addReferenceScore(new ConstantGPExpression(1.0), 366.7); // C(1)
     //objective->addReferenceScore(new UnaryGPExpression(gpOpposite, relativeIndex), 512.6); // Always last
-   
 
-    // minimalistic ucb
-    objective->addReferenceScore(new BinaryGPExpression(rewardMean, gpAddition,
-      new BinaryGPExpression(new ConstantGPExpression(1), gpDivision, playedCount)), 846.6);  // B(add, V(rk), B(div, C(2), V(tk)))
-    objective->addReferenceScore(new BinaryGPExpression(rewardMean, gpAddition,
-      new BinaryGPExpression(new ConstantGPExpression(2), gpDivision, playedCount)), 862.2);  // B(add, V(rk), B(div, C(2), V(tk)))
+    // ucb1
+    objective->addReferenceScore(Policies::ucb1(0.01), 849.4);
+    objective->addReferenceScore(Policies::ucb1(0.05), 838.8);
+    objective->addReferenceScore(Policies::ucb1(0.1), 817.4);
+    objective->addReferenceScore(Policies::ucb1(0.2), 762.5);
 
-    objective->addReferenceScore(new BinaryGPExpression(rewardMean, gpAddition,
-      new BinaryGPExpression(new ConstantGPExpression(30), gpDivision, playedCount)), 723.6);
-    objective->addReferenceScore(new BinaryGPExpression(rewardMean, gpAddition,
-      new BinaryGPExpression(new ConstantGPExpression(50), gpDivision, playedCount)), 650.5);
+    // minimal ucb 1
+    objective->addReferenceScore(Policies::minimalUCB(0.15), 776.8);
+    objective->addReferenceScore(Policies::minimalUCB(1.0), 846.6);
+    objective->addReferenceScore(Policies::minimalUCB(1.5), 876.3);
+    objective->addReferenceScore(Policies::minimalUCB(2.0), 862.2);
+    objective->addReferenceScore(Policies::minimalUCB(30.0), 723.6);
+    objective->addReferenceScore(Policies::minimalUCB(50.0), 650.5);
+
+    // minimal ucb 2
+    objective->addReferenceScore(Policies::minimalUCB2(0.2), 864.8);
+    objective->addReferenceScore(Policies::minimalUCB2(0.3), 877.2);
+    objective->addReferenceScore(Policies::minimalUCB2(0.4), 866.3);
+    objective->addReferenceScore(Policies::minimalUCB2(0.5), 859.9);
 
     // max(rk, C)
-    objective->addReferenceScore(new BinaryGPExpression(rewardMean, gpMax, new ConstantGPExpression(0.01)), 670.8);
-    objective->addReferenceScore(new BinaryGPExpression(rewardMean, gpMax, new ConstantGPExpression(0.03)), 740.1);
-    objective->addReferenceScore(new BinaryGPExpression(rewardMean, gpMax, new ConstantGPExpression(0.04)), 763.1);
+    objective->addReferenceScore(Policies::maxRkC(0.01), 670.8);
+    objective->addReferenceScore(Policies::maxRkC(0.02), 747.5);
+    objective->addReferenceScore(Policies::maxRkC(0.03), 740.1);
+    objective->addReferenceScore(Policies::maxRkC(0.04), 763.1);
+    objective->addReferenceScore(Policies::maxRkC(0.05), 768.7);
+    objective->addReferenceScore(Policies::maxRkC(0.06), 757.1);
+    objective->addReferenceScore(Policies::maxRkC(0.07), 761.1);
+    
+    // misc
+
+    // Rk + 0.2 log(Tk) / T	==> 682.6
+    objective->addReferenceScore(new BinaryGPExpression(Policies::rewardMean(), gpAddition,
+      new BinaryGPExpression(new ConstantGPExpression(0.2), gpMultiplication,
+        new BinaryGPExpression(new UnaryGPExpression(gpLog, Policies::playedCount()), gpDivision, Policies::presentedCount()))), 682.6);
+
+    // log(Tk)(Rk - 0.2)	==> 767.0
+    objective->addReferenceScore(new BinaryGPExpression(
+      new UnaryGPExpression(gpLog, Policies::playedCount()),
+      gpMultiplication,
+      new BinaryGPExpression(Policies::rewardMean(), gpSubtraction, new ConstantGPExpression(0.2))), 767.0);
     return objective;
   }
 
@@ -154,7 +216,7 @@ public:
     ExploChallengeReverseObjectivePtr objective = createObjective();
     SamplerPtr sampler = new ExploChallengeReverseSampler();
     OptimizationProblemPtr problem = new OptimizationProblem(objective, Variable(), sampler);
-    OptimizerPtr optimizer = edaOptimizer(10, 300, 30, StoppingCriterionPtr(), 0.0, false, true);
+    OptimizerPtr optimizer = edaOptimizer(10, 100, 30, StoppingCriterionPtr(), 0.0, false, true);
     OptimizerStatePtr state = optimizer->optimize(context, problem);
 
 
@@ -168,12 +230,12 @@ public:
     //context.resultCallback("state", state);
 
     context.enterScope(T("Validate Best Solution"));
-    double score = objective->validate(context, state->getBestSolution());
-    context.leaveScope(score);
+    Variable res = objective->validate(context, state->getBestSolution());
+    context.leaveScope(res);
 
     context.enterScope(T("Validate Sampler Expectation"));
-    score = objective->validate(context, samplerExpectation);
-    context.leaveScope(score);
+    res = objective->validate(context, samplerExpectation);
+    context.leaveScope(res);
     return true;
   }
 
