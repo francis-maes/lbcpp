a5fb36336a80a1924cb0afee3a368dd124bca657
francis <francis@885cb74b-98ed-4c74-c960-63734602d729>
2012-03-21 18:42:18 +0000

    hop

--- projects/SequentialDecision/Luape/LuapeClassificationSandBox.h
+++ projects/SequentialDecision/Luape/LuapeClassificationSandBox.h
@@ -523,7 +523,8 @@ protected:
   {
     bool useRandomSplits = method.startsWith(T("ET"));
     bool hasFeatureGeneration = (featureLength > 0);
-    bool useRandomSubspaces = ((method.startsWith(T("ET")) && method != T("ETn")) || method == T("RF"));
+    bool useRandomSubspaces = ((method.startsWith(T("ET")) && method != T("ETn")) || method == T("RF") || (featureBudget > 0 && featureBudget < 1));
+
     size_t budget = featureBudget ? (size_t)juce::jmax(1.0, numVariables * featureBudget) : (size_t)(0.5 + sqrt((double)numVariables));
 
     LuapeLearnerPtr conditionLearner;
