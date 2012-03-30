9f09c51207c54fe1297c5eb6450230dbe0c08325
alejandro <alejandro@885cb74b-98ed-4c74-c960-63734602d729>
2012-03-30 09:27:44 +0000

    hop

--- trunk/projects/Proteins/Rosetta/WorkUnit/DistributableWorkUnit.h
+++ trunk/projects/Proteins/Rosetta/WorkUnit/DistributableWorkUnit.h
@@ -164,7 +164,7 @@ public:
       {
         if (numExecuted >= 20)
         {
-          remoteContext->waitUntilAllWorkUnitsAreDone(time * 60 * 60 * 501);
+          remoteContext->waitUntilAllWorkUnitsAreDone(time * 60 * 60 * 451);
           numExecuted = 0;
         }
         remoteContext->pushWorkUnit(units->getWorkUnit(i), new DistributableExecutionContextCallback(context, distributable->toString()));
