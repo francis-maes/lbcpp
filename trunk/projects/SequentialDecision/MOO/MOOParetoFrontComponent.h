f7b4f4c5a7a58cf641087ce5ac23d560d9973eb4
francis <francis@885cb74b-98ed-4c74-c960-63734602d729>
2012-09-11 18:00:25 +0000

    '''ParetoFront UserInterface'''
     * ParetoFrontComponent: Make selectable with mouse and keyboard
     * ParetoFrontComponent: draw intermediate lines

--- trunk/projects/SequentialDecision/MOO/MOOParetoFrontComponent.h
+++ trunk/projects/SequentialDecision/MOO/MOOParetoFrontComponent.h
@@ -11,6 +11,7 @@
 
 # include "MOOCore.h"
 # include <lbcpp/UserInterface/ComponentWithPreferedSize.h>
+# include <lbcpp/UserInterface/VariableSelector.h>
 # include "../../../src/UserInterface/Plot/TwoDimensionalPlotDrawable.h"
 
 namespace lbcpp
@@ -44,30 +45,81 @@ public:
       return;
 
     const MOOParetoFront::ParetoMap& m = front->getParetoMap();
-    g.setColour(juce::Colours::black);
-    int baseSize = juce::jmin(g.getClipBounds().getWidth() / 10, g.getClipBounds().getHeight() / 10);
-    for (MOOParetoFront::ParetoMap::const_iterator it = m.begin(); it != m.end(); ++it)
+    g.setColour(juce::Colours::lightgrey);
+    MOOParetoFront::ParetoMap::const_iterator it, nxt;
+    for (it = m.begin(); it != m.end(); it = nxt)
     {
-      double x = it->first->getObjective(0);
-      double y = it->first->getObjective(1);
-      transform.transformPoint(x, y);
-      paintPoint(g, (int)x, (int)y, baseSize);
+      nxt = it; ++nxt;
+      if (nxt == m.end())
+        break;
+      int x1, y1, x2, y2, x3, y3;
+      getPixelPosition(it->first, x1, y1, transform);
+      getPixelPosition(MOOFitness::makeWorstCombination(it->first, nxt->first), x2, y2, transform);
+      getPixelPosition(nxt->first, x3, y3, transform);
+      g.drawLine((float)x1, (float)y1, (float)x2, (float)y2);
+      g.drawLine((float)x2, (float)y2, (float)x3, (float)y3);
+    }
+
+    for (it = m.begin(); it != m.end(); ++it)
+    {
+      int x, y;
+      getPixelPosition(it->first, x, y, transform);
+      g.setColour(it->first == currentFitness ? juce::Colours::red : juce::Colours::black);
+      paintPoint(g, x, y);
     }
   }
+
+  MOOFitnessPtr hittest(int x, int y, const juce::AffineTransform& transform) const
+  {
+    double minDistance = DBL_MAX;
+    MOOFitnessPtr res;
+
+    const MOOParetoFront::ParetoMap& m = front->getParetoMap();
+    for (MOOParetoFront::ParetoMap::const_iterator it = m.begin(); it != m.end(); ++it)
+    {
+      int ox, oy;
+      getPixelPosition(it->first, ox, oy, transform);
+      double distance = sqrt((double)((x - ox) * (x - ox) + (y - oy) * (y - oy)));
+      if (distance < minDistance)
+      {
+        minDistance = distance;
+        res = it->first;
+      }
+    }
+
+    const int hittestRadius = 7;
+    return minDistance < hittestRadius ? res : MOOFitnessPtr();
+  }
+
+  void setCurrentFitness(const MOOFitnessPtr& fitness)
+    {currentFitness = fitness;}
+
+  const MOOFitnessPtr& getCurrentFitness() const
+    {return currentFitness;}
 
 protected:
   MOOParetoFrontPtr front;
   PlotAxisPtr xAxis, yAxis;
+  MOOFitnessPtr currentFitness;
 
-  void paintPoint(juce::Graphics& g, int x, int y, int baseSize) const
+  void getPixelPosition(MOOFitnessPtr fitness, int& x, int& y, const juce::AffineTransform& transform) const
   {
-    enum {s = 5};
-    int pointHalfSize = baseSize / 10;
+    double dx = fitness->getObjective(0);
+    double dy = fitness->getObjective(1);
+    transform.transformPoint(dx, dy);
+    x = (int)dx;
+    y = (int)dy;
+  }
+
+  void paintPoint(juce::Graphics& g, int x, int y) const
+  {
+    const int pointHalfSize = 4;
+    const float lineWidth = 2.f;
+
     float x1 = (float)(x - pointHalfSize);
     float y1 = (float)(y - pointHalfSize);
     float x2 = (float)(x + pointHalfSize);
     float y2 = (float)(y + pointHalfSize);
-    float lineWidth = juce::jmax(1.f, baseSize / 20.f);
     g.drawLine(x1, y1, x2 + 1.f, y2 + 1.f, lineWidth);
     g.drawLine(x1, y2, x2 + 1.f, y1 + 1.f, lineWidth);
   }
@@ -87,12 +139,13 @@ protected:
   }
 };
 
-class MOOParetoFrontComponent : public juce::Component, public ComponentWithPreferedSize
+class MOOParetoFrontComponent : public juce::Component, public ComponentWithPreferedSize, public VariableSelector
 {
 public:
   MOOParetoFrontComponent(MOOParetoFrontPtr front, const String& name)
-    : front(front)
+    : front(front), drawable(front)
   {
+    setWantsKeyboardFocus(true);
   }
 
   virtual int getDefaultWidth() const
@@ -115,25 +168,108 @@ public:
       return;
     }
 
-    enum
-    {
-      leftMargin = 60,
-      rightMargin = 20,
-      topMargin = 20,
-      bottomMargin = 40
-    };
-
-    MOOParetoFrontDrawable drawable(front);
-    drawable.drawWithin(g, leftMargin, topMargin, getWidth() - leftMargin - rightMargin, getHeight() - topMargin - bottomMargin, juce::RectanglePlacement::stretchToFit);
+    if (getWidth() > leftMargin + rightMargin && getHeight() > topMargin + bottomMargin)
+      drawable.draw(g, getTransform());
   }
+
+  virtual bool keyPressed(const juce::KeyPress& key)
+  {
+    if (key.getKeyCode() == juce::KeyPress::leftKey)
+    {
+      changeSelection(false);
+      return true;
+    }
+    if (key.getKeyCode() == juce::KeyPress::rightKey)
+    {
+      changeSelection(true);
+      return true;
+    }
+    return false;
+  }
+
+  virtual void mouseUp(const juce::MouseEvent& e)
+    {select(drawable.hittest(e.getMouseDownX(), e.getMouseDownY(), getTransform()));}
 
 protected:
   MOOParetoFrontPtr front;
+  MOOParetoFrontDrawable drawable;
 
   void paintText(juce::Graphics& g, const String& text)
   {
     g.setColour(juce::Colours::grey);
     g.drawText(text, 0, 0, getWidth(), getHeight(), juce::Justification::centred, true);
+  }
+
+  enum
+  {
+    leftMargin = 60,
+    rightMargin = 20,
+    topMargin = 20,
+    bottomMargin = 40
+  };
+
+  juce::AffineTransform getTransform() const
+  {
+    const int destX = leftMargin;
+    const int destY = topMargin;
+    const int destW = getWidth() - leftMargin - rightMargin;
+    const int destH = getHeight() - topMargin - bottomMargin;
+
+    float x, y, w, h;
+    drawable.getBounds(x, y, w, h);
+    juce::RectanglePlacement placement(juce::RectanglePlacement::stretchToFit);
+    return placement.getTransformToFit(x, y, w, h, (float)destX, (float)destY, (float)destW, (float)destH);
+  }
+
+  void select(const MOOFitnessPtr& fitness)
+  {
+    if (fitness)
+    {
+      std::vector<ObjectPtr> solutions;
+      front->getSolutionsByFitness(fitness, solutions);
+
+      Variable res;
+      if (solutions.size() == 1)
+        res = Variable::pair(fitness->getVariable(0), solutions[0]);
+      else
+      {
+        ObjectVectorPtr sol(new ObjectVector(objectClass, 0));
+        for (size_t i = 0; i < solutions.size(); ++i)
+          sol->append(solutions[i]);
+        res = Variable::pair(fitness->getVariable(0), sol);
+      }
+      sendSelectionChanged(res, T("pareto point"));
+    }
+    else
+      sendSelectionChanged(std::vector<Variable>(), String::empty);
+
+    drawable.setCurrentFitness(fitness);
+    repaint();
+  }
+
+  void changeSelection(bool gotoRight)
+  {
+    const MOOFitnessPtr& current = drawable.getCurrentFitness();
+    if (!current)
+      return;
+    const MOOParetoFront::ParetoMap& m = front->getParetoMap();
+    MOOParetoFront::ParetoMap::const_iterator it = m.find(current);
+    if (it == m.end())
+      return;
+    if (gotoRight)
+    {
+      ++it;
+      if (it != m.end())
+        select(it->first);
+    }
+    else
+    {
+      if (it != m.begin())
+      {
+        --it;
+        select(it->first);
+      }
+    }
   }
 };
 
