eff8b4d56f0192065b7bd93f380a6da2e625d410
francis <francis@885cb74b-98ed-4c74-c960-63734602d729>
2012-09-14 14:34:44 +0000

    '''Multi-objective optimization: comparators'''
     * Comparator base class
     * Re-implement multi-objective cross-entropy as a particular case of cross-entropy with a dedicated comparator

---
+++ trunk/projects/SequentialDecision/MOO/ObjectiveComparator.h
@@ -0,0 +1,44 @@
+/*-----------------------------------------.---------------------------------.
+| Filename: ObjectiveComparator.h          | Objective Comparator            |
+| Author  : Francis Maes                   |                                 |
+| Started : 1/09/2012 16:07                |                                 |
+`------------------------------------------/                                 |
+                               |                                             |
+                               `--------------------------------------------*/
+
+#ifndef LBCPP_MOO_COMPARATOR_OBJECTIVE_H_
+# define LBCPP_MOO_COMPARATOR_OBJECTIVE_H_
+
+# include "MOOCore.h"
+
+namespace lbcpp
+{
+
+class ObjectiveComparator : public MOOSolutionComparator
+{
+public:
+  ObjectiveComparator(size_t index = (size_t)-1) : index(index) {}
+
+  virtual void initialize(const MOOSolutionSetPtr& solutions)
+    {this->solutions = solutions;}
+
+  virtual int compare(size_t index1, size_t index2)
+  {
+    double value1 = solutions->getFitness(index1)->getValue(index);
+    double value2 = solutions->getFitness(index2)->getValue(index);
+    double deltaValue = solutions->getFitnessLimits()->getObjectiveSign(index) * (value2 - value1);
+    return deltaValue > 0 ? 1 : (deltaValue < 0 ? -1 : 0);
+  }
+
+protected:
+  friend class ObjectiveComparatorClass;
+
+  size_t index;
+
+private:
+  MOOSolutionSetPtr solutions;
+};
+
+}; /* namespace lbcpp */
+
+#endif // !LBCPP_MOO_COMPARATOR_OBJECTIVE_H_
