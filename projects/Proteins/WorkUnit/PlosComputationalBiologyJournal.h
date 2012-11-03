96ef1dcbdabaf9f2aff613133c11c72bf969c624
julien <julien@885cb74b-98ed-4c74-c960-63734602d729>
2012-11-03 16:13:32 +0000

    Pattern predictor

--- projects/Proteins/WorkUnit/PlosComputationalBiologyJournal.h
+++ projects/Proteins/WorkUnit/PlosComputationalBiologyJournal.h
@@ -5,47 +5,6 @@
 namespace lbcpp
 {
 
-class ExportDisulfidePatternEvaluator : public SupervisedEvaluator
-{
-public:
-  /* SupervisedEvaluator */
-  virtual TypePtr getRequiredPredictionType() const
-    {return matrixClass(doubleType);}
-
-  virtual TypePtr getRequiredSupervisionType() const
-    {return matrixClass(probabilityType);}
-  
-  virtual void addPrediction(ExecutionContext& context, const Variable& prediction, const Variable& supervision, const ScoreObjectPtr& result) const
-  {
-    MatrixPtr predictedMatrix = prediction.getObjectAndCast<Matrix>();
-    printToFile(context, T("unmatched.pattern"), predictedMatrix);
-
-    predictedMatrix = (new KolmogorovPerfectMatchingFunction(0.f))->compute(context, predictedMatrix).getObjectAndCast<Matrix>(context);
-    printToFile(context, T("matched.pattern"), predictedMatrix);
-  }
-  
-  /* Evaluator */
-  virtual ScoreObjectPtr createEmptyScoreObject(ExecutionContext& context, const FunctionPtr& function) const
-    {return new DummyScoreObject();}
-  
-  virtual void finalizeScoreObject(const ScoreObjectPtr& score, const FunctionPtr& function) const {}
-
-private:
-  void printToFile(ExecutionContext& context, const String& fileName, const MatrixPtr& matrix) const
-  {
-    OutputStream* o = context.getFile(fileName).createOutputStream();
-    for (size_t i = 0; i < matrix->getNumRows(); ++i)
-    {
-      for (size_t j = 0; j < matrix->getNumColumns(); ++j)
-      {
-        *o << matrix->getElement(i, j).toString() + T("\t");
-      }
-      *o << T("\n");
-    }
-    delete o;
-  }
-};
-
 class PlosComputationalBiologyJournal : public WorkUnit
 {
 public:
@@ -78,13 +37,10 @@ public:
       context.errorCallback(T("Invalid PSSM file !"));
       return false;
     }
-    // Compile protein
+
+    // Add PSSM to the protein
     protein->setPositionSpecificScoringMatrix(pssm);
-    // Dummy supervision
     protein->setDisulfideBonds(symmetricMatrix(probabilityType, protein->getCysteinIndices().size()));
-
-    proteins = proteins->apply(context, proteinToInputOutputPairFunction(false));
-
     // Features
     LargeProteinParametersPtr parameter = new LargeProteinParameters();
     parameter->pssmWindowSize = 15;
@@ -96,10 +52,10 @@ public:
     ProteinPredictorPtr predictor = new ProteinPredictor(learningMachine);
     predictor->addTarget(dsbTarget);
     // Evaluators
-    ProteinEvaluatorPtr evaluator = new ProteinEvaluator();
-    evaluator->addEvaluator(dsbTarget, new ExportDisulfidePatternEvaluator(), T("Export Patterns"));
 
-    predictor->evaluate(context, proteins, evaluator, T("Evaluating protein ..."));
+    Variable res = predictor->compute(context, protein, protein);
+
+    exportResult(context, res.getObjectAndCast<Protein>());
     return true;
   }
 
@@ -108,6 +64,106 @@ protected:
 
   File fastaFile;
   File pssmFile;
+  File outputFile;
+
+private:
+  void exportResult(ExecutionContext& context, const ProteinPtr& protein) const
+  {
+    const SymmetricMatrixPtr& pattern = protein->getDisulfideBonds(context);
+    const std::vector<size_t> cysteinIndices = protein->getCysteinIndices();
+    jassert(pattern->getDimension() == cysteinIndices.size());
+    const size_t n = cysteinIndices.size();
+
+    if (outputFile.exists())
+      outputFile.deleteFile();
+    
+    OutputStream* o = outputFile.createOutputStream();
+    *o << "#------------------------------------------#\n";
+    *o << "|               x3CysBridges               |-------------------------#\n";
+    *o << "#------------------------------------------#          Julien Becker  |\n";
+    *o << "                                      |                Francis Maes  |\n";
+    *o << "    Disulfide Bonding Probability     |              Louis Wehenkel  |\n";
+    *o << "                  &                   #------------------------------#\n";
+    *o << "   Disulfide Connectivity Pattern\n";
+    *o << "\n";
+    *o << "\n";
+    *o << "\n";
+
+    String ps = protein->getPrimaryStructure()->toString();
+    String formatedPs;
+    for (size_t i = 0; i < (size_t)ps.length(); ++i)
+    {
+      if (i % 50 == 0)
+        formatedPs += T("\n     ");
+      else if (i % 5 == 0)
+        formatedPs += T(" ");
+      formatedPs += ps[i];
+    }
+    
+    String cysString;
+    for (size_t i = 0; i < n; ++i)
+    {
+      if (i % 5 == 0)
+        cysString += T("\n     ");
+      cysString += T("Cys") + String((int)cysteinIndices[i] + 1) + T(" ");
+    }
+    
+    *o << "#- Query ------------------------------------------------------------#\n";
+           
+    *o << "Sequence : " << formatedPs << "\n";
+    *o << "Cysteines: " << cysString << "\n";
+    *o << "\n";
+    *o << "\n";
+
+    *o << "#- Disulfide Bonding Probabilities - (Rank, Probability, Cys, Cys) --#\n";
+
+    typedef std::multimap<double, std::pair<size_t, size_t> > ScoresMap;
+    ScoresMap bondingProbabilities;
+    for (size_t i = 0; i < n; ++i)
+      for (size_t j = i + 1; j < n; ++j)
+        bondingProbabilities.insert(std::make_pair(pattern->getElement(i, j).getDouble(), std::make_pair(cysteinIndices[i] + 1, cysteinIndices[j] + 1)));
+    
+    ScoresMap::const_reverse_iterator it = bondingProbabilities.rbegin();
+    for (size_t i = 1; it != bondingProbabilities.rend(); ++i, it++)
+    {
+      if (it->first < 1e-6)
+        break;
+
+      *o << toFixedLengthStringLeftJustified(String((int)i), 3) << " ";
+      *o << toFixedLengthStringLeftJustified(String(it->first, 5), 7) << " ";
+      *o << toFixedLengthStringLeftJustified(T("Cys") + String((int)it->second.first), 6) << " ";
+      *o << toFixedLengthStringLeftJustified(T("Cys") + String((int)it->second.second), 6);
+      *o << "\n";
+    }
+
+    *o << "\n";
+    *o << "\n";
+
+    *o << "#- Disulfide Connectivity Pattern - (Cys, Cys) ----------------------#\n";
+
+    SymmetricMatrixPtr kolmogorov = (new KolmogorovPerfectMatchingFunction(0.f))->compute(context, pattern).getObjectAndCast<SymmetricMatrix>(context);
+    jassert(kolmogorov && kolmogorov->getDimension() == n);
+    for (size_t i = 0; i < n; ++i)
+      for (size_t j = i + 1; j < n; ++j)
+      {
+        if (kolmogorov->getElement(i,j).getDouble() != 0.f)
+        {
+          *o << toFixedLengthStringLeftJustified(T("Cys") + String((int)cysteinIndices[i] + 1), 6) << " ";
+          *o << toFixedLengthStringLeftJustified(T("Cys") + String((int)cysteinIndices[j] + 1), 6) << "\n";
+        }
+      }
+
+    delete o;
+  }
+  
+  String toFixedLengthStringLeftJustified(const String& str, int length) const
+  {
+    jassert(str.length() <= length);
+    String res = str;
+    while (res.length() < length)
+      res += T(" ");
+    return res;
+  }
 };
 
 }
