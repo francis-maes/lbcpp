f6fb125db0741f601fe753272302516179190bf5
francis <francis@885cb74b-98ed-4c74-c960-63734602d729>
2012-11-16 10:09:34 +0000

    Make directory for tree views

--- trunk/lbcpp-core/UserInterface/TreeView/ExecutionTraceTreeView.cpp
+++ trunk/lbcpp-core/UserInterface/TreeView/ExecutionTraceTreeView.cpp
@@ -17,6 +17,168 @@ using juce::Graphics;
 using juce::Colour;
 
 /*
+** ExecutionTraceTreeViewItem
+*/
+ExecutionTraceTreeViewItem::ExecutionTraceTreeViewItem(ExecutionTraceTreeView* owner, const ExecutionTraceItemPtr& trace)
+  : GenericTreeViewItem(owner, trace, trace->toShortString())
+{
+  string str = getUniqueName();
+  numLines = 1;
+  for (int i = 0; i < str.length() - 1; ++i)
+    if (str[i] == '\n')
+      ++numLines;
+}
+
+ExecutionTraceTreeViewItem* ExecutionTraceTreeViewItem::create(ExecutionTraceTreeView* owner, const ExecutionTraceItemPtr& item)
+{
+  ExecutionTraceNodePtr node = item.dynamicCast<ExecutionTraceNode>();
+  if (node)
+    return new ExecutionTraceTreeViewNode(owner, node);
+  else
+    return new ExecutionTraceTreeViewItem(owner, item);
+}
+
+void ExecutionTraceTreeViewItem::paintProgression(Graphics& g, ProgressionStatePtr progression, int x, int width, int height)
+{
+  juce::GradientBrush brush(Colour(200, 220, 240), (float)x, -(float)width / 3.f, Colours::white, (float)width, (float)width, true);
+
+  g.setBrush(&brush);
+  if (progression->isBounded())
+    g.fillRect(x, 0, (int)(width * progression->getNormalizedValue() + 0.5), height);
+  else
+  {
+    double p = (progression->getValue() / 1000.0);
+    p -= (int)p;
+    jassert(p >= 0.0 && p <= 1.0);
+    g.fillRect(x + (int)(width * p + 0.5) - 3, 0, 6, height);
+  }
+  g.setBrush(NULL);
+
+  g.setColour(Colours::black);
+  g.setFont(10);
+  g.drawText(progression->toString(), x, 0, width, height, juce::Justification::centred, true);
+
+  g.setColour(Colour(180, 180, 180));
+  g.drawRect(x, 0, width, height, 1);
+}
+
+void ExecutionTraceTreeViewItem::paintIcon(Graphics& g, int width, int height)
+  {g.setColour(Colours::black); g.drawImageAt(iconToUse, 0, (height - iconToUse->getHeight()) / 2);}
+
+void ExecutionTraceTreeViewItem::paintIconTextAndProgression(Graphics& g, int width, int height)
+{
+  paintIcon(g, width, height);
+  width -= labelX;
+  int textWidth = width;
+
+  ExecutionTraceNodePtr traceNode = getTrace().dynamicCast<ExecutionTraceNode>();
+  if (traceNode && traceNode->getProgression() && width > minWidthToDisplayProgression)
+  {
+    textWidth -= progressionColumnWidth;
+    paintProgression(g, traceNode->getProgression(), labelX + textWidth, progressionColumnWidth, height);
+  }
+  
+  g.setColour(Colours::black);
+  g.setFont(12);
+  
+  string str = getUniqueName();
+  StringArray lines;
+  lines.addTokens(str, T("\n"), NULL);
+  for (int i = 0; i < lines.size(); ++i)
+    g.drawText(lines[i], labelX, 20 * i, textWidth, 20, juce::Justification::centredLeft, true);
+}
+
+void ExecutionTraceTreeViewItem::paintItem(Graphics& g, int width, int height)
+{
+  if (isSelected())
+    g.fillAll(Colours::darkgrey);
+  --height; // 1 px margin
+  if (width > minWidthToDisplayTimes)
+  {
+    int w = width - 2 * timeColumnWidth;
+    paintIconTextAndProgression(g, w, height);
+    g.setColour(Colours::grey);
+    g.setFont(12);
+
+    ExecutionTraceItemPtr trace = getTrace();
+    ExecutionTraceNodePtr node = trace.dynamicCast<ExecutionTraceNode>();
+    if (node)
+    {
+      ObjectPtr returnValue = node->getReturnValue();
+      string text = returnValue ? returnValue->toShortString() : string::empty;
+      g.drawText(text, w, 0, timeColumnWidth, height, juce::Justification::centredRight, false);
+    }
+
+    ExecutionTraceNodePtr workUnitTrace = trace.dynamicCast<ExecutionTraceNode>();
+    if (workUnitTrace)
+    {
+      double timeLength = workUnitTrace->getTimeLength();
+      string timeLengthString = timeLength ? ObjectPtr(new Time(timeLength))->toShortString() : T("...");
+      g.drawText(timeLengthString, w + timeColumnWidth, 0, timeColumnWidth, height, juce::Justification::centredRight, false);
+    }
+  }
+  else
+    paintIconTextAndProgression(g, width, height);
+}
+
+/*
+** ExecutionTraceTreeViewNode
+*/
+ExecutionTraceTreeViewNode::ExecutionTraceTreeViewNode(ExecutionTraceTreeView* owner, const ExecutionTraceNodePtr& trace)
+  : ExecutionTraceTreeViewItem(owner, trace)
+{
+  WorkUnitPtr workUnit = trace->getWorkUnit();
+  if (workUnit)
+  {
+    // online
+    CompositeWorkUnitPtr compositeWorkUnit = trace->getWorkUnit().dynamicCast<CompositeWorkUnit>();
+    setOpen((!compositeWorkUnit || compositeWorkUnit->getNumWorkUnits() <= 10));
+  }
+  else
+    setOpen(trace->getNumSubItems() < 10 && trace->getTimeLength() > 1); // open nodes that took more than 1 seconds and that have less than 10 childs
+}
+
+void ExecutionTraceTreeViewNode::itemOpennessChanged(bool isNowOpen)
+{
+  if (isNowOpen)
+  {
+    if (!hasBeenOpened)
+      hasBeenOpened = true;
+    if (getTraceNode()->getNumSubItems() != (size_t)getNumSubItems())
+    {
+      clearSubItems();
+      createSubItems();
+    }
+  }
+}
+
+ObjectPtr ExecutionTraceTreeViewNode::getTargetObject(ExecutionContext& context) const
+{
+  ExecutionTraceNodePtr trace = getTraceNode();
+  bool hasResults = trace->getResults().size() > 0;//(size_t)(trace->getReturnValue().exists() ? 1 : 0);
+  bool hasSubItems = trace->getNumSubItems() > 0;
+  if (!hasResults && !hasSubItems)
+    return ObjectPtr();
+          
+  VectorPtr results;
+  TablePtr table;
+  if (hasResults)
+    results = trace->getResultsVector(context);
+
+  if (hasSubItems)
+  {
+    table = trace->getChildrenResultsTable(context);
+    if (table && table->getNumColumns() == 1)
+      table = TablePtr(); // do not display tables that have only one column
+  }
+
+  if (results || table)
+    return new Pair(results, table);
+  else
+    return ObjectPtr();
+}
+
+/*
 ** ExecutionTraceTreeView
 */
 ExecutionTraceTreeView::ExecutionTraceTreeView(ExecutionTracePtr trace, const string& name, ExecutionContextPtr context)
---
+++ trunk/lbcpp-core/UserInterface/TreeView/ObjectTreeView.h
@@ -0,0 +1,116 @@
+/*-----------------------------------------.---------------------------------.
+| Filename: ObjectTreeView.h               | Object Tree component           |
+| Author  : Francis Maes                   |                                 |
+| Started : 14/06/2010 12:05               |                                 |
+`------------------------------------------/                                 |
+                               |                                             |
+                               `--------------------------------------------*/
+
+#ifndef LBCPP_USER_INTERFACE_TREE_VIEW_OBJECT_H_
+# define LBCPP_USER_INTERFACE_TREE_VIEW_OBJECT_H_
+
+# include "GenericTreeView.h"
+
+namespace lbcpp
+{
+  
+class ObjectTreeViewItem : public GenericTreeViewItem
+{
+public:
+  ObjectTreeViewItem(GenericTreeView* owner, const string& name, const ObjectPtr& object)
+    : GenericTreeViewItem(owner, object, name), typeName(object->getClassName()), shortString(object->toShortString()) {}
+
+  virtual const string getTooltip()
+  {
+    string res = T("Name: ") + getUniqueName() +
+      T("\nType: ") + typeName +
+      T("\nValue: ") + shortString;
+    if (getNumSubItems())
+      res += T("\nNum sub variables: ") + string((int)getNumSubItems());
+    return res;
+  }
+
+  virtual void paintItem(Graphics& g, int width, int height)
+  {
+    if (isSelected())
+      g.fillAll(Colours::lightgrey);
+    g.setColour(Colours::black);
+    int x1 = 0;
+    if (iconToUse)
+    {
+      g.drawImageAt(iconToUse, 0, (height - iconToUse->getHeight()) / 2);
+      x1 += iconToUse->getWidth() + 5;
+    }
+
+    int typeAndNameLength;
+    enum {wantedLength = 400, numFields = 3};
+    int remainingWidth = width - x1;
+    if (remainingWidth >= numFields * wantedLength)
+      typeAndNameLength = wantedLength;
+    else
+      typeAndNameLength = remainingWidth / numFields;
+
+    g.setFont(juce::Font(12, juce::Font::bold));
+    g.drawText(getUniqueName(), x1, 0, typeAndNameLength - 5, height, juce::Justification::centredLeft, true);
+    x1 += typeAndNameLength;
+    g.setFont(juce::Font(12, juce::Font::italic));
+    g.drawText(typeName, x1, 0, typeAndNameLength - 5, height, juce::Justification::centredLeft, true);
+    x1 += typeAndNameLength;
+
+    g.setFont(juce::Font(12));
+    g.drawText(shortString, x1, 0, width - x1 - 2, height, juce::Justification::centredLeft, true);
+  }
+
+  juce_UseDebuggingNewOperator
+
+protected:
+  string typeName;
+  string shortString;
+};
+
+class ObjectTreeView : public GenericTreeView
+{
+public:
+  ObjectTreeView(const ObjectPtr& object, const string& name, bool makeRootNodeVisible)
+    : GenericTreeView(object, name)
+  {
+    setRootItemVisible(makeRootNodeVisible);
+    buildTree();
+  }
+  
+  juce_UseDebuggingNewOperator
+
+protected:
+  virtual GenericTreeViewItem* createItem(const ObjectPtr& object, const string& name)
+    {return new ObjectTreeViewItem(this, name, object);}
+
+  virtual bool mightHaveSubObjects(const ObjectPtr& object)
+    {return object->getNumVariables() > 0 || object.isInstanceOf<Vector>();}
+
+  virtual std::vector< std::pair<string, ObjectPtr> > getSubObjects(const ObjectPtr& object)
+  {
+    ClassPtr type = object->getClass();
+    std::vector< std::pair<string, ObjectPtr> > res;
+    res.reserve(type->getNumMemberVariables());
+    for (size_t i = 0; i < type->getNumMemberVariables(); ++i)
+    {
+      ObjectPtr value = object->getVariable(i);
+      if (value)
+        res.push_back(std::make_pair(type->getMemberVariableName(i), value));
+    }
+  
+    VectorPtr vector = object.dynamicCast<Vector>();
+    if (vector)
+    {
+      size_t count = vector->getNumElements();
+      res.reserve(res.size() + count);
+      for (size_t i = 0; i < count; ++i)
+        res.push_back(std::make_pair(string((int)i), vector->getElement(i)));
+    }
+    return res;
+  }
+};
+
+}; /* namespace lbcpp */
+
+#endif // !LBCPP_USER_INTERFACE_TREE_VIEW_OBJECT_H_
