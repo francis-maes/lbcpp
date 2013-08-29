069ca7ddcc4e3480206ba2bd7edc449c3c380273
julien <julien@885cb74b-98ed-4c74-c960-63734602d729>
2013-08-29 11:48:56 +0000

    EspritzPredictionToProtein

--- projects/Proteins/WorkUnit/ConverterWorkUnits.h
+++ projects/Proteins/WorkUnit/ConverterWorkUnits.h
@@ -1453,4 +1453,103 @@ protected:
   }
 };
 
+class EspritzPredictionFileParser : public TextParser
+{
+public:
+  EspritzPredictionFileParser(ExecutionContext& context, const File& file, const ProteinPtr& protein)
+    : TextParser(context, file), protein(protein), residueNumber(0) {}
+  
+  virtual TypePtr getElementsType() const
+    {return proteinClass;}
+  
+  virtual void parseBegin()
+  {
+    dr = Protein::createEmptyProbabilitySequence(protein->getLength());
+  }
+
+  virtual bool parseLine(const String& line)
+  {
+
+    String str = line.trim();
+    StringArray tokens;
+    tokens.addTokens(str, false);
+    
+    if (tokens[0] == T("D"))
+      dr->setElement(residueNumber, probability(1.f));
+    else if (tokens[0] == T("O"))
+      dr->setElement(residueNumber, probability(0.f));
+    else
+      jassertfalse;
+    
+    ++residueNumber;
+
+    return true;
+  }
+  
+  virtual bool parseEnd()
+  {
+    if (residueNumber != protein->getLength())
+    {
+      context.errorCallback(T("EspritzPredictionFileParser::parseEnd")
+                          , T("Invalide number of residues"));
+      return false;
+    }
+
+    protein->setDisorderRegions(dr);
+    setResult(protein);
+
+    return true;
+  }
+
+private:
+  ProteinPtr protein;
+  size_t residueNumber;
+  DenseDoubleVectorPtr dr;
+};
+
+class EspritzPredictionToProtein : public WorkUnit
+{
+public:
+  virtual Variable run(ExecutionContext& context)
+  {
+    if (espritzFile.isDirectory())
+    {
+      juce::OwnedArray<File> files;
+      espritzFile.findChildFiles(files, File::findFiles, false, T("*.diso"));
+      bool res = true;
+      for (size_t i = 0; i < (size_t)files.size(); ++i)
+        res &= extract(context
+                     , inputFile.getChildFile(files[i]->getFileNameWithoutExtension() + T(".xml"))
+                     , *files[i]
+                     , outputFile.getChildFile(files[i]->getFileNameWithoutExtension() + T(".xml")));
+      return res;
+    }
+    else
+      return extract(context, inputFile, espritzFile, outputFile);
+  }
+
+protected:
+  friend class EspritzPredictionToProteinClass;
+
+  File inputFile;
+  File espritzFile;
+  File outputFile;
+
+  bool extract(ExecutionContext& context, File input, File espritz, File output) const
+  {
+    ProteinPtr protein = Protein::createFromXml(context, input);
+    if (!protein)
+    {
+      context.errorCallback(T("DisopredPredictionToProtein::extract"),
+                            T("Error while loading file: ") + espritz.getFullPathName());
+      return false;
+    }
+
+    StreamPtr(new EspritzPredictionFileParser(context, espritz, protein))->next();
+
+    protein->saveToXmlFile(context, output);
+    return true;    
+  }
+};
+
 };
